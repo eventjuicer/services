5f38dc8dab0b6e29e287c4c05ded0b6e814a755c
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-03-04 13:54:30 +0100

    ehandel2

---
+++ resources/views/emails/company/ehandelcompl2-pl.blade.php
@@ -0,0 +1,32 @@
+
+
+@component('mail::message')
+ 
+# Dzień dobry {{ array_get($profile, "fname") }},
+
+Po bardzo udanym starcie [ehandel.com.pl](https://ehandel.com.pl/), kompletujemy **radę Ekspertów**.
+
+
+@component('mail::panel')
+
+Jeśli w Twojej firmie znajdują się osoby chętne do **okresowego komentowania trendów rynkowych** - serdecznie zapraszamy do współpracy. Prosimy także o dane kontaktowe do osoby, z którą można kontaktować się w temacie oferty firmy - Jej głos przyda się przy tworzeniu artykułów dot. konkretnych grup usług lub produktów.
+
+@component('mail::button', ['url' => 'https://docs.google.com/forms/d/e/1FAIpQLSem6zookmmZIfdVwzz82R8SAFKk3um9CLCBplPgf06n8lqG3w/viewform'])
+Dodaj Ekspertów
+@endcomponent
+
+@endcomponent
+
+Przy okazji dziękujemy za wszystkie sugestie dot. rozwoju [ehandel.com.pl](https://ehandel.com.pl/).
+
+Od momentu poprzedniej wiadomości, **do grona Autorów dołączyło kilkanaście nowych marek.**
+
+Przypominamy, że publikacja jest bezpłatna. Zachęcamy do nadsyłania materiałów na **redakcja@ehandel.com.pl** (lub przekazania adresu działowi/agencji PR żeby dodał nas do swojej listy wysyłkowej)
+
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+
+@endcomponent
