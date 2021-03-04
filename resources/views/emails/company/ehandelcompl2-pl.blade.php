0947e22a9f04208d339fbbc162ef9f2fa9b1cf64
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-03-04 14:19:22 +0100

    fix

--- resources/views/emails/company/ehandelcompl2-pl.blade.php
+++ resources/views/emails/company/ehandelcompl2-pl.blade.php
@@ -10,7 +10,7 @@ Po bardzo udanym starcie [ehandel.com.pl](https://ehandel.com.pl/) (kilkanaście
 
 Jeśli w Twojej firmie znajdują się osoby chętne do **okresowego komentowania trendów rynkowych** - serdecznie zapraszamy do współpracy. 
 
-Prosimy także o dane kontaktowe do osoby, z którą można kontaktować się w temacie oferty firmy - Jej głos przyda się przy tworzeniu artykułów dot. konkretnych grup usług lub produktów.
+Prosimy także o dane kontaktowe do osoby, z którą można kontaktować się w temacie oferty firmy - wykorzystamy Jej głos przy tworzeniu artykułów dot. konkretnych grup usług lub produktów.
 
 @component('mail::button', ['url' => 'https://docs.google.com/forms/d/e/1FAIpQLSem6zookmmZIfdVwzz82R8SAFKk3um9CLCBplPgf06n8lqG3w/viewform'])
 Dodaj Ekspertów
@@ -18,11 +18,9 @@ Dodaj Ekspertów
 
 @endcomponent
 
-Dziękujemy za wszystkie wstępne sugestie dotyczące działania [ehandel.com.pl](https://ehandel.com.pl/). Kolejne sugestie dotyczące rozwoju i atrakcyjności tej platformy z Waszego punktu widzenia można wysłać przez [ankietę](https://docs.google.com/forms/d/e/1FAIpQLSem6zookmmZIfdVwzz82R8SAFKk3um9CLCBplPgf06n8lqG3w/viewform).
+Dziękujemy za wszystkie wstępne sugestie dotyczące działania [ehandel.com.pl](https://ehandel.com.pl/). Kolejne **sugestie dotyczące kierunku rozwoju** i atrakcyjności tej platformy dla dostawców usług i produktów dla e-commmerce można wysłać przez [ankietę](https://docs.google.com/forms/d/e/1FAIpQLSem6zookmmZIfdVwzz82R8SAFKk3um9CLCBplPgf06n8lqG3w/viewform).
 
-Od momentu wysłania poprzedniej wiadomości, **do grona Autorów dołączyło kilkanaście nowych marek.**
-
-Przypominamy, że publikacja jest bezpłatna. Zachęcamy do nadsyłania materiałów na **redakcja@ehandel.com.pl** (lub przekazania adresu działowi/agencji PR żeby dodał nas do swojej listy wysyłkowej)
+Przypominamy, że **publikacja jest bezpłatna.** Zachęcamy do nadsyłania materiałów na **redakcja@ehandel.com.pl** (lub przekazania adresu działowi/agencji PR żeby dodał nas do swojej listy wysyłkowej)
 
 
 Z pozdrowieniami,
