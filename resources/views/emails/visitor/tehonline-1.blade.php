c8fd6aa20d58a149978666e0d44f5521823658ce
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-03-18 16:01:27 +0100

    mailing

---
+++ resources/views/emails/visitor/tehonline-1.blade.php
@@ -0,0 +1,46 @@
+
+
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+Minęło już ponad 15 miesięcy od ostatnich Targów eHandlu, które mieliśmy przyjemność dla Ciebie organizować.
+
+Czas pandemii inwestujemy w uruchomienie lub reaktywację projektów stricte internetowych. Wznowiliśmy działanie [portalu informacyjnego https://ehandel.com.pl/](https://ehandel.com.pl/) - już teraz znajdziesz na nim szereg interesujących treści.
+
+Nie zapominamy jednak o społeczności zgromadzonej wokół Targów eHandlu! 
+
+### Zaapraszamy na wirtualną konferencję, która odbędzie się 24 marca na targiehandlu.pl, podczas której będziemy transmitowali aż 22 wystąpienia na 4 wirtualnych scenach.
+
+<a href="https://targiehandlu.pl" target="_blank"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650,f_auto/v1616077249/tehonline_fpeventcover.jpg" style="max-width: 650px;" alt="prelegenci Teh online"/></a>
+
+Jeśli wydarzenie się spodoba być może uda nam się je realizować okresowo - aż do momentu jak będzie możliwe organizowanie Targów fizycznych.
+
+Wirtualna konferencja Targów eHandlu **będzie trwała mniej niż 3 godziny** - i w każdym momencie będziesz mógł/mogła przeskoczyć na 1 z 4 wirtualnych scen.
+
+Równolegle do transmisji będziemy mogli rozmawiać z innymi uczestnikami a przede wszystkim z prelegentami na serwerze Discorda.
+
+## Wśród zaplanowanych wystąpień znajdziesz między innymi takie tematy jak:
+
+SEO rok później, czyli sprawdzamy czy nowoczesna strategia pozycjonowania sklepu prezentowana w 2019 roku ma sens i przynosi fajny wynik.
+
+Jak nie zbankrutować na e-Commerce
+
+8 testów A/B, które pomogą Ci pozyskiwać więcej i lepsze opinie od klientów.
+
+Jak zachęcać do zapisu na newsletter? Szybko i konkretnie o skutecznych lead magnetach.
+
+Recepta na sukces w e-commerce. 5 strategii dla e-commerce managerów
+
+Zwiększenie sprzedaży dzięki automatyzacji cen w sklepie.
+
+Segmentacja użytkowników + machine learning = wygrana w Google Ads! Na przykładzie Showroom.pl
+
+### [Rejestracja na targiehandlu.pl](https://targiehandlu.pl)
+
+Do usłyszenia!
+
+@endcomponent
+
+
+
