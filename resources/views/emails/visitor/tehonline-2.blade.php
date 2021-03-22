484f2077cfc67a48d7aa2cc0b7a0a718acab7d47
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-03-22 14:40:43 +0100

    fix

--- resources/views/emails/visitor/tehonline-2.blade.php
+++ resources/views/emails/visitor/tehonline-2.blade.php
@@ -6,56 +6,59 @@
 
 **Już w środę pierwsza eKonferencja Targów eHandlu** - zostało jeszcze ok 620 wirtualnych miejsc.
 
-(link do rezerwacji poniżej)
+(link do odebrania biletu poniżej)
 
-## Na 4 wirtualnych scenach zaprezentuje się aż 22 Ekspertów eHandlu - m.in.
+# 22 Ekspertów na 4 wirtualnych scenach
 
-**AtomStore** - "Tego nie dowiesz się z Google, czyli o otwieraniu sklepu internetowego za granicą"
+M.in.
 
-**@e-com.house** i **KUBOTA** - _"KUBOTA reaktywacja! Czyli jak w e-commerce ożywić i skalować markę z historią"_
+💥 **AtomStore** - "Tego nie dowiesz się z Google, czyli o otwieraniu sklepu internetowego za granicą"
 
-**Sempai** - _"Segmentacja użytkowników + machine learning = wygrana w Google Ads! Na przykładzie Showroom.pl"_
+💥 **@e-com.house** i **KUBOTA** - _"KUBOTA reaktywacja! Czyli jak w e-commerce ożywić i skalować markę z historią"_
 
-**Futuriti** - _"…acja" w eCommerce. Automatyzacja, integracja, optymalizacja. 100% praktyki."_
+💥 **Sempai** - _"Segmentacja użytkowników + machine learning = wygrana w Google Ads! Na przykładzie Showroom.pl"_
 
-**GetResponse** - _"Jak zachęcać do zapisu na newsletter? Szybko i konkretnie o skutecznych lead magnetach."_
+💥 **Futuriti** - _"…acja" w eCommerce. Automatyzacja, integracja, optymalizacja. 100% praktyki."_
 
-Ireneusz Klimczak Content Project Manager @ 
+💥 **GetResponse** - _"Jak zachęcać do zapisu na newsletter? Szybko i konkretnie o skutecznych lead magnetach."_
 
 
-@component('mail::button', ['url' => "https://teh.com.pl"])
-Zarezerwuj 1 miejsce na eKonferencji Targów eHandlu
+@component('mail::button', ['url' => "http://teh.com.pl/virtual"])
+Odbierz bilet na 1 miejsce na eKonferencję
 @endcomponent
 
 
-## Oto marki, których reprezentacji podzielą się swoją wiedzą
+# Wszystkie marki, których Przedstawiciele podzielą się swoją wiedzą
 
 <a href="https://teh.com.pl" target="_blank"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1616417465/tehonline_brands.png" style="max-width: 650px;" alt="firmy prelegentów Teh online"/></a>
 
+# ... kolejna eKonferencja?
 
-## Przygotowując naszą pilotażową eKonferencję trzymaliśmy się kilku założen:
+Mamy nadzieję, że dostarczymy coś unikatowego. Przygotowując naszą pilotażową eKonferencję trzymaliśmy się kilku założen:
 
-- chcemy by wydarzenie trwało nie więcej niż **3 godziny** (większa dawka wiedzy byłaby mało efektywna a być może dla wielu męcząca)
+💥 chcemy by wydarzenie trwało nie więcej niż **3 godziny** (większa dawka wiedzy byłaby mało efektywna a być może dla wielu męcząca)
 
-- chcemy by w każdym z **6 slotów agendy** można było znaleźć interesującą prezentację (w każdym slocie odbywają się 4 prezentacje równolegle i przełączanie jest płynne)
+💥 chcemy by w każdym z **6 slotów agendy** można było znaleźć interesującą prezentację (w każdym slocie odbywają się 4 prezentacje równolegle i przełączanie jest płynne)
 
-- chcemy by każdy kto potrzebuje więcej wiedzy mógł **równolegle do prezentacji porozmawiać na czacie z Prelegentem**
+💥 chcemy by każdy kto potrzebuje więcej wiedzy mógł **równolegle do prezentacji porozmawiać na czacie z Prelegentem**
 
+@component('mail::button', ['url' => "http://teh.com.pl/virtual"])
+Zarezerwuj miejsce na eKonferencji Targów eHandlu
+@endcomponent
 
 <a href="https://teh.com.pl" target="_blank"><img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fit,w_650,f_auto/v1616077249/tehonline_fpeventcover.jpg" style="max-width: 650px;" alt="prelegenci Teh online"/></a>
 
 
-@component('mail::button', ['url' => "https://targiehandlu.pl?utm_source=visitors&utm_campaign=tehonline1&utm_source=email"])
-Zarezerwuj miejsce na eKonferencji Targów eHandlu
-@endcomponent
-
-
 **Jeśli wydarzenie się spodoba, być może będziemy je realizować okresowo - aż do momentu jak będzie możliwe organizowanie Targów fizycznych.**
-
 
 Do usłyszenia!
 
 Karolina Michalak
+
+@component('mail::button', ['url' => "http://teh.com.pl/virtual"])
+Zarezerwuj miejsce na eKonferencji Targów eHandlu
+@endcomponent
+
 
 ## [Zarządzaj wysyłką]({{ $unsubscribe }}) 
 
