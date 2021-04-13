0e178f62f412412efa9d05a909d717c4dfeccd2d
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-04-13 14:32:59 +0200

    fix

--- resources/views/emails/company/teh18-teh20-pl.blade.php
+++ resources/views/emails/company/teh18-teh20-pl.blade.php
@@ -8,16 +8,16 @@ przesyłamy 2 ważne informacje.
 
 (1)
 
-z uwagi na obecną sytuację dotyczącą przebiegu epidemii COVID-19, postanowiliśmy **przełożyć czerwcowe** Targi eHandlu na **20 października.** Nie pozostaje nam nic innego jak wierzyć, że do tego czasu większość potencjalnych uczestników Targów będzie miała możliwość przyjęcia szczepionki i będzie mogła uczestniczyć (nie narażając siebie i innych) w wydarzeniu o skali jaką pamiętamy z 2019 roku.
+z uwagi na obecną sytuację związaną z przebiem epidemii COVID-19 w Polsce, postanowiliśmy **przełożyć czerwcowe** Targi eHandlu na **20 października 2021**. Nie pozostaje nam nic innego jak wierzyć, że do tego czasu większość potencjalnych uczestników Targów będzie miała możliwość przyjęcia szczepionki i będzie mogła uczestniczyć (nie narażając siebie i innych) w wydarzeniu o skali jaką pamiętamy z 2019 roku.
 
 Podpisane porozumienia i ich postanowienia pozostają w mocy. 
 
 
 (2) 
 
-**9 czerwca** planujemy organizację dużego wydarzenia ONLINE. Zorganizowana - z sukcesem -  24 marca Konferencja Online Targów eHandlu dała nam dużo doświadczeń w tym temacie.
+**9 czerwca** planujemy organizację dużego wydarzenia ONLINE. Zorganizowana - z sukcesem - 24 marca Konferencja Online Targów eHandlu dała nam dużo doświadczeń w tym temacie.
 
-**Do końca tego tygodnia wystartuje sprzedaż świadczeń** związana z w/w wydarzeniem. Pierwszeństwo w ich zakupie będą miały firmy, które zdecydowały się na przeniesienie targowego budżetu.
+**Do końca tego tygodnia wystartuje sprzedaż świadczeń** związana z w/w wydarzeniem.  Pierwszeństwo w ich zakupie będą miały firmy, które zdecydowały się na przeniesienie targowego budżetu.
 
 
 Dziękujemy za wsparcie i wyrozumiałość.
