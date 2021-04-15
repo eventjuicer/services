8c5db2667c1765a3917301b82b43a85f4178e0e8
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-04-15 12:58:22 +0200

    fixed mailing

--- resources/views/emails/company/ekongres-earlybird-pl.blade.php
+++ resources/views/emails/company/ekongres-earlybird-pl.blade.php
@@ -4,19 +4,19 @@
  
 # Dzień dobry {{ array_get($profile, "fname") }},
 
-## Jutro (16 kwietnia) o 10:00 startuje przedsprzedaż slotów prezentacyjnych na Kongres eHandlu Online, który będzie transmitowany online 9 czerwca na https://live.targiehandlu.pl z wcześniej przygotowanych materiałów video.
+## Już jutro (piątek, 16 kwietnia) - o 10:00 - startuje przedsprzedaż slotów prezentacyjnych na Kongres eHandlu Online, który będzie transmitowany online 9 czerwca na https://live.targiehandlu.pl z wcześniej przygotowanych materiałów video.
 
 Przewidzieliśmy **3 typy uczestnictwa**: 
 
-- Udział standardowy (prezentacja 15 minut)
+👍 Udział standardowy (prezentacja 15 minut)
 
-- Sponsor wirtualnej sceny (prezentacja 25 minut + dodatkowy pakiet korzyści)
+❤️ Sponsor wirtualnej sceny (prezentacja 25 minut + dodatkowy pakiet korzyści)
 
-- Partner główny Kongresu (prezentacja 25 minut, równolegle na 3 scenach + dodatkowy ultra-pakiet korzyści)
+🔥 Partner główny Kongresu (prezentacja 25 minut, równolegle na 3 scenach + dodatkowy ultra-pakiet korzyści)
 
 oraz **2 możliwości zrealizowania video** (co w głównej mierze podyktowane jest sytuacją w związku z COVID-19): 
 
-- nagranie we własnym zakresie (np u siebie w firmie)
+- nagranie we własnym zakresie (np u siebie w firmie lub w domu)
 
 - zorganizowanie nagrania w studio w Warszawie (dodatkowo płatne)
 
@@ -24,7 +24,7 @@ oraz **2 możliwości zrealizowania video** (co w głównej mierze podyktowane j
 Szczegóły na live.targiehandlu.pl
 @endcomponent
 
-**Jeśli otrzymujesz tą wiadomość, oznacza to, że Twoja firma bierze udział w przedsprzedaży. Od poniedziałku (19 kwietnia) oferta będzie publiczna.**
+**Jeśli otrzymujesz tę wiadomość, oznacza to, że Twoja firma bierze udział w przedsprzedaży. Od poniedziałku (19 kwietnia) oferta będzie publiczna.**
 
 
 Z pozdrowieniami,
