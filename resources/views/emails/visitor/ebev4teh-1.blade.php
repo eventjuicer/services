a44e579e2ce18842803aaa2215fd8383c9110293
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-05-25 14:01:29 +0200

    ebev4teh

---
+++ resources/views/emails/visitor/ebev4teh-1.blade.php
@@ -0,0 +1,77 @@
+
+
+@component('mail::message')
+
+# Cześć {{ $p->translate("[[fname]]") }}!
+
+**Już jutro o 9:00 startuje nasze siostrzane wydarzenie - 2 dniowa eKonferencja E-commerce Berlin Virtual**
+
+(link do pobrania biletu poniżej)
+
+Co Cię czeka?
+
+**29 Prelegentów**
+
+**2 dni (26 maja - środa i 27 maja - czwartek)**
+
+**2 wirtualne sceny każdego dnia**
+
+**90% prezentacji w języku angielskim**
+
+**Topowe marki: 💥Lowell, 💥Henkel, 💥HUGO BOSS, 💥Douglas, 💥Mercedes-Benz, 💥Shopware, 💥Criteo
+
+
+<a href="https://virtual.ecommerceberlin.com/stages/a" target="_blank"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1621943431/ebe_standby.jpg" style="max-width: 650px;" alt="odliczanie"/></a>
+
+Wybrane tematy:
+
+💥The Greta effect in retail – How to reach a new generation of conscious shoppers: Insights from merchants
+
+
+💥Up to 21% Conversion Rate Uplift: How personalization can help
+
+💥55 Marketing Mistakes we made since launching our period underwear brand KORA MIKINO.
+
+💥Future UX: Designing dynamic experiences with AI-driven design
+
+💥Marketing in China
+
+💥Growing D2C international e-commerce and expanding into new markets: The successful expansion of HUGO BOSS
+
+💥Global SEO: Think Global, Act Local
+
+💥Operational Excellence
+
+💥What is sustainable E-commerce according to consumers?
+
+💥How much revenue are you losing to PSD2 and how can you avoid it?
+
+💥From Zero to the Top: the E-commerce App for Connected Services Will Soon Emerge to Become a Scalable Marketplace for Mercedes-Benz
+
+💥„Artificial Intelligence“ - The customer journey doesn´t stop beyond dunning
+
+💥Expand product catalogue by opening PIM to your partners using PimCore
+
+💥The purpose of Cloud for your future
+
+💥Emotional is the new digital - how to sell products online in times of Covid-19
+
+💥Becoming the leading beauty platform with a marketplace model
+
+💥It's a Seller's Market: The Top Questions Sellers Should be Asking Buyers before Signing an LOI
+
+
+Do usłyszenia!
+
+Team Targów eHandlu
+
+@component('mail::button', ['url' => "https://virtual.ecommerceberlin.com/stages/a"])
+Zarezerwuj miejsce na E-commerce Berlin Virtual
+@endcomponent
+
+## [Zarządzaj wysyłką]({{ $unsubscribe }}) 
+
+@endcomponent
+
+
+
