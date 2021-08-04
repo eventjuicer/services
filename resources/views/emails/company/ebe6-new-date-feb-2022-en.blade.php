d107d3cfd955ec066e4d5642d00a01205c1d68dc
jprzybylski4 <infoguru@infogurus-MacBook-Air.local>
2021-08-04 18:24:39 +0200

    New date announcement emails
    E-mail subjects:
    ENG: E-commerce Berlin Expo 2022 - new date announced!
    
    DE: E-commerce Berlin Expo 2022 - das neue Datum ist bekannt!

---
+++ resources/views/emails/company/ebe6-new-date-feb-2022-en.blade.php
@@ -0,0 +1,31 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+We are happy to announce the new date of the E-commerce Berlin Expo 2022! After almost two years without the physical events we are finally **coming back to Berlin on February 9th 2022**. 
+
+We fully understand how much you have missed the physical events and what an incomparable value the face to face interactions have. Therefore we are already working on organizing the event in its original, offline format, just like many major events across Europe do.
+
+We always prioritize the health and safety of our attendees and partners above all else. We have been in constant contact with partners, authorities, and other relevant parties since the current Coronavirus outbreak in Europe and we can ensure that the event will be aligned with all safety regulations.
+
+We thank you for your overwhelming support over the last months and wish you all the best in the upcoming time. 
+
+If you have any questions regarding the event feel free to contact us.
+We look forward to welcoming you back to the exhibition venue! 
+
+
+Best regards,
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/v1606743599/images/podpisy_mt_psz.png" alt="signatures" style="max-width: 250px;">
+
+Peter Szczepaniak & Mark Tomaszewski
+
+Managing Partners, E-commerce Berlin Expo
+
+
+@endcomponent
+
+
+
