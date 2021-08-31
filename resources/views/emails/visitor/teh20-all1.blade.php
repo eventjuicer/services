f54fe266e14166f6b68f81bba7e22dd3afa22f43
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-08-31 14:20:18 +0200

    Update teh20-all1.blade.php

--- resources/views/emails/visitor/teh20-all1.blade.php
+++ resources/views/emails/visitor/teh20-all1.blade.php
@@ -10,36 +10,45 @@ Po tak długim czasie od ostatniej - fizycznej - edycji Targów (Warszawa - 22 p
 
 Nie możemy się doczekać wejścia do hali targowej! :)
 
-Formuła jesiennych Targów eHandlu będzie jednak niezmienna (wydaje nam się optymalna): 
+Formuła jesiennych Targów eHandlu będzie jednak niezmienna (gdyż niezmiennie wydaje nam się optymalna): 
 
 - ok **120-130 Wystawców** (kończymy kompletować listę - **zostało ok 11 stoisk**)
 
-- aż **6 scen prezentacyjnych** (2 duże, 2 otwarte, 2 "studyjne" pod tematy szkoleniowe) w których będziemy realizowali różne formuły edukacji
+- aż **6 scen prezentacyjnych** (2 duże, 2 otwarte, 2 studyjne pod tematy szkoleniowe) w których będziemy realizowali różne formuły edukacji
 
 - **strefa VIP** (do której wejście będą mieli wybrani nie tylko Wystawcy i Prelegenci ale także wybrani przez nas Zwiedzający reprezentujący największych e-retailerów)
 
 To co możesz zrobić teraz:
 
-## Chcesz opowiedzieć o czym co jest interesujące dla właścicieli sklepów? Zgłoś się - to nic nie kosztuje. Propozycje wystąpień przyjmujemy wyłącznie do piątku.
+## Chcesz opowiedzieć o czymś co będzie wartościowe dla właścicieli sklepów internetowych? 
 
 @component('mail::button', ['url' => "https://targiehandlu.pl/speaking"])
 Wystąp / Zgłoś propozycję prezentacji
 @endcomponent
 
-## Jesteś dostawcą usług dla sprzedających w Internecie? Ostatnie przestrzenie wystawiennicze czekają na Ciebie.
+Zgłoś się (zgłoszenie jest bezpłatne). Propozycje wystąpień przyjmujemy wyłącznie do piątku.
+
+
+## Jesteś dostawcą usług dla sprzedających w Internecie? 
 
 @component('mail::button', ['url' => "https://targiehandlu.pl/exhibit"])
 Wystaw się / zobacz listę wystawców
 @endcomponent
 
+Ostatnie przestrzenie wystawiennicze czekają na Ciebie. Tym bliżej daty targów tym cena wyższa.
+
 
 <a href="https://targiehandlu.pl/exhibit"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1630408049/emails/teh20_bookingmap.png" alt="booking map" style="max-width: 600px;"></a>
 
-## Zarejestruj się jako Uczestnik (na ten moment nie planujemy wprowadzenia opłat za uczestnictwo lecz szczegółowe warunki - sanitarne - udziału będą znane bezpośrednio przed Targami)
+## Zarejestruj się jako Uczestnik
 
 @component('mail::button', ['url' => "https://targiehandlu.pl/visit"])
 Zarejestruj się jako Zwiedzający
 @endcomponent
+
+Na ten moment nie planujemy wprowadzenia opłat za uczestnictwo lecz szczegółowe warunki - sanitarne - udziału będą znane bezpośrednio przed Targami.
+
+
 
 
 <a href="https://targiehandlu.pl/speaking"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1630408054/emails/teh20_stage2.jpg" alt="stage" style="max-width: 600px;"></a>
