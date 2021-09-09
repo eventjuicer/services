e6cc505574f4c2e20ae15626a3a8cc815e13a6a0
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-09-09 14:53:41 +0200

    email

---
+++ resources/views/emails/visitor/teh20-all2.blade.php
@@ -0,0 +1,64 @@
+
+
+@component('mail::message')
+ 
+# Dzień dobry {{ $p->translate("[[fname]]") }},
+
+20 Targi eHandlu zbliżają się wielkimi krokami. Jaki stan na dzisiaj? Mamy już ponad 115 Wystawców (**23 nowe marki!**) i 24 Prelegentów (Eksperci z takich firm jak: **Gonito, AtomStore, Sempai, 'merce, GetResponse, shopware, IAI/IdoSell czy e-point**), działamy nad tematami logistycznymi, niedługo startujemy z promocją :)
+
+a teraz do rzeczy
+
+### Wystartowało głosowanie na tematy, które powinny znaleźć się w Agendzie
+
+Głosowanie zajmuje max 2 minuty (wymagane konto na LinkedIn). Masz 6 głosów (tak aby oddać 1 głos w każdej kategorii.)
+
+Czy możemy na Ciebie liczyć?
+
+
+<a href="https://targiehandlu.pl/exhibit"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1631191098/teh20_cfp.jpg" alt="call for papers voting" style="max-width: 600px;"></a>
+
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/vote"])
+Wybierz kto powinien wystąpić
+@endcomponent
+
+Przykładowe zgłoszenia, które walczą o miejsce w agendzie 20. Targów eHandlu:
+
+7 najczęstszych sposobów **przepalania budżetu w Google**
+
+Vlogmas, prezentownik, a może haul? Jak wykorzystać **potencjał influencerów w przedświątecznym szale zakupowym** - dane, praktyki, case studies.
+
+Case study w jak w 21 dni osiągnąć **status bestsellera na Kaufland.de**
+
+Nowy standard śledzenia - **Google Analytics 4**
+
+**Czy anonimowy użytkownik może stać się lojalnym klientem?**
+
+**BLIKNIJ za zakupy w Messengerze**, czyli jak robić social commerce i podnosić konwersję!
+
+Petarda w koszyku zakupowym, czyli **jak wygrać walkę o niezdecydowanych klientów?**
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/vote"])
+Wybierz kto powinien wystąpić
+@endcomponent
+
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
