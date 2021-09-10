7fca171d60c01d7d513612c78f619dcf0f42bd61
jprzybylski4 <infoguru@infogurus-MacBook-Air.local>
2021-09-10 15:44:14 +0200

    CFP emails
    Send as Marta Masternak
    
    ENG title:
    Call for Papers - become a speaker at the E-commerce Berlin Expo 2022!
    
    DE title:
    Call for Papers - Teile dein Präsentationsvorschlag mit uns!

---
+++ resources/views/emails/company/ebe6-newcfp-physical-reminder-de.blade.php
@@ -0,0 +1,52 @@
+
+
+@component('mail::message')
+ 
+# Hallo {{ array_get($profile, "fname") }},
+
+Unser E-commerce Berlin Expo - Call for Papers Wettbewerb wartet noch auf deine Einreichung! 
+
+Du kannst dein Präsentationsvorschlag uns immer noch zusenden und Speaker bei einem der bekanntesten E-Commerce-Events in Deutschland werden!
+
+# Warum auf der E-Commerce Berlin Expo sprechen?
+
+Mit einem Vortrag auf der Expo stellst du sicher, dass deine Marke für Innovation steht. 
+Schick uns dein Vorschlag zu und präsentiere vor mehr als **7.000 E-Commerce-Experten**, die nach der richtigen Lösung suchen, um ihr Geschäft zu verbessern.
+
+Mit einem Vortrag erhältst du den Zugang zu:
+
+> 3,500+ CEOs, Direktoren & Abteilungsleiter
+
+> Relevanten Besucher, von denen 65 % von Einzelhändlern und Marken stammen
+
+> Interessanten Folgegesprächen und somit potenzieller Kundenakquise
+
+**Wie funktioniert es?**
+
+Reiche deinen Präsentationsvorschlag bis zum **30. September 2021** über den untenstehenden Link ein und lass dich von den Besuchern und Beiratsmitgliedern in unsere Agenda wählen!
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking"])
+Submit a presentation proposal
+@endcomponent
+
+**Was suchen wir?**
+
+Wir suchen aufschlussreiche und fesselnde Case Studies, Geschichten oder Trendprognosen. Wir erwarten, dass die Inhalte für die Besucher unserer Expo, aber auch für E-Commerce-Profis, die sie später sehen, wertvoll und interessant sind.
+
+> Kannst du eine interessante Case Study voller Daten und umsetzbarer Erkenntnisse präsentieren?
+
+> Kennst du einige Markthacks, die euren Kunden oder Partnern geholfen haben, und kannst du diese teilen?
+
+> Hast du in deiner Branche etwas außergewöhnliches erreicht?
+
+Möchtest du die Marktprognose für die Zukunft präsentieren?
+
+
+Best regards,
+
+Marta Masternak
+
+@endcomponent
+
+
+
---
+++ resources/views/emails/company/ebe6-newcfp-physical-reminder-en.blade.php
@@ -0,0 +1,48 @@
+
+
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname") }},
+
+We would like to kindly remind you about the **E-commerce Berlin Expo - Call for Papers contest**. You can still submit your presentation proposal and become a speaker at one of the most recognizable e-commerce events in Germany!
+
+# Why speak at the E-commerce Berlin Expo?
+
+Speaking at the Expo ensures that your brand is synonymous with innovation. Present to **7,000+ e-commerce professionals** looking for the right solution to enhance their business.
+
+When you submit your speaking proposal, you gain access to:
+
+> 90% average session capacity
+
+> 3,500+ CEOs, Directors & Heads of Departments
+
+> Relevant visitors, 65% of whom come from retailers & brands
+
+**How does it work?** 
+
+Submit your presentation proposal by **September 30, 2021** by pressing the button below and be chosen by the visitors and advisory board members to join our agenda!
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking"])
+Submit a presentation proposal
+@endcomponent
+
+**What are we looking for?**
+
+We're seeking insightful and captivating client cases, stories, or trends’ forecasts. We expect the content to be valuable and interesting for the attendees of our Expo, but also for those e-commerce professionals who may see it afterward. 
+
+> Can you present an interesting client case, full of data and actionable insights?
+
+> Do you know, and could share, some market hacks that helped your clients or partners?
+
+> Have you achieved something extraordinary in your industry?
+
+> Would you like to present your market prediction for the future?
+
+Best regards,
+
+Marta Masternak
+
+@endcomponent
+
+
+
