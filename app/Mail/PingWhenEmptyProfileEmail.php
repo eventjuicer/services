b15a6d1f84d104ccb48d49b2692c12f6c68b5b59
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-04 00:15:47 +0200

    fix

--- app/Mail/PingWhenEmptyProfileEmail.php
+++ app/Mail/PingWhenEmptyProfileEmail.php
@@ -221,9 +221,9 @@ class PingWhenEmptyProfileEmail extends Mailable
             $domain = "targiehandlu.pl";
             $cc = "targiehandlu+auto@targiehandlu.pl";
 
-            if($this->viewlang === "en"){
+            if($this->lang === "en"){
                 app()->setLocale("en");
-                config(["app.name" => "E-commerce Cracow Expo"]);
+                config(["app.name" => "E-commerce Warsaw Expo"]);
             }
             else
             {
@@ -286,6 +286,6 @@ class PingWhenEmptyProfileEmail extends Mailable
 
         $this->subject( $this->getSetting("subject") );
 
-        return $this->markdown('emails.company.ebe5-badprofile-' . $this->lang);
+        return $this->markdown('emails.company.badprofile2-' . $this->lang);
     }
 }
