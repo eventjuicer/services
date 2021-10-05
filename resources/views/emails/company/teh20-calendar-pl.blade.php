7273ba2fd12072a726db9d57d20a77f13252f2dc
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-05 15:19:17 +0200

    Update teh20-calendar-pl.blade.php

--- resources/views/emails/company/teh20-calendar-pl.blade.php
+++ resources/views/emails/company/teh20-calendar-pl.blade.php
@@ -8,9 +8,9 @@ Oto lista tematów, z którymi będziemy się do Ciebie zwracać w najbliższych
 
 **Środa** (6 października) - wyślemy Tobie **kompleksowe informacje logistyczne**
 
-**Środa** (6 października) - to będzie idealny czas na dodanie przedstawicieli Wystawcy, którzy będą na stoisku (uruchomimy tę opcję w Panelu Wystawcy)
+**Środa** (6 października) - to będzie idealny czas na **dodanie przedstawicieli Wystawcy**, którzy będą na stoisku (uruchomimy tę opcję w Panelu Wystawcy)
 
-**Czwartek** (7 października) - udostępnimy moduł przeglądania zarejestrowanych uczestników i zapraszania ich na spotkanie przy stoisku 
+**Czwartek** (7 października) - udostępnimy moduł **przeglądania zarejestrowanych uczestników i zapraszania ich na spotkanie przy stoisku** 
 
 **Poniedziałek** (11 października) - przyznamy nagrodę "Twój logotyp na identyfikatorach zwiedzających"
 
