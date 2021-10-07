0abcef507fbe548f6cd39d8ed38f8ccdcbb87447
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-07 15:13:03 +0200

    email to visitors re agenda

---
+++ resources/views/emails/visitor/teh20-all-agenda.blade.php
@@ -0,0 +1,57 @@
+
+
+@component('mail::message')
+ 
+# Dzień dobry {{ $p->translate("[[fname]]") }},
+
+20 Targi eHandlu zbliżają się wielkimi krokami (przypominamy: **20 października, Warszawa**). 
+
+Dziękujemy wszystkim, którzy zaangażowali się w wybór kolejnej partii prelekcji - **łącznie wystąpią aż 52 osoby!**
+
+### Dzisiaj przedstawiamy wybrane tematy 2 z 6 scen.
+
+**InPost Fulfillment**
+
+**Wysyłka do Czech, Słowacji, Węgier, Rumunii, Bułgarii, Grecji...**
+
+**Jak zwiększyć sprzedaż dzięki Ceneo**
+
+**Aukcje Real Time Bidding**
+
+**SXO czyli UX + SEO ...w generowaniu sprzedaży**
+
+**Maksymalizacja sprzedaży na Amazon**
+
+Nowe zasady naliczania **VAT w E-commerce**
+
+**Machine Learning w tantis.pl**
+
+**Progressive Web Apps - dostępny zawsze i wszędzie sklep internetowy**
+
+**7 najczęstszych sposobów przepalania budżetu w Google**
+
+Targi eHandlu to nie tylko aktualny stan wiedzy przekazywany przez Ekspertów. Targi eHandlu to także unikatowa  możliwość poznania prawie całej panoramy usług i produktów wspomagających prowadzenie skutecznej sprzedaży online odwiedzając stoiska wystawiennicze! **... a mamy już ponad 125 Wystawców (25 nowych marek!)** 
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/visit?fname=" . $p->translate("[[fname]]") . "&lname=" . $p->translate("[[lname]]") . "&cname2=" . $p->translate("[[cname2]]")]) . "&position=" . $p->translate("[[position]]")
+Odbierz wejściówkę na Targi za 0 pln.
+@endcomponent
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
