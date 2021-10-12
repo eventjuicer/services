53174badbd1a47a4c58a031d4beb67bd319852b9
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-12 12:20:05 +0200

    Create teh20-all-agenda2.blade.php

---
+++ resources/views/emails/visitor/teh20-all-agenda2.blade.php
@@ -0,0 +1,69 @@
+
+
+@component('mail::message')
+ 
+# Dzień dobry {{ $p->translate("[[fname]]") }},
+
+20 Targi eHandlu zbliżają się wielkimi krokami (przypominamy: **20 października, Warszawa**). 
+
+## Dzisiaj przedstawiamy kolejne tematy 2 z 6 scen.
+
+
+**Gonito** - Rydwany ognia, czyli jak wykorzystać synergię modeli **Seller i Vendor w serwisie Amazon.**
+
+Zaufane.pl - **Czy opinie naprawdę sprzedają?** Prezentacja wyników [prawdodpobnie] pierwszego i najbardziej rzetelnego badania w Polsce.
+
+'merce - Jakie są biznesowe zalety i możliwości rozwiązań **headless commerce**
+
+**PayPo** - Zwiększaj sprzedaż z PayPo
+
+**IAI / IdoSell** - Co pomaga uruchomić sklep szybko i tanio, a w jakim przypadku wdrożenie przekroczy budżet i się nie uda? **Doświadczenia z wdrożeń 10000 sklepów internetowych.**
+
+Sky-Shop - Bitwa o rynek. **Sklep internetowy vs. marketplace.**
+
+GoGlobal24 - **Czy warto sprzedawać do UK?**
+
+iCEA - Zapytaj o ... SXO!!!
+
+IDEA commerce - **Automatyzacja obsługi zamówień** w eCommerce
+
+Webmetric - **UX/CRO w e-commerce**, czyli jak przepalić budżet reklamowy
+
+Macopedia - Dlaczego system **Akeneo PIM** to "must have" dla sklepów e-commerce?
+
+**GetResponse** - Czy email marketing może jeszcze czymś zaskoczyć?
+
+TakeDrop - **DropShipping** - czy ten model jeszcze działa?
+
+**sembot.com** - Ekstremalnie skuteczne reklamy produktowe
+
+Global Sellers - Jak skutecznie wykorzystać modele **FBA i FBM w sprzedaży na Amazonie** by odnieść sukces.
+
+**Playlink** - Nowoczesny mailing reklamowy w służbie konwersji dla ecommerce.
+
+## ...a to wszystko od czołowych w Polsce Ekspertów e-commerce!
+
+Targi eHandlu to nie tylko aktualny stan wiedzy przekazywany przez Ekspertów. Targi eHandlu to także **unikatowa  możliwość poznania prawie całej panoramy usług i produktów** wspomagających prowadzenie skutecznej sprzedaży online odwiedzając stoiska wystawiennicze! **... a mamy już ponad 125 Wystawców (25 nowych marek!)** 
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/visit?fname=" . $p->translate("[[fname]]") . "&lname=" . $p->translate("[[lname]]") . "&cname2=" . $p->translate("[[cname2]]") . "&position=" . $p->translate("[[position]]")."&email=".$p->translate("[[email]]")])
+Zarejestruj się (za 0pln!)
+@endcomponent
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
