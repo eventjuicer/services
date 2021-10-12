6e7284a6ea58fb0979974f13a5b6c7fb29956c7a
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-12 12:42:47 +0200

    Update teh20-all-agenda2.blade.php

--- resources/views/emails/visitor/teh20-all-agenda2.blade.php
+++ resources/views/emails/visitor/teh20-all-agenda2.blade.php
@@ -9,7 +9,7 @@
 # 20 października, Warszawa, EXPO XXI 
 
 
-@component('mail::button', ['url' => "https://targiehandlu.pl/visit?fname=" . $p->translate("[[fname]]") . "&lname=" . $p->translate("[[lname]]") . "&cname2=" . $p->translate("[[cname2]]") . "&position=" . $p->translate("[[position]]")."&email=".$p->translate("[[email]]")])
+@component('mail::button', ['url' => "https://targiehandlu.pl/schedule?fname=" . $p->translate("[[fname]]") . "&lname=" . $p->translate("[[lname]]") . "&cname2=" . $p->translate("[[cname2]]") . "&position=" . $p->translate("[[position]]")."&email=".$p->translate("[[email]]")])
 Agenda + Rejestracja (0 pln)
 @endcomponent
 
@@ -19,32 +19,32 @@ Agenda + Rejestracja (0 pln)
 
 **Gonito** - Rydwany ognia, czyli jak wykorzystać synergię modeli **Seller i Vendor w serwisie Amazon.**
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_109079_logotype,w_600,h_250,x_0,y_220/v1631623883/p_109079_avatar.png" style="max-width: 500px;" alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_109079_logotype,w_600,h_250,x_0,y_220/v1631623883/p_109079_avatar.png" style="max-width: 500px;" alt="" />
 
 <hr />
 
 **Zaufane.pl** - **Czy opinie naprawdę sprzedają?** Prezentacja wyników [prawdodpobnie] pierwszego i najbardziej rzetelnego badania w Polsce.
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126988_logotype,w_600,h_250,x_0,y_220/v1632130975/p_126988_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126988_logotype,w_600,h_250,x_0,y_220/v1632130975/p_126988_avatar.png" style="max-width: 500px;"  alt="" />
 
 <hr />
 
 **'merce** - Jakie są biznesowe zalety i możliwości rozwiązań **headless commerce**
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_111899_logotype,w_600,h_250,x_0,y_220/v1598424698/p_111899_avatar.png"style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_111899_logotype,w_600,h_250,x_0,y_220/v1598424698/p_111899_avatar.png"style="max-width: 500px;"  alt="" />
 
 <hr />
 
 **PayPo** - Zwiększaj sprzedaż z PayPo
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126125_logotype,w_600,h_250,x_0,y_220/v1633330290/p_126125_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126125_logotype,w_600,h_250,x_0,y_220/v1633330290/p_126125_avatar.png" style="max-width: 500px;"  alt="" />
 
 <hr />
 
 
 **IAI / IdoSell** - Co pomaga uruchomić sklep szybko i tanio, a w jakim przypadku wdrożenie przekroczy budżet i się nie uda? **Doświadczenia z wdrożeń 10000 sklepów internetowych.**
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_112079_logotype,w_600,h_250,x_0,y_220/v1626765727/p_112079_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_112079_logotype,w_600,h_250,x_0,y_220/v1626765727/p_112079_avatar.png" style="max-width: 500px;"  alt="" />
 
 <hr />
 
@@ -53,34 +53,35 @@ Agenda + Rejestracja (0 pln)
 Zarejestruj się (za 0pln!)
 @endcomponent
 
+<hr />
 
 **Sky-Shop** - Bitwa o rynek. **Sklep internetowy vs. marketplace.**
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126092_logotype,w_600,h_250,x_0,y_220/v1633414805/p_126092_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126092_logotype,w_600,h_250,x_0,y_220/v1633414805/p_126092_avatar.png" style="max-width: 500px;"  alt="" />
 
 <hr />
 
 **GoGlobal24** - **Czy warto sprzedawać do UK?**
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126996_logotype,w_600,h_250,x_0,y_220/v1633585388/p_126996_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126996_logotype,w_600,h_250,x_0,y_220/v1633585388/p_126996_avatar.png" style="max-width: 500px;"  alt="" />
 
 <hr />
 
 **iCEA** - Zapytaj o ... SXO!!!
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_127743_logotype,w_600,h_250,x_0,y_220/v1633418352/p_127743_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_127743_logotype,w_600,h_250,x_0,y_220/v1633418352/p_127743_avatar.png" style="max-width: 500px;"  alt="" />
 
 
 
 **IDEA commerce** - **Automatyzacja obsługi zamówień** w eCommerce
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126566_logotype,w_600,h_250,x_0,y_220/v1630675774/p_126566_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126566_logotype,w_600,h_250,x_0,y_220/v1630675774/p_126566_avatar.png" style="max-width: 500px;"  alt="" />
 
 <hr />
 
 **Webmetric** - **UX/CRO w e-commerce**, czyli jak przepalić budżet reklamowy
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126638_logotype,w_600,h_250,x_0,y_220/v1631083194/p_126638_avatar.png"style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126638_logotype,w_600,h_250,x_0,y_220/v1631083194/p_126638_avatar.png"style="max-width: 500px;"  alt="" />
 
 <hr />
 
@@ -89,43 +90,44 @@ Zarejestruj się (za 0pln!)
 Zarejestruj się (za 0pln!)
 @endcomponent
 
+<hr />
 
 **Macopedia** - Dlaczego system **Akeneo PIM** to "must have" dla sklepów e-commerce?
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_127854_logotype,w_600,h_250,x_0,y_220/v1633498827/p_127854_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_127854_logotype,w_600,h_250,x_0,y_220/v1633498827/p_127854_avatar.png" style="max-width: 500px;"  alt="" />
 
 <hr />
 
 **GetResponse** - Czy email marketing może jeszcze czymś zaskoczyć?
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_112091_logotype,w_600,h_250,x_0,y_220/v1580968202/p_112091_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_112091_logotype,w_600,h_250,x_0,y_220/v1580968202/p_112091_avatar.png" style="max-width: 500px;"  alt="" />
 
 
 <hr />
 
 **TakeDrop** - **DropShipping** - czy ten model jeszcze działa?
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126474_logotype,w_600,h_250,x_0,y_220/v1630653775/p_126474_avatar.png" style="max-width: 500px;" alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126474_logotype,w_600,h_250,x_0,y_220/v1630653775/p_126474_avatar.png" style="max-width: 500px;" alt="" />
 
 <hr />
 
 **sembot.com** - Ekstremalnie skuteczne reklamy produktowe
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126928_logotype,w_600,h_250,x_0,y_220/v1631866226/p_126928_avatar.png" style="max-width: 500px;"  alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126928_logotype,w_600,h_250,x_0,y_220/v1631866226/p_126928_avatar.png" style="max-width: 500px;"  alt="" />
 
 <hr />
 
 **Global Sellers** - Jak skutecznie wykorzystać modele **FBA i FBM w sprzedaży na Amazonie** by odnieść sukces.
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126460_logotype,w_600,h_250,x_0,y_220/v1630879193/p_126460_avatar.png" style="max-width: 500px;" alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_126460_logotype,w_600,h_250,x_0,y_220/v1630879193/p_126460_avatar.png" style="max-width: 500px;" alt="" />
 
 <hr />
 
 **Playlink** - Nowoczesny mailing reklamowy w służbie konwersji dla ecommerce.
 
-<img href="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_111974_logotype,w_600,h_250,x_0,y_220/v1632392570/p_111974_avatar.png" style="max-width: 500px;" alt="" />
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/c_fill,h_500,q_90,r_max,w_500/g_center,u_teh20_tmpl_spkr_sq1,x_0,y_200/c_fit,g_center,l_p_111974_logotype,w_600,h_250,x_0,y_220/v1632392570/p_111974_avatar.png" style="max-width: 500px;" alt="" />
 
-<hr />
+
 
 ## ...a to wszystko od czołowych w Polsce Ekspertów e-commerce!
 
