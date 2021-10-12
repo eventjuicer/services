85458b83f64798b5171eadacd3f864f1d4742bf2
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-12 12:28:44 +0200

    Update teh20-all-agenda2.blade.php

--- resources/views/emails/visitor/teh20-all-agenda2.blade.php
+++ resources/views/emails/visitor/teh20-all-agenda2.blade.php
@@ -4,40 +4,48 @@
  
 # Dzień dobry {{ $p->translate("[[fname]]") }},
 
-20 Targi eHandlu zbliżają się wielkimi krokami (przypominamy: **20 października, Warszawa**). 
+20 Targi eHandlu już w przyszłym tygodniu!
 
-## Dzisiaj przedstawiamy kolejne tematy 2 z 6 scen.
+# 20 października, Warszawa, EXPO XXI 
+
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/visit?fname=" . $p->translate("[[fname]]") . "&lname=" . $p->translate("[[lname]]") . "&cname2=" . $p->translate("[[cname2]]") . "&position=" . $p->translate("[[position]]")."&email=".$p->translate("[[email]]")])
+Agenda + Rejestracja (0 pln)
+@endcomponent
+
+
+## Dzisiaj przedstawiamy kolejne tematy, które pojawia się na 2 z 6 scen.
 
 
 **Gonito** - Rydwany ognia, czyli jak wykorzystać synergię modeli **Seller i Vendor w serwisie Amazon.**
 
-Zaufane.pl - **Czy opinie naprawdę sprzedają?** Prezentacja wyników [prawdodpobnie] pierwszego i najbardziej rzetelnego badania w Polsce.
+**Zaufane.pl** - **Czy opinie naprawdę sprzedają?** Prezentacja wyników [prawdodpobnie] pierwszego i najbardziej rzetelnego badania w Polsce.
 
-'merce - Jakie są biznesowe zalety i możliwości rozwiązań **headless commerce**
+**'merce** - Jakie są biznesowe zalety i możliwości rozwiązań **headless commerce**
 
 **PayPo** - Zwiększaj sprzedaż z PayPo
 
 **IAI / IdoSell** - Co pomaga uruchomić sklep szybko i tanio, a w jakim przypadku wdrożenie przekroczy budżet i się nie uda? **Doświadczenia z wdrożeń 10000 sklepów internetowych.**
 
-Sky-Shop - Bitwa o rynek. **Sklep internetowy vs. marketplace.**
+**Sky-Shop** - Bitwa o rynek. **Sklep internetowy vs. marketplace.**
 
-GoGlobal24 - **Czy warto sprzedawać do UK?**
+**GoGlobal24** - **Czy warto sprzedawać do UK?**
 
-iCEA - Zapytaj o ... SXO!!!
+**iCEA** - Zapytaj o ... SXO!!!
 
-IDEA commerce - **Automatyzacja obsługi zamówień** w eCommerce
+**IDEA commerce** - **Automatyzacja obsługi zamówień** w eCommerce
 
-Webmetric - **UX/CRO w e-commerce**, czyli jak przepalić budżet reklamowy
+**Webmetric** - **UX/CRO w e-commerce**, czyli jak przepalić budżet reklamowy
 
-Macopedia - Dlaczego system **Akeneo PIM** to "must have" dla sklepów e-commerce?
+**Macopedia** - Dlaczego system **Akeneo PIM** to "must have" dla sklepów e-commerce?
 
 **GetResponse** - Czy email marketing może jeszcze czymś zaskoczyć?
 
-TakeDrop - **DropShipping** - czy ten model jeszcze działa?
+**TakeDrop** - **DropShipping** - czy ten model jeszcze działa?
 
 **sembot.com** - Ekstremalnie skuteczne reklamy produktowe
 
-Global Sellers - Jak skutecznie wykorzystać modele **FBA i FBM w sprzedaży na Amazonie** by odnieść sukces.
+**Global Sellers** - Jak skutecznie wykorzystać modele **FBA i FBM w sprzedaży na Amazonie** by odnieść sukces.
 
 **Playlink** - Nowoczesny mailing reklamowy w służbie konwersji dla ecommerce.
 
