fffb3ae1125c383c850eea8bfadee6c791d13d10
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-14 14:18:32 +0200

    Update teh20-promo-pl.blade.php

--- resources/views/emails/company/teh20-promo-pl.blade.php
+++ resources/views/emails/company/teh20-promo-pl.blade.php
@@ -1,12 +1,9 @@
-
-
 @component('mail::message')
  
-# Cześć
+# Cześć {{ array_get($profile, "fname") }},
 
 @component('mail::panel')
-Przypominamy o tym, że dla każdego Wystawcy przygotowaliśmy materiały promocyjne i każdy Wystawca ma swój publiczny profil Targowy
-[{{ $trackingLink }}]({{ $trackingLink }}) pokazujący m.in. informacje o firmie i lokalizację stoiska w Hali targowej.
+Przypominamy o tym, że dla każdego Wystawcy przygotowaliśmy materiały promocyjne i każdy Wystawca ma swój publiczny profil Targowy [{{ $trackingLink }}]({{ $trackingLink }}) pokazujący m.in. informacje o firmie i lokalizację stoiska w Hali targowej.
 @endcomponent
 
 @component('mail::button', ['url' => "https://promo.targiehandlu.pl/companies/" .$company_id])
@@ -15,9 +12,7 @@ Materiały promocyjne
 
 Serdecznie zachęcamy do publikowania informacji o swoim udziale na kanałach społecznościowych, wysłania mailingu do kontrahentów. Zmiana nabytych przez okres pandemiczny przyzwyczajeń do form online i powrót do wiary w skuteczność formuły fizycznych targów to ogromna praca i mocno wierzymy, że nam Państwo w tym pomożecie.
 
-
-
-Regards, 
+Pozdrawiamy, 
 
 Karolina Michalak
 
