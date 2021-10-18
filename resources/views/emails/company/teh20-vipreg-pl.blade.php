f8209e88246ae489ec3aa324d03c730d16a09d8a
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-18 10:29:44 +0200

    vipreg2

--- resources/views/emails/company/teh20-vipreg-pl.blade.php
+++ resources/views/emails/company/teh20-vipreg-pl.blade.php
@@ -2,17 +2,19 @@
  
 # Cześć {{ array_get($profile, "fname") }},
 
+
+Link do rejestracji dla VIP dla Twoich Partnerów i Kontrahentów **Roześlij go a my nadamy tym Zwiedzającym status VIP (otrzymają specjalną opaską uprawniająca do wejścia do Strefy VIP)**
+
+[https://targiehandlu.pl/vip?referral=C{{ $company_id }}](https://targiehandlu.pl/vip?referral=C{{ $company_id }}) 
+
+
 @component('mail::panel')
 Podczas nadchodzącej edycji Targów **Strefa VIP będzie miejscem wyjątkowych spotkań** Prelegentów, Wystawców, Zwiedzających, którzy zaakceptowali zaproszenia Wystawców. 
 
 W strefie będzie można prowadzić biznesowe rozmowy w najbardziej sprzyjających warunkach, popijając przy tym dobrą herbatę lub kawę :)
 @endcomponent
 
-Link do rejestracji Zwiedzającego VIP
 
-[https://targiehandlu.pl/vip?referral=C{{ $company_id }}](https://targiehandlu.pl/vip?referral=C{{ $company_id }}) 
-
-**Wyślij go swoim Kontrahentom oraz Partnerom a my nadamy im status VIP (otrzymają specjalną opaską uprawniająca do wejścia do Strefy VIP)**
 
 Pozdrawiamy, 
 
