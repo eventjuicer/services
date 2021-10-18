4156a93641536afb3fe46a2c8269144675f00fec
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-18 14:39:07 +0200

    logistics final info

---
+++ resources/views/emails/company/teh20-logistics-final-pl.blade.php
@@ -0,0 +1,62 @@
+
+
+@component('mail::message')
+ 
+# Cześć
+
+# Oto przypomnienie podstawowych informacji logistycznych:
+
+**Wysłane paczki (jeśli były wysłane) znajdą się na Państwa stoisku podczas dnia montażowego czyli 19 października (we wtorek). Hala jest zamykana i ochraniana po wyjściu organizatora w dzień montażowy.**
+
+## Miejsce Targów
+
+Expo XXI, ul. Prądzyńskiego 12/14, 01-222 Warszawa *ta edycja będzie realizowana w HALI 1 (największej :-)*
+
+## Parkowanie
+
+Bilet parkingowy należy pobrać z parkomatu TYLKO PRZY PIERWSZYM WJEŹDZIE . 
+
+**Do bezpłatnego wyjazdu** i każdego kolejnego wjazdu/wyjazdu potrzebny będzie voucher, który dostaniecie Państwo od nas przy pierwszym wspólnym spotkaniu z nami (dzień montażowy lub rano - przed wydarzeniem).
+
+## Dzień montażowy (opcjonalny) - 19 października
+
+Godziny montażowe to 16:00 - 20:00 (o ile nie ustalono z nami inaczej). 
+
+Po wejściu na Halę #1 (wejście po lewej patrząc od wejścia głównego) należy udać się do punktu informacyjnego dla Wystawców (Strefa VIP - początek Hali #1), w którym będziemy czekać z "welcome-packiem" (identyfikatory, vouchery na catering, WIFI, karta parkingowa potrzebna do bezpłatnego wyjazdu)
+
+## Dzień Targowy - 20 października
+
+Montaż w dzień targowy możliwy jest tylko i wyłącznie w godzinach **8:00 - 9:30.** (O 10:00 hala zostanie otwarta do publicznego zwiedzania.)
+
+Jeśli nie spotkaliśmy się podczas dnia montażowego to po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców, w którym będziemy czekać z "welcome-packiem" (identyfikatory, vouchery na catering, WIFI, karta parkingowa potrzebna do bezpłatnego wyjazdu)
+
+**Demontaż** jest możliwy od 17:15. Hala będzie dostępna do 20:00 (o ile nie ustalono z nami inaczej).
+
+W razie wystąpienia jakichkolwiek problemów i wątpliwości związanych z Państwa stoiskiem (np z prądem) należy: 
+
+wysłać SMS na tel +48 721 945 134 z nr swojego stoiska i opisem problemu 
+
+LUB
+
+udać się do informacji dla wystawców w punkcie VIP.
+
+## Dodatkowe informacje i zalecenia
+
+** Prosimy o wzięcie własnych listew zasilających **
+
+** Prosimy pamiętać, że standardowe przyłącze prądu to tylko 300W** jeśli planujesz coś więcej niż korzystanie z 2 laptopów i ew. TV trzeba dokupić indywidualne przyłącze (Panel Wystawcy). Prosimy o kontakt jeśli masz wątpliwości.
+
+@component('mail::panel')
+
+UWAGA: W przypadku podklejania wykładzin we własnym zakresie (brak wykupienia stosownej usługi od nas) jedyną rekomendowaną opcją jest profesjonalna taśma dwustronna ETG firmy Imawex. Typowa (=marketowa) taśma dwustronna jest nie do usunięcia po wydarzeniu co wygeneruje dodatkowe (naprawdę kosmiczne) koszty sprzątania, które będziemy musieli refakturować.
+
+@endcomponent
+
+Regards, 
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+@endcomponent
+
