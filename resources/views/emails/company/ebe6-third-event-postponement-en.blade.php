413440c517894093dea870e03f4d7a424146e53b
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-12-07 19:14:03 +0100

    ebe6 emails

---
+++ resources/views/emails/company/ebe6-third-event-postponement-en.blade.php
@@ -0,0 +1,35 @@
+
+
+@component('mail::message')
+ 
+# Dear {{ array_get($profile, "fname") }},
+
+
+In the past few weeks, there has been a tremendous effort to contain the fourth wave of the COVID-19 pandemic. As of the 2nd December 2021, we were confident that the initial offline format of the event in 2022 would be successful.
+
+As the event organizers, we were prepared to comply with all the necessary standards for hygiene and safety. In order to ensure the safety of all participants, appropriate measures have been taken. Unfortunately, the new restrictions imposed by the German government to combat the COVID-19 epidemic of 2nd Dec 2021 drastically reduce the potential number of participants for this kind of event. 
+
+The risks associated with the organization of the event were thoroughly analyzed. Following consultation with the Berlin Convention Office and the venue operator, we made the only decision possible - **to reschedule the event for May 5, 2022**, to successfully implement the planned formula. **In addition, the services and obligations associated with your bookings will be carried over to the new date without additional charges.**
+
+Our decision was driven by the best interest of our clients and maximizing the event's ROI for them. 
+
+Based on the current limitations and the capacity of the STATION Berlin facility, the event could be held in a closed format for about only 1000-1500 guests. A low number would probably not allow for the appropriate return on investment for the event, and we recognize that this outcome may not be satisfactory for any of our clients. 
+
+We know the value of face-to-face meetings, and that virtual or hybrid meeting formats cannot replace live meetings. The E-commerce Berlin Expo is an ecosystem of projects for our fairs, the conference, the E-commerce Germany Awards, as well as an e-commerce professional meeting place. We are convinced that the postponed date of the event will allow us to meet in Berlin in May and take full advantage of what we have prepared for you.
+
+Thank you for your trust and support. We apologize for this out of our control situation and appreciate your understanding.
+
+If you have any questions please do not hesitate to contact your Account Manager.
+
+Yours faithfully,
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/v1606743599/images/podpisy_mt_psz.png" alt="signatures" style="max-width: 300px;">
+
+Peter Szczepaniak & Mark Tomaszewski
+
+Managing Partners, E-commerce Berlin Expo
+
+@endcomponent
+
+
+
