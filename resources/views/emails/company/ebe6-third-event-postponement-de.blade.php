070829384001d668da92112d1e95cd9fdc7f316b
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-12-08 12:27:20 +0100

    postonement email $30$

--- resources/views/emails/company/ebe6-third-event-postponement-de.blade.php
+++ resources/views/emails/company/ebe6-third-event-postponement-de.blade.php
@@ -4,17 +4,24 @@
 # Hallo {{ array_get($profile, "fname") }},
 
 
-In den letzten Wochen wurden weltweit enorme Anstrengungen unternommen, um die vierte Welle der COVID-19-Pandemie einzudämmen. Zum 2. Dezember 2021 waren wir zuversichtlich, dass die Veranstaltung im Februar 2022 im ursprünglichen, d.h. physischen Format, stattfinden würde. 
+In den letzten Wochen wurden weltweit enorme Anstrengungen unternommen, um die vierte Welle der COVID-19-Pandemie einzudämmen. 
+Zum 2. Dezember 2021 waren wir zuversichtlich, dass die Veranstaltung im Februar 2022 im ursprünglichen, d.h. physischen Format, stattfinden würde. 
 
-Als Veranstalter waren wir bereit, alle notwendigen Hygiene- und Sicherheitsstandards einzuhalten. Um die Sicherheit aller Teilnehmer zu gewährleisten, wurden entsprechende Maßnahmen getroffen. Die neuen Beschränkungen der Bundesregierung zur Bekämpfung der COVID-19-Pandemie vom 02.12.2021 reduzieren aber drastisch die potentiellen Teilnehmerzahlen für Veranstaltungen dieser Art.
+Als Veranstalter waren wir bereit, alle notwendigen Hygiene- und Sicherheitsstandards einzuhalten. 
+Um die Sicherheit aller Teilnehmer zu gewährleisten, wurden entsprechende Maßnahmen getroffen. 
+**Die neuen Beschränkungen der Bundesregierung zur Bekämpfung der COVID-19-Pandemie vom 02.12.2021 reduzieren aber drastisch die potentiellen Teilnehmerzahlen für Veranstaltungen dieser Art.**
 
-Die mit der Organisation der Veranstaltung verbundenen Risiken wurden gründlich analysiert. Nach Rücksprache mit dem Berlin Convention Office und dem Venue-Betreiber haben wir die einzig mögliche Entscheidung getroffen - die E-commerce Berlin Expo auf den **5. Mai 2022** zu verschieben, um die geplante Formel erfolgreich umzusetzen. **Darüber hinaus werden die mit euren Buchungen verbundenen Leistungen und Verpflichtungen ohne zusätzliche Kosten auf den neuen Termin übertragen.**
+Die mit der Organisation der Veranstaltung verbundenen Risiken wurden gründlich analysiert. 
+Nach Rücksprache mit dem Berlin Convention Office und dem Venue-Betreiber haben wir die einzig mögliche Entscheidung getroffen - die E-commerce Berlin Expo auf den **5. Mai 2022** zu verschieben, um die geplante Formel erfolgreich umzusetzen. **Darüber hinaus werden die mit euren Buchungen verbundenen Leistungen und Verpflichtungen ohne zusätzliche Kosten auf den neuen Termin übertragen.**
 
-Unsere Entscheidung wurde im Interesse unserer Kunden und der Maximierung des ROI der Veranstaltung getroffen. Aufgrund der aktuellen Beschränkungen und der Kapazitäten der STATION Berlin konnte die Veranstaltung in geschlossener Form für nur etwa 1000-1500 Gäste durchgeführt werden. Eine niedrige Zahl würde wahrscheinlich nicht die angemessene Kapitalrendite für die Veranstaltung ermöglichen, und wir sind uns bewusst, dass dieses Ergebnis für keinen unserer Kunden zufriedenstellend sein kann.
+Unsere Entscheidung wurde im Interesse unserer Kunden und der Maximierung des ROI der Veranstaltung getroffen. 
+Aufgrund der aktuellen Beschränkungen und der Kapazitäten der STATION Berlin konnte die Veranstaltung in geschlossener Form für nur etwa 1000-1500 Gäste durchgeführt werden. Eine niedrige Zahl würde wahrscheinlich nicht die angemessene Kapitalrendite für die Veranstaltung ermöglichen, und wir sind uns bewusst, dass dieses Ergebnis für keinen unserer Kunden zufriedenstellend sein kann.
 
-Wir wissen um den Wert von Face-to-Face-Meetings und sind uns bewusst, dass virtuelle oder hybride Meeting-Formate Live-Meetings nicht ersetzen können. Die E-Commerce Berlin Expo ist ein Ökosystem von Projekten. Nicht nur die Messe, aber auch die Konferenz, die E-commerce Germany Awards und der Treffpunkt für die Experten machen dieses Event aus. Wir sind überzeugt, dass wir uns bei dem neuen Veranstaltungstermin im Mai in Berlin treffen und das, was wir für euch vorbereitet worden ist, voll ausgeschöpft werden kann. 
+Wir wissen um den Wert von Face-to-Face-Meetings und sind uns bewusst, dass virtuelle oder hybride Meeting-Formate Live-Meetings nicht ersetzen können. 
+Die E-Commerce Berlin Expo ist ein Ökosystem von Projekten. Nicht nur die Messe, aber auch die Konferenz, die E-commerce Germany Awards und der Treffpunkt für die Experten machen dieses Event aus. Wir sind überzeugt, dass wir uns bei dem neuen Veranstaltungstermin im Mai in Berlin treffen und das, was wir für euch vorbereitet worden ist, voll ausgeschöpft werden kann. 
 
-Vielen Dank für euer Vertrauen und eure Unterstützung. Wir entschuldigen uns für diese außerhalb unseres Machtbereichs liegende Situation und bitten um euer Verständnis.
+Vielen Dank für euer Vertrauen und eure Unterstützung. 
+Wir entschuldigen uns für diese außerhalb unseres Machtbereichs liegende Situation und bitten um euer Verständnis.
 
 Bei Fragen wenden euch bitte an euren Account Manager.
 
--- resources/views/emails/company/ebe6-third-event-postponement-en.blade.php
+++ resources/views/emails/company/ebe6-third-event-postponement-en.blade.php
@@ -5,17 +5,23 @@
 # Dear {{ array_get($profile, "fname") }},
 
 
-In the past few weeks, there has been a tremendous effort to contain the fourth wave of the COVID-19 pandemic. As of the 2nd December 2021, we were confident that the initial offline format of the event in 2022 would be successful.
+In the past few weeks, there has been a tremendous effort to contain the fourth wave of the COVID-19 pandemic. 
+As of the 2nd December 2021, we were confident that the initial offline format of the event in 2022 would be successful.
 
-As the event organizers, we were prepared to comply with all the necessary standards for hygiene and safety. In order to ensure the safety of all participants, appropriate measures have been taken. Unfortunately, the new restrictions imposed by the German government to combat the COVID-19 epidemic of 2nd Dec 2021 drastically reduce the potential number of participants for this kind of event. 
+As the event organizers, we were prepared to comply with all the necessary standards for hygiene and safety. 
+In order to ensure the safety of all participants, appropriate measures have been taken. 
+Unfortunately, the **new restrictions imposed by the German government to combat the COVID-19 epidemic of 2nd Dec 2021 drastically reduce the potential number of participants for this kind of event. **
 
-The risks associated with the organization of the event were thoroughly analyzed. Following consultation with the Berlin Convention Office and the venue operator, we made the only decision possible - **to reschedule the event for May 5, 2022**, to successfully implement the planned formula. **In addition, the services and obligations associated with your bookings will be carried over to the new date without additional charges.**
+The risks associated with the organization of the event were thoroughly analyzed. Following consultation with the Berlin Convention Office and the venue operator, we made the only decision possible - **to reschedule the event for May 5, 2022**, to successfully implement the planned formula. 
+**In addition, the services and obligations associated with your bookings will be carried over to the new date without additional charges.**
 
 Our decision was driven by the best interest of our clients and maximizing the event's ROI for them. 
 
-Based on the current limitations and the capacity of the STATION Berlin facility, the event could be held in a closed format for about only 1000-1500 guests. A low number would probably not allow for the appropriate return on investment for the event, and we recognize that this outcome may not be satisfactory for any of our clients. 
+Based on the current limitations and the capacity of the STATION Berlin facility, the event could be held in a closed format for about only 1000-1500 guests. 
+A low number would probably not allow for the appropriate return on investment for the event, and we recognize that this outcome may not be satisfactory for any of our clients. 
 
-We know the value of face-to-face meetings, and that virtual or hybrid meeting formats cannot replace live meetings. The E-commerce Berlin Expo is an ecosystem of projects for our fairs, the conference, the E-commerce Germany Awards, as well as an e-commerce professional meeting place. We are convinced that the postponed date of the event will allow us to meet in Berlin in May and take full advantage of what we have prepared for you.
+We know the value of face-to-face meetings, and that virtual or hybrid meeting formats cannot replace live meetings. 
+The E-commerce Berlin Expo is an ecosystem of projects for our fairs, the conference, the E-commerce Germany Awards, as well as an e-commerce professional meeting place. We are convinced that the postponed date of the event will allow us to meet in Berlin in May and take full advantage of what we have prepared for you.
 
 Thank you for your trust and support. We apologize for this out of our control situation and appreciate your understanding.
 
