f7cf1951803010c1c9171520bc2e6de16cb55c0c
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-01-25 12:46:41 +0100

    additional services email $20$

---
+++ resources/views/emails/company/ebe6-top10-survey-2-de.blade.php
@@ -0,0 +1,19 @@
+
+
+@component('mail::message')
+
+# Hallo {{ array_get($profile, "fname") }},
+
+Wir hoffen, dass ihr euch auch schon darauf freut, alle offline auf der E-Commerce Berlin Expo im Mai 2022 zu treffen!
+ 
+Es ist uns extrem wichtig, dass ihr auf der Expo die bestmögliche Erfahrung mit den bestmöglichen Ergebnissen machen könnt.
+
+Um uns zu helfen dies erfüllen, bitten wir euch, uns die Namen der Kunden oder Unternehmen mitzuteilen, die ihr dort am liebsten treffen würdet. Wir werden dann unser Bestes geben, die Vertreter dorthin einzuladen und euch zu helfen, sie in Berlin zu treffen!
+
+Bitte füllen Sie die folgende Umfrage aus und lässt ihr uns zaubern: https://t.ly/4W9B
+
+Vielen Dank für die Unterstützung und bis bald!
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/ebe6-top10-survey-2-en.blade.php
@@ -0,0 +1,24 @@
+
+
+@component('mail::message')
+
+# Hello {{ array_get($profile, "fname") }},
+
+I hope that you also can’t wait to meet everyone offline at the E-commerce Berlin Expo in May 2022!
+
+We really want you to have the greatest experience at the Expo and to achieve the best possible outcome. 
+
+To help us fulfill it, we kindly ask you to share with us the names of clients or companies you’d like to meet there the most. We promise to do our best, to invite the representatives and help you meet them in Berlin!
+
+Please fill out below survey and let us do the magic: https://t.ly/4W9B
+
+Many thanks! 
+
+Regards,
+
+{{$footer}}
+
+@endcomponent
+
+
+
