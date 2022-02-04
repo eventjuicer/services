c13f928ad1677e40dc8f4953d6befcd5b03fc605
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-02-04 13:02:40 +0100

    teh21 cfp videos final email

--- resources/views/emails/visitor/teh21-videos_cfp.blade.php
+++ resources/views/emails/visitor/teh21-videos_cfp.blade.php
@@ -8,31 +8,31 @@ Kolejne Targi eHandlu odbędą się **14 kwietnia na EXPO XXI w Warszawie.**
 
 Aktualnie pracujemy nad agendą wystąpień. **Chcesz wystąpić? informacje poniżej!**
 
-Udostępniamy nagrania video z jubileuszowej 20. edycji Targów, która odbyła się w październiku. 
+Udostępniamy pierwszą porcję nagrań video z jubileuszowej 20. edycji Targów, która odbyła się w październiku. 
 
 
-<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:15px;">
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1633698528/c_1283_logotype.jpg" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1633698528/c_1283_logotype.jpg" alt="" /></td>
         <td>Damian Wiszowaty - CEO Gonito 
             <strong><a href="https://ehandel.com.pl/rydwany-ognia-czyli-jak-wykorzystac-synergie-modeli-seller-i-vendor-w-serwisie-amazon,2202">Rydwany ognia, czyli jak wykorzystać synergię modeli Seller i Vendor w serwisie Amazon</a></strong>
        
     </td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1582620906/c_1705_logotype.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1582620906/c_1705_logotype.png" alt="" /></td>
         <td>Piotr Buszka - co-founder & CEO - feeCOMPASS 
             <strong><a href="https://ehandel.com.pl/wirtualny-doradca-jak-poprawic-zadowolenie-klientow-i-konwersje-w-twoim-sklepie,2197">Wirtualny doradca. Jak poprawić zadowolenie klientów i konwersję w Twoim sklepie?</a></strong>
         </td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1597311044/c_1726_logotype.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1597311044/c_1726_logotype.png" alt="" /></td>
         <td>Mateusz Wróbel - Head of Sales - LivePrice 
             <strong><a href="https://ehandel.com.pl/zwiekszenie-sprzedazy-dzieki-automatyzacji-cen-w-sklepie,2195">Zwiększenie sprzedaży dzięki automatyzacji cen w sklepie</a></strong>
         </td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1554133278/c_1462_logotype.jpg" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1554133278/c_1462_logotype.jpg" alt="" /></td>
         <td>Krzysztof Marzec - CEO - DevaGroup 
             <strong><a href="https://ehandel.com.pl/7-najczestszych-sposobow-przepalania-budzetu-w-google,2193">7 najczęstszych sposobów przepalania budżetu w Google</a></strong>
         </td>   
@@ -51,30 +51,31 @@ Chcę zostać Prelegentem na Targach eHandlu
 
 <table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left">
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1540458331/c_1202_logotype.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1540458331/c_1202_logotype.png" alt="" /></td>
         <td>Paweł Zakielarz - CEO - Global24 
             <strong><a href="https://ehandel.com.pl/czy-warto-sprzedawac-do-uk,2189">Czy warto sprzedawać do UK?</a></strong>
     </td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1561617400/c_1500_logotype.jpg" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1561617400/c_1500_logotype.jpg" alt="" /></td>
         <td>Kacper Gugała - Business Development Manager Poland - Shopware 
-            <strong><a href="https://ehandel.com.pl/uxcro-w-e-commerce-czyli-jak-przepalic-budzet-reklamowy,2184">Unikalny sklep. Jak pozostać w świadomości Klienta?</a></strong></td>
+            <strong><a href="https://ehandel.com.pl/unikalny-sklep-jak-pozostac-w-swiadomosci-klienta,2187">Unikalny sklep. Jak pozostać w świadomości Klienta?</a></strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1637231628/c_1903_logotype.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1637231628/c_1903_logotype.png" alt="" /></td>
         <td>Kamil Bolek - CMO - LTTM 
             <strong><a href="https://ehandel.com.pl/vlogmas-prezentownik-a-moze-haul-jak-wykorzystac-potencjal-influencerow-w-przedswiatecznym-szale-zakupowym,2180">Vlogmas, prezentownik, a może haul? Jak wykorzystać potencjał influencerów w przedświątecznym szale zakupowym</a></strong>
         </td>
        
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1613654859/c_1142_logotype.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1613654859/c_1142_logotype.png" alt="" /></td>
         <td>Paweł Piotuch - Chief Retention Officer - IdoSell 
             <strong><a href="https://ehandel.com.pl/co-pomaga-uruchomic-sklep-szybko-i-tanio-a-w-jakim-przypadku-wdrozenie-przekroczy-budzet-i-sie-nie-uda,2179">Co pomaga uruchomić sklep szybko i tanio, a w jakim przypadku wdrożenie przekroczy budżet i się nie uda?</a></strong>
         </td>
     </tr>
 </table>
+
 
 
 Z pozdrowieniami,
