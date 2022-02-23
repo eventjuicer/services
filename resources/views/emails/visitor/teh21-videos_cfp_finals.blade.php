85436b6396487c581b6a6edffdcd9e438a9d7918
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-02-23 13:29:50 +0100

    cfp final mailing

---
+++ resources/views/emails/visitor/teh21-videos_cfp_finals.blade.php
@@ -0,0 +1,70 @@
+
+
+@component('mail::message')
+ 
+# Dzień dobry {{ $p->translate("[[fname]]") }},
+
+Kolejne Targi eHandlu odbędą się **14 kwietnia na EXPO XXI w Warszawie.**
+
+Niedawno **ruszyło głosowanie na prezentacje**, 
+które po spełnieniu warunków wejdą do targowej agendy wystąpień.
+
+Niezależnie od tego czy planujesz obecność na 21. Targach eHandlu czy nie - **WARTO ZAGŁOSOWAĆ. Wszyscy, którzy zagłosują otrzymają dostęp do video.**
+
+**Masz 6 głosów!**
+
+<a href="https://targiehandlu.pl/vote"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_600,h_600,c_limit,f_auto/v1645617347/teh21_cfp_categories.png" alt="" /></a>
+
+## Wybrane propozycje Wystąpień:
+
+Czego Barack Obama może nauczyć Cię o SMS-ach? **O skuteczności krótkich wiadomości tekstowych.**
+
+Złapał Kozak Tatarzyna, a Tatarzyn za łeb trzyma, czyli o tym, **jak umiejętnie analizować działania konkurencji na Amazon**
+
+EMAG **Rumunia - duży rynek, mała konkurencja, wysokie marże**
+
+**Jak BIMI pomoże Ci zwiększyć wskaźnik OR i konwersję?**
+
+Jak zwiększać zyski na Amazon i Kaufland.de? **Dynamiczny repricing i automatyzacja w pigułce.**
+
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/vote"])
+CHCĘ ODDAĆ GŁOS
+@endcomponent
+
+
+**Za 50$ zablokuję całkowicie Twój sklep na 24 h **- co Ty na to?
+
+**Skandynawia - jak sprawnie dostarczać przesyłki na tym rynku?**
+
+Wielokanałowa logistyka transgraniczna - **jak zebrać najlepsze spedycje w jednym miejscu?**
+
+**Paszportyzacja żywności - blockchain i NFT** w logistyce i kontraktowaniu dostaw do internetowego targu naturalnego jedzenia
+
+Teoria ograniczeń w e-commerce: **jak biznesowe wąskie gardła zamienić w zyski [+ przykłady zastosowania]**
+
+Optymalizacja szybkości strony a **Core Web Vitals**
+
+**Jak polaryzacja dobrobytu wpływa na decyzje zakupowe klienta w e-commerce?**
+
+<a href="https://targiehandlu.pl/vote"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_600,h_600,c_limit,f_auto/v1645617347/teh21_cfp_categories.png" alt="" /></a>
+
+
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
