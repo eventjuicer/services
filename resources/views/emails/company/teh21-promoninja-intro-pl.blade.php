f4cda210c7473f8516c6b0eb288be515aa27b934
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-31 12:12:18 +0200

    Update teh21-promoninja-intro-pl.blade.php

--- resources/views/emails/company/teh21-promoninja-intro-pl.blade.php
+++ resources/views/emails/company/teh21-promoninja-intro-pl.blade.php
@@ -14,18 +14,19 @@ Strona z materiałami dla Twojej firmy
 
 **Strona z materiałami promocyjnym jest publiczna.** 
 
-Adres URL do tej strony możesz wysłać osobie odpowiedzialnej za działania reklamowe w firmie lub do innych osób, które mają konta na serwisach społecznościowych (każda może skorzystać z linków udostępniających link do Twojej strony Wystawcy w serwisach społecznościowych.)
+Adres URL do tej strony możesz wysłać osobie odpowiedzialnej za działania reklamowe w firmie lub do innych osób, które mają konta na serwisach społecznościowych - ** można skorzystać z linków udostępniających link do Twojej strony Wystawcy w serwisach społecznościowych.**
 
 Wszystkie materiały mają dodany specjalny link, który:
 
-a) kieruje do strony Wystawcy (ta strona nie tylko opisuje Twoją firmę ale też pokazuje lokalizację Twojego stoiska na hali targowej) 
+a) **kieruje do profilu Wystawcy** (ta strona nie tylko opisuje Twoją firmę ale też pokazuje **lokalizację Twojego stoiska** na hali targowej) 
 
-b) zlicza punkty w Konkursie dla Wystawców, dzięki czemu uzyskujesz dodatkowe świadczenia.
+b) **zlicza punkty w Konkursie** dla Wystawców, dzięki czemu uzyskujesz dodatkowe świadczenia.
 
 ## Konkurs (Panel -> Pokaż się -> Ranking)
 
-Część dodatkowych usług i świadczeń jest dostępna tylko dla Wystawców, którzy zapraszają swoich Klientów i Partnerów do odwiedzenia stoiska swojej firmy na Targach. Wśród takich świadczeń znajdują się: dostęp do puli early-bird na kolejną edycję Targów, dostęp do większej puli zaproszeń Zwiedzających na spotkanie (dostęp do listy zarejestrowanych rusza na początku przyszłego tygodnia) czy wyróżnienie logotypu na stronie.
+**Część dodatkowych usług i świadczeń jest dostępna tylko dla Wystawców, którzy zapraszają swoich Klientów i Partnerów do odwiedzenia stoiska swojej firmy na Targach.** Wśród takich świadczeń znajdują się: **dostęp do puli early-bird** na kolejną edycję Targów, dostęp do **większej puli zaproszeń** Zwiedzających na spotkanie (dostęp do listy zarejestrowanych rusza na początku przyszłego tygodnia) czy **wyróżnienie logotypu na stronie.**
 
+Czas start! :)
 
 Pozdrawiam, 
 
