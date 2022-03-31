bbbee7d334e0022d6b865655a3f81613de471ebe
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-31 15:33:00 +0200

    visitor email

--- resources/views/emails/visitor/teh21-agenda1.blade.php
+++ resources/views/emails/visitor/teh21-agenda1.blade.php
@@ -10,42 +10,41 @@
 
 Wśród marek, których nie widzieliśmy jeszcze lub dawno nie widzieliśmy na Targach znajdują się m.in. **Grupa WP, Furgonetka.pl, GLS Poland, Klarna, Rating Captain, octopia, direct call, zen.com** 
 
-Bilet? ...proszę! [https://targiehandlu.pl/visit](https://targiehandlu.pl/visit?utm_source=oldvisitors&utm_campaign=teh21visitor&utm_medium=email)
+"Już mnie macie, dawać bilet!" ...proszę! :) [https://targiehandlu.pl/visit](https://targiehandlu.pl/visit?utm_source=oldvisitors&utm_campaign=teh21visitor&utm_medium=email)
 
-Po ostatnich średnio-udanych eksperymentach z małymi scenami **wracamy do 4 ogromnych scen**, z której każda powinna zapewnić odpowiedni komfort przyswajania wiedzy... **Zaplanowane są 33 wystąpienia oraz jeden godzinny panel dyskusyjny** 
+Po ostatnich średnio-udanych eksperymentach z małymi scenami **wracamy do 4 ogromnych scen**, z których każda powinna zapewnić odpowiedni komfort przyswajania wiedzy... **Zaplanowane są 33 wystąpienia oraz jeden godzinny panel dyskusyjny** 
 
 Oto część zaplanowanej agendy Wystąpień!
 
 <table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137054_avatar.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137054_avatar.png" alt="" /></td>
         <td>Rafał Agnieszczak, CEO Furgonetka <strong>"Pora na wyrównane szanse - zwrot akcji w e-commerce"</strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137111_avatar.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137111_avatar.png" alt="" /></td>
         <td>Adrian Piwko, Prezes Zarządu Pora na Pola S.A. <strong>"Paszportyzacja żywności - blockchain i NFT w logistyce i kontraktowaniu dostaw do internetowego targu naturalnego jedzenia"</strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_138990_avatar.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_138990_avatar.png" alt="" /></td>
         <td>Joanna Drabik, Ekspert Influencer Marketing Tchibo Polska, Wojas S.A. <strong>"Influencer Marketing. Moda czy skuteczne narzędzie promocji firmy?"</strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_137128_avatar.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_137128_avatar.png" alt="" /></td>
         <td>Łukasz Glewicz, Founder MonsterCam <strong>"Trendy ponad standardami - Formaty wideo, które poprawią skuteczność twojej kampanii."</strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_138406_avatar.png" alt="" /></td>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_138406_avatar.png" alt="" /></td>
         <td>Łukasz Dwulit, Head of Poland Klarna <strong>"Więcej niż płatności - Klarna jako partner wzrostu w e-commerce"</strong></td>
     </tr>
 </table>
 
+Udział w Targach eHandlu jest nadal otwarty - wystarczy zarejestrować się i oczekiwać na bilet.
 
 @component('mail::panel')
 
-Bilet? Proszę!
-
-@component('mail::button', ['url' => "https://targiehandlu.pl/visit"])
-Wygeneruj bilet
+@component('mail::button', ['url' => "https://targiehandlu.pl/visit?utm_source=oldvisitors&utm_campaign=teh21visitor&utm_medium=email"])
+Pobierz bilet
 @endcomponent
 
 @endcomponent
