b87a77fdf63952301de72e9ba7be5cbfa80997ad
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-31 15:38:43 +0200

    Update teh21-agenda1.blade.php

--- resources/views/emails/visitor/teh21-agenda1.blade.php
+++ resources/views/emails/visitor/teh21-agenda1.blade.php
@@ -23,23 +23,23 @@ Oto część zaplanowanej agendy Wystąpień!
 
 <table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
     <tr>
-        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137054_avatar.png" alt="" /></td>
+        <td width="200"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137054_avatar.png" alt="" /></td>
         <td>Rafał Agnieszczak, CEO Furgonetka <strong>"Pora na wyrównane szanse - zwrot akcji w e-commerce"</strong></td>
     </tr>
     <tr>
-        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137111_avatar.png" alt="" /></td>
+        <td width="200"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137111_avatar.png" alt="" /></td>
         <td>Adrian Piwko, Prezes Zarządu Pora na Pola S.A. <strong>"Paszportyzacja żywności - blockchain i NFT w logistyce i kontraktowaniu dostaw do internetowego targu naturalnego jedzenia"</strong></td>
     </tr>
     <tr>
-        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_138990_avatar.png" alt="" /></td>
+        <td width="200"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_138990_avatar.png" alt="" /></td>
         <td>Joanna Drabik, Ekspert Influencer Marketing Tchibo Polska, Wojas S.A. <strong>"Influencer Marketing. Moda czy skuteczne narzędzie promocji firmy?"</strong></td>
     </tr>
     <tr>
-        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_137128_avatar.png" alt="" /></td>
+        <td width="200"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_137128_avatar.png" alt="" /></td>
         <td>Łukasz Glewicz, Founder MonsterCam <strong>"Trendy ponad standardami - Formaty wideo, które poprawią skuteczność twojej kampanii."</strong></td>
     </tr>
     <tr>
-        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_138406_avatar.png" alt="" /></td>
+        <td width="200"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_138406_avatar.png" alt="" /></td>
         <td>Łukasz Dwulit, Head of Poland Klarna <strong>"Więcej niż płatności - Klarna jako partner wzrostu w e-commerce"</strong></td>
     </tr>
 </table>
