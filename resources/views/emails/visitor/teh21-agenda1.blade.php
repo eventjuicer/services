0e6144539eafbbff0b0391024c72a17f6b84cfd0
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-03-31 15:37:27 +0200

    Update teh21-agenda1.blade.php

--- resources/views/emails/visitor/teh21-agenda1.blade.php
+++ resources/views/emails/visitor/teh21-agenda1.blade.php
@@ -23,33 +23,33 @@ Oto część zaplanowanej agendy Wystąpień!
 
 <table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137054_avatar.png" alt="" /></td>
+        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137054_avatar.png" alt="" /></td>
         <td>Rafał Agnieszczak, CEO Furgonetka <strong>"Pora na wyrównane szanse - zwrot akcji w e-commerce"</strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137111_avatar.png" alt="" /></td>
+        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137111_avatar.png" alt="" /></td>
         <td>Adrian Piwko, Prezes Zarządu Pora na Pola S.A. <strong>"Paszportyzacja żywności - blockchain i NFT w logistyce i kontraktowaniu dostaw do internetowego targu naturalnego jedzenia"</strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_138990_avatar.png" alt="" /></td>
+        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_138990_avatar.png" alt="" /></td>
         <td>Joanna Drabik, Ekspert Influencer Marketing Tchibo Polska, Wojas S.A. <strong>"Influencer Marketing. Moda czy skuteczne narzędzie promocji firmy?"</strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_137128_avatar.png" alt="" /></td>
+        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_137128_avatar.png" alt="" /></td>
         <td>Łukasz Glewicz, Founder MonsterCam <strong>"Trendy ponad standardami - Formaty wideo, które poprawią skuteczność twojej kampanii."</strong></td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_400,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_138406_avatar.png" alt="" /></td>
+        <td width="300"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_138406_avatar.png" alt="" /></td>
         <td>Łukasz Dwulit, Head of Poland Klarna <strong>"Więcej niż płatności - Klarna jako partner wzrostu w e-commerce"</strong></td>
     </tr>
 </table>
 
-Udział w Targach eHandlu jest nadal otwarty - wystarczy zarejestrować się i oczekiwać na bilet.
+**Udział w Targach eHandlu jest nadal otwarty - wystarczy zarejestrować się i oczekiwać na bilet.**
 
 @component('mail::panel')
 
 @component('mail::button', ['url' => "https://targiehandlu.pl/visit?utm_source=oldvisitors&utm_campaign=teh21visitor&utm_medium=email"])
-Pobierz bilet
+Szybka Rejestracja
 @endcomponent
 
 @endcomponent
