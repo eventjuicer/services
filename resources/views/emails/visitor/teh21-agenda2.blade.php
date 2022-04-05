727100c78c597345873ce624d28ba6c8b9bb3dbe
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-05 15:02:21 +0200

    Create teh21-agenda2.blade.php

---
+++ resources/views/emails/visitor/teh21-agenda2.blade.php
@@ -0,0 +1,85 @@
+
+
+@component('mail::message')
+ 
+# Dzień dobry {{ $p->translate("[[fname]]") }},
+
+@component('mail::panel')
+
+XXI Targi eHandlu już za niewiele ponad tydzień w Warszawie (czwartek, 14 kwietnia. EXPO XXI Warszawa)
+
+Ponad **150 Wystawców** i 33 Wystąpienia na 4, komfortowych, scenach prezentacyjnych. 
+
+EXTRA: **Godzinny panel dyskusyjny organizowany przez Futuriti z przedstawicielami firm Zalando, SFD i Atomstore.**
+
+@endcomponent
+
+
+Chcesz uczestniczyć?  Proszę. [https://targiehandlu.pl/visit](https://targiehandlu.pl/visit?utm_source=oldvisitors&amp;utm_campaign=teh21visitor2&amp;utm_medium=email)
+
+(Bilet w PDF otrzymasz na początku kolejnego tygodnia.)
+
+Oto dalsza część zaplanowanej agendy Wystąpień!
+
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#000000;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;min-width:400px; max-width: 800px;">
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_137083_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Artur Halik, Head of Sales Shoper<strong>"U ciebie działa?! 7 sekretów, co robić, żeby twój sklep internetowy nie przestawał sprzedawać."</strong></td>
+    </tr>
+
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_136967_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Michał Kręcisz, Head Of Business Development Revhunter / Digitree Group<strong>"Jak prowadzić komunikację z anonimowym odwiedzającym sklep, by poprowadzić go przez cały lejek sprzedażowy, aż do konwersji?"</strong></td>
+    </tr>
+
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_137124_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Witold Wrodarczyk, CEO adequate<strong>"Nie daj się zjeść ciastkom"</strong></td>
+    </tr>
+
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_136678_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Damian Wiszowaty, CEO Gonito<strong>"Złapał Kozak Tatarzyna, a Tatarzyn za łeb trzyma, czyli o tym, jak umiejętnie analizować działania konkurencji na Amazon"</strong></td>
+    </tr>
+
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_136767_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Tomasz Zabojszcz, Lord Vader Nowe Kolory<strong>"EMAG Rumunia - duży rynek, mała konkurencja, wysokie marże"</strong></td>
+    </tr>
+  
+</table>
+
+**Udział w Targach eHandlu jest nadal otwarty - wystarczy zarejestrować się i oczekiwać na bilet w PDF.**
+
+@component('mail::panel')
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/visit?utm_source=oldvisitors&utm_campaign=teh21visitor2&utm_medium=email"])
+Szybka Rejestracja
+@endcomponent
+
+@endcomponent
+
+
+
+**Nadchodząca edycja będzie rekordowa** pod kątem liczby Wystawców, którymi - dla przypomnienia - są dostawcy usług i produktów wspierających sprzedaż online. 
+
+Wśród marek, których nie widzieliśmy jeszcze lub dawno nie widzieliśmy na Targach znajdują się m.in. **Grupa WP, Furgonetka.pl, GLS Poland, Klarna, Rating Captain, octopia, direct call, zen.com** 
+
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
