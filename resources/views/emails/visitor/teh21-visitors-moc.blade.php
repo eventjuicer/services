d92c0d6f3d96e1e0f708c8766ee2a73995787e89
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-07 15:17:48 +0200

    visitors3 moc

---
+++ resources/views/emails/visitor/teh21-visitors-moc.blade.php
@@ -0,0 +1,142 @@
+
+
+@component('mail::message')
+ 
+Dzień dobry {{ $fname }},
+
+# Prowadzisz oryginalny sklep internetowy lub oferujesz rewolucyjną usługę wspomagającą sprzedaż w Internecie ?
+
+**...i planujesz uczestniczyć w kolejnych Targach eHandlu?**
+
+**...i chciał(a)byś zostać Gościem specjalnego wydania podcastu "Merytorycznie o eCommerce"?**
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_700,h_700,c_limit,f_auto/v1649335215/1400x1400-MOC.png" alt="" style="max-width: 800px; width: 100%;"/>
+
+
+@component('mail::button', ['url' => "https://forms.gle/CdAaBRirzJbFLgHy8"])
+Zgłoś się tutaj. Wybierzemy 2 osoby.
+@endcomponent
+
+Podcast Marcina Rudzika nagrywany w strefie VIP na Targach eHandlu ukaże się na YouTube, Spotify i kanałach społecznościowych Marcina i naszych.
+
+Do zobaczenia na Targach!
+
+
+
+@component('mail::panel')
+
+**Targi eHandlu już za tydzień w Warszawie - w czwartek, 14 kwietnia, w EXPO XXI)**
+
+@endcomponent
+
+**Wracamy do 4 ogromnych scen**, z których każda powinna zapewnić odpowiedni komfort przyswajania wiedzy... **Zaplanowane są 33 wystąpienia oraz jeden godzinny panel dyskusyjny** 
+
+## "Już mnie macie, dawać bilet!" ...proszę! :) [https://targiehandlu.pl/visit]({!! $registerURl !!})
+
+**Nadchodząca edycja będzie rekordowa** pod kątem liczby Wystawców, którymi - dla przypomnienia - są dostawcy usług i produktów wspierających sprzedaż online.
+
+Wśród marek, których nie widzieliśmy jeszcze lub dawno nie widzieliśmy na Targach znajdują się m.in. **Grupa WP, Furgonetka.pl, GLS Poland, Klarna, Rating Captain, octopia, direct call, zen.com** 
+
+@component('mail::panel')
+
+@component('mail::button', ['url' => $registerURl])
+Szybka Rejestracja
+@endcomponent
+
+@endcomponent
+
+
+Oto część zaplanowanej agendy Wystąpień!
+
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#000000;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;min-width: 400px; max-width: 800px;">
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137054_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Rafał Agnieszczak, CEO Furgonetka <strong>"Pora na wyrównane szanse - zwrot akcji w e-commerce"</strong></td>
+    </tr>
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_137111_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Adrian Piwko, Prezes Zarządu Pora na Pola S.A. <strong>"Paszportyzacja żywności - blockchain i NFT w logistyce i kontraktowaniu dostaw do internetowego targu naturalnego jedzenia"</strong></td>
+    </tr>
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732396/teh21/visitors1/p_138990_avatar.png" alt="" /></td>
+        <td style="min-width: 200px">Joanna Drabik, Ekspert Influencer Marketing Tchibo Polska, Wojas S.A. <strong>"Influencer Marketing. Moda czy skuteczne narzędzie promocji firmy?"</strong></td>
+    </tr>
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_137128_avatar.png" alt="" /></td>
+        <td style="min-width: 200px">Łukasz Glewicz, Founder MonsterCam <strong>"Trendy ponad standardami - Formaty wideo, które poprawią skuteczność twojej kampanii."</strong></td>
+    </tr>
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1648732395/teh21/visitors1/p_138406_avatar.png" alt="" /></td>
+        <td style="min-width: 200px">Łukasz Dwulit, Head of Poland Klarna <strong>"Więcej niż płatności - Klarna jako partner wzrostu w e-commerce"</strong></td>
+    </tr>
+</table>
+
+**Udział w Targach eHandlu jest nadal otwarty - wystarczy zarejestrować się. Bilet (w PDF) otrzymasz w potwierdzeniu rejestracji.**
+
+@component('mail::panel')
+
+@component('mail::button', ['url' => $registerURl])
+Szybka Rejestracja
+@endcomponent
+
+@endcomponent
+
+Oto dalsza część zaplanowanej agendy Wystąpień!
+
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#000000;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;min-width:400px; max-width: 800px;">
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_137083_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Artur Halik, Head of Sales Shoper<strong>"U ciebie działa?! 7 sekretów, co robić, żeby twój sklep internetowy nie przestawał sprzedawać."</strong></td>
+    </tr>
+
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_136967_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Michał Kręcisz, Head Of Business Development Revhunter / Digitree Group<strong>"Jak prowadzić komunikację z anonimowym odwiedzającym sklep, by poprowadzić go przez cały lejek sprzedażowy, aż do konwersji?"</strong></td>
+    </tr>
+
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_137124_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Witold Wrodarczyk, CEO adequate<strong>"Nie daj się zjeść ciastkom"</strong></td>
+    </tr>
+
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_136678_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Damian Wiszowaty, CEO Gonito<strong>"Złapał Kozak Tatarzyna, a Tatarzyn za łeb trzyma, czyli o tym, jak umiejętnie analizować działania konkurencji na Amazon"</strong></td>
+    </tr>
+
+    <tr>
+        <td style="min-width: 200px"><img src="https://res.cloudinary.com/eventjuicer/image/upload/v1649162232/teh21/visitors1/p_136767_avatar.png" alt="" /></td>
+        <td style="min-width: 300px">Tomasz Zabojszcz, Lord Vader Nowe Kolory<strong>"EMAG Rumunia - duży rynek, mała konkurencja, wysokie marże"</strong></td>
+    </tr>
+  
+</table>
+
+
+
+@component('mail::panel')
+
+@component('mail::button', ['url' => $registerURl])
+Szybka Rejestracja
+@endcomponent
+
+@endcomponent
+
+
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
