bf55a9b329fb8b21658e4b88e281f2fb5d19b279
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-08 13:53:07 +0200

    fix

--- resources/views/emails/company/teh21-logistics-pl.blade.php
+++ resources/views/emails/company/teh21-logistics-pl.blade.php
@@ -31,10 +31,15 @@ Expo XXI, ul. Prądzyńskiego 12/14, 01-222 Warszawa
 
 **ta edycja będzie realizowana w HALI 1 (stoiska i sceny) oraz HALI 3 (Strefa VIP)**
 
+Mapka sytuacyjna.
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_1200,h_1000,c_limit,f_auto/v1649418029/www/logistics/teh21_exhibitor_orientation_map.png" alt="" style="max-width: 700px;"/>
+
+
 ## Parkowanie (dzień przed eventem, dzień eventu)
 
-Bilet parkingowy należy pobrać z parkomatu. 
-**Do bezpłatnego wyjazdu** potrzebny będzie voucher, który dostaniecie Państwo od nas przy pierwszym wspólnym spotkaniu z nami (dzień montażowy lub rano - przed wydarzeniem).
+Przy pierwszym wjeździe na teren EXPO XXI, bilet parkingowy należy pobrać z parkomatu. 
+**Do bezpłatnego wyjazdu** potrzebna będzie karta parkingowa, który dostaniecie Państwo od nas przy pierwszym wspólnym spotkaniu z nami (dzień montażowy lub rano - przed wydarzeniem). **Do kolejnych wjazdów i wyjazdów trzeba korzystać z karty parkingowej (niczego nie pobieramy z parkomatu)**
 
 ## Dzień montażowy (opcjonalny) - 13 kwietnia
 
@@ -46,11 +51,12 @@ Po wejściu na Halę #1 (wejście po lewej patrząc od wejścia głównego) nale
 
 Montaż w dzień targowy możliwy jest tylko i wyłącznie w godzinach **8:00 - 9:30.** (O 10:00 hala zostanie otwarta do publicznego zwiedzania.)
 
-Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców znajdującego się w strefie VIP (Hala #3 - po prawej stronie od wejścia głównego)
+Jeśli nie odbieraliście Państwo welcome-packu w opcjonalny dzień montażowy, w dzień targowy należy udać się do punktu informacyjnego dla Wystawców znajdującego się w strefie VIP (Hala #3 - po prawej stronie od wejścia głównego do kompleksu hal)
 
 W razie wystąpienia jakichkolwiek problemów i wątpliwości związanych z Państwa stoiskiem (np z prądem) należy udać się do informacji dla wystawców (Hala #3 / Strefa VIP) lub wysłać SMS na numer do Opiekuna, który znajdzie się w pakiecie powitalnym.
 
-**Demontaż** jest możliwy od 17:15. Hala będzie dostępna do 20:00 (o ile nie ustalono z nami inaczej).
+**Demontaż** jest możliwy od 17:15. Hala będzie dostępna do 20:00 
+
 
 ## Welcome pack
 
@@ -59,7 +65,7 @@ Welcome pack zostanie przekazany podczas naszego pierwszego spotkania (13 kwietn
 * identyfikatory przedstawicieli
 * vouchery obiadowe (w liczbie zarejestrowanych w Panelu Wystawcy ale nie więcej niż 4 sztuki - chyba, że dokupiono dodatkowe...)
 * numer kontaktowy do osoby z obsługi targów, 
-* karta parkingowa.
+* karta parkingowa (potrzebna do wyjazdu i ew. kolejnego wjazdu-wyjazdu)
 * kod WWIFI
 
 ## Dodatkowe informacje i zalecenia
