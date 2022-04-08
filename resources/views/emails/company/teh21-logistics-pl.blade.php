3cfffbf8e84f78403e5673b349ed9292aecfb884
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-08 13:27:42 +0200

    teh21 logistics PL

---
+++ resources/views/emails/company/teh21-logistics-pl.blade.php
@@ -0,0 +1,84 @@
+
+
+@component('mail::message')
+ 
+# Cześć
+
+# Oto podstawowe informacje logistyczne:
+
+## Wysyłka paczek i materiałów, które mają trafić bezpośrednio na Targi.
+
+@component('mail::panel')
+
+ExpoSped Sp. z o.o. / Piotr Berus
+
+EHANDEL [nr stoiska lub marka handlowa firmy]
+
+ul. Prądzyńskiego 12/14
+
+01-222 Warszawa
+
+(tel. kontaktowy: Piotr Berus +48 733 888 031)
+
+@endcomponent
+
+**Wysłane paczki znajdą się na Państwa stoisku podczas dnia montażowego czyli 13 kwietnia (w środę).
+Hala jest zamykana i ochraniana po wyjściu organizatora w dzień montażowy.**
+
+## Miejsce Targów
+
+Expo XXI, ul. Prądzyńskiego 12/14, 01-222 Warszawa
+
+**ta edycja będzie realizowana w HALI 1 (stoiska i sceny) oraz HALI 3 (Strefa VIP)**
+
+## Parkowanie (dzień przed eventem, dzień eventu)
+
+Bilet parkingowy należy pobrać z parkomatu. 
+**Do bezpłatnego wyjazdu** potrzebny będzie voucher, który dostaniecie Państwo od nas przy pierwszym wspólnym spotkaniu z nami (dzień montażowy lub rano - przed wydarzeniem).
+
+## Dzień montażowy (opcjonalny) - 13 kwietnia
+
+Godziny montażowe to 16:00 - 20:00 (o ile nie ustalono z nami inaczej). 
+
+Po wejściu na Halę #1 (wejście po lewej patrząc od wejścia głównego) należy udać się do punktu odbioru welcome-packów (początek Hali #1), w którym będzie czekał Państwa Opiekun i zaprowadzi Państwa do właściwego stoiska.
+
+## Dzień Targowy - 14 kwietnia
+
+Montaż w dzień targowy możliwy jest tylko i wyłącznie w godzinach **8:00 - 9:30.** (O 10:00 hala zostanie otwarta do publicznego zwiedzania.)
+
+Po wejściu na halę należy udać się do punktu informacyjnego dla Wystawców znajdującego się w strefie VIP (Hala #3 - po prawej stronie od wejścia głównego)
+
+W razie wystąpienia jakichkolwiek problemów i wątpliwości związanych z Państwa stoiskiem (np z prądem) należy udać się do informacji dla wystawców (Hala #3 / Strefa VIP) lub wysłać SMS na numer do Opiekuna, który znajdzie się w pakiecie powitalnym.
+
+**Demontaż** jest możliwy od 17:15. Hala będzie dostępna do 20:00 (o ile nie ustalono z nami inaczej).
+
+## Welcome pack
+
+Welcome pack zostanie przekazany podczas naszego pierwszego spotkania (13 kwietnia lub w dzień targowy - 14 kwietnia). 
+
+* identyfikatory przedstawicieli
+* vouchery obiadowe (w liczbie zarejestrowanych w Panelu Wystawcy ale nie więcej niż 4 sztuki - chyba, że dokupiono dodatkowe...)
+* numer kontaktowy do osoby z obsługi targów, 
+* karta parkingowa.
+* kod WWIFI
+
+## Dodatkowe informacje i zalecenia
+
+** Prosimy o wzięcie własnych listew zasilających **
+
+** Prosimy pamiętać, że standardowe przyłącze prądu to tylko 300W** jeśli planujesz coś więcej niż korzystanie z 2 laptopów i ew. TV trzeba dokupić indywidualne przyłącze (Panel Wystawcy). Prosimy o kontakt jeśli masz wątpliwości.
+
+@component('mail::panel')
+
+UWAGA: W przypadku podklejania wykładzin we własnym zakresie (brak wykupienia stosownej usługi od nas) jedyną rekomendowaną opcją jest profesjonalna taśma dwustronna ETG firmy Imawex (nie prowadzimy jej sprzedaży). Typowa (=marketowa) taśma dwustronna jest nie do usunięcia po wydarzeniu co wygeneruje dodatkowe (naprawdę kosmiczne) koszty sprzątania, które będziemy musieli refakturować.
+
+@endcomponent
+
+Regards, 
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+@endcomponent
+
