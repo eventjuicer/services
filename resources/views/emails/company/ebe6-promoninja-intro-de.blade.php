d8d8e7abd244e874b8ca412128a09f52f28db168
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-08 12:53:06 +0200

    promoninja

---
+++ resources/views/emails/company/ebe6-promoninja-intro-de.blade.php
@@ -0,0 +1,66 @@
+
+
+@component('mail::message')
+
+# Hello {{ array_get($profile, "fname") }},
+
+Wir befinden uns im Endspurt vor der E-Commerce Berlin Expo. Es ist höchste Zeit, euch die neueste Herausforderung vorzustellen, die exklusiv für unsere Aussteller verfügbar ist.
+
+## Was ist die Idee dahinter?
+
+Wir glauben, dass es bei einer erfolgreichen Veranstaltung nicht nur darum geht, neue Kunden kennenzulernen, sondern auch die Beziehungen zu bestehenden Kunden zu stärken. Und was wäre eine bessere Gelegenheit dafür, als sie auf der Expo zu treffen?
+
+## Wie funktioniert das?
+
+Bei diesem Wettbewerb könnt ihr Punkte sammeln, indem ihr einen Link teilt, der eure Teilnahme an der Expo bewirbt und eure Kunden zur Teilnahme an der Veranstaltung einlädt. Jeder Klick eines Nutzers entspricht einem Punkt – ganz einfach! Der Trick ist, dass ihr euren personalisierten Link teilen müsst, um die Punkte zu erhalten.
+ 
+Euren individuellen Link findet ihr in eurem Ausstellerprofil im Bereich **Werbematerial**.
+
+@component('mail::button', ['url' => $accountUrl])
+Zu meinem Account gelangen
+@endcomponent
+
+Ihr könnt den Link in E-Mail-Signaturen, Anzeigen (FB-Anzeigen, Adwords, sozialen Medien) oder mit Hilfe des direkten Kommunikationsweges bei euren Kunden verwenden. Jeder Link kann auch in die Zwischenablage kopiert und per Messenger, E-Mail oder in euren Newslettern versendet werden.
+
+## Wie teile ich den Link?
+
+Der in den sozialen Medien gepostete Link kann unterschiedliche visuelle Ergebnisse liefern:
+* die von uns entworfene Standardgrafik (erscheint automatisch, keine Handlung eurerseits erforderlich),
+* euer individuelles Design (nur aktiv, wenn ihr die vollständige URL zu eurer Grafik im Bereich **Firma** eures Ausstellerprofils angebt).
+
+Ihr könnt die Grafik in Tools wie Facebook Debugger oder LinkedIn Post Inspector in der Vorschau anzeigen lassen. Wenn die aktualisierte Grafik nicht sofort sichtbar ist, leert bitte euren Cache und versucht es erneut.
+
+Darüber hinaus haben wir im Abschnitt **Newsletter Vorlagen** zwei versandfertige Newsletter (Englisch und Deutsch) vorbereitet, die eure Kunden und Partner über eure Teilnahme an der Veranstaltung informieren sollen. Ihr könnt die HTML kopieren oder eine ZIP-Datei herunterladen, die einfach von Newsletter2go oder Mailchimp-ähnlicher Software importiert werden kann.
+
+## Was kann man gewinnen?
+
+In eurem Ausstellerprofil könnt ihr die vollständige Liste der verfügbaren Preise im Abschnitt **Preise** einsehen, wie zum Beispiel:
+* Speaking Slot auf einer unserer Bühnen während der Expo,
+* Video-Interview,
+* Logo-Highlight auf der EBE-Website,
+* Und vieles mehr!
+ 
+Jeder der Preise setzt die Erfüllung der beschriebenen Bedingungen für seine Gewährung voraus:
+* minimale Punktzahl (= Seitenaufrufe eures öffentlichen Ausstellerprofils über den individualisierten Link),
+* Position im Ausstellerranking.
+ 
+**Die Preise schließen einander nicht aus** – ihr könnet gleichzeitig mehrere Preise gewinnen.
+
+## Wo sind wir im Ranking? 
+
+Im Bereich **Preise** eures Ausstellerprofils findet ihr den Referenzlink zum Ranking, wo ihr eure aktuelle Position im Wettbewerb einsehen könnt.
+
+Das Ranking wird auf der Grundlage von Daten erstellt, die von unserem Analysetool abgerufen werden. Bitte beachtet, dass es eine Weile dauert, bis die Punkte dort sichtbar sind - wenn ihr die Ergebnisse früh morgens überprüft, seht ihr möglicherweise die Ergebnisse vom Vortag.
+
+## Fragen?
+
+Meldet euch gerne bei mir und ich helfe gerne weiter! Vereinbart am besten einen Call mit mir, damit wir alle Fragen durchgehen können.
+
+**Das Gewinnspiel endet am 22. April 2022, 23:59 Uhr MEZ.**
+
+Viel Erfolg!
+
+{{$footer}}
+
+@endcomponent
+
---
+++ resources/views/emails/company/ebe6-promoninja-intro-en.blade.php
@@ -0,0 +1,66 @@
+
+
+@component('mail::message')
+
+# Hello {{ array_get($profile, "fname") }},
+
+We’re in the final stretch before the E-commerce Berlin Expo. It’s the high time to introduce you to the newest challenge, available exclusively for our exhibitors.
+
+## What’s the idea behind it? 
+
+We believe that a successful event is not only about getting to know new customers, but also strengthening relationships with existing clients. And what could be a better opportunity for it than meeting them at the Expo? 
+
+## How does that work?
+
+In this competition you can win points by sharing a link which promotes your participation in the Expo and invites your clients to join the Event. Every user’s click equals one point - simple as that! The trick is, you have to share your customised link to receive the points.
+ 
+You can find your customised link in your Exhibitor’s profile in the **Promo materials** section. 
+
+@component('mail::button', ['url' => $accountUrl])
+Sign Into My Account
+@endcomponent
+
+Feel free to use the link in email footers, advertisements (FB ads, adwords, social media) or when directly informing your clients. It can be also copied to clipboard and sent via messenger, email or in your newsletters. 
+
+## How do I share the link?
+
+The link posted on social media can give different visual results:
+* the default graphic we designed (appears automatically, no action needed on your side),
+* your customised design (active only if you provide the full URL to your graphic in the **Company data** section of your exhibitor’s profile).
+
+You can preview the graphic in tools such as Facebook Debugger or LinkedIn Post Inspector. If the updated graphic isn’t immediately visible, please clear your cache and try again.
+ 
+Furthermore, in the **Newsletter templates** section we prepared two ready-to-send newsletters (English and German), which are designed to inform your clients and partners about your participation in the Event. You can copy HTML or download a ZIP file that can be easily imported by Newsletter2go or Mailchimp-like software.
+
+## What is there to win?
+
+In your Exhibitor's profile you can see the full list of available prizes in the **Rewards** section, such as:
+* Presentation slot on one of our stages during the Expo, 
+* Video interview,
+* Logo highlight on the EBE website,
+* And many more!
+ 
+Each of the prizes requires fulfilment of the described conditions for its granting:
+* minimal number of points (= pageviews of your public exhibitor's profile through the customised link),
+* position in the exhibitors ranking.
+ 
+**The prizes do not exclude each other** - you can be entitled to several prizes simultaneously.
+
+## Where am I in the Ranking?
+
+In the **Rewards** section of your exhibitors profile you can find the reference link to the ranking where you can check your current position in the contest. 
+
+The ranking is created on the basis of data fetched from our analytics tool. Please note that it takes a while for the points to be visible there - when you check the results early in the morning, you may see the results from the previous day.
+
+## Any questions?
+
+Let me know, I'll be happy to help! The best way would be to schedule a call with me so we can go through all your questions.
+
+**The Exhibitor Contest ends on April 22, 2022, 11:59 PM CET.
+
+Best of luck!
+
+{{$footer}}
+
+@endcomponent
+
