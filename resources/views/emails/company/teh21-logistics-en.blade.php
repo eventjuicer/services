93f7f302303fd8fb2b09e8b323906ae7c9e22863
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-08 14:34:07 +0200

    Create teh21-logistics-en.blade.php

---
+++ resources/views/emails/company/teh21-logistics-en.blade.php
@@ -0,0 +1,93 @@
+
+
+@component('mail::message')
+ 
+ # Hello!
+ 
+ # Here you have basic logistic information about E-commerce Warsaw Expo:
+ 
+ ## Oversized parcels and materials which you want to ship and have delivered directly to the booth. Recipient:
+ 
+ @component('mail::panel')
+ 
+ ExpoSped Sp. z o.o. / Piotr Berus
+ 
+ EHANDEL [booth ID / or BRAND name]
+ 
+ ul. Prądzyńskiego 12/14 (street name)
+  
+ 01-222 Warszawa POLAND
+ 
+ (contact person: Piotr Berus +48 733 888 031)
+ 
+ @endcomponent
+ 
+ 
+ ## Place:
+ 
+ EXPO XXI (HALL #1)
+ 
+ ul. Prądzyńskiego 12/14
+ 
+ 01-222 Warszawa 
+
+This edition will be held in HALL 1 (stands and stages) and HALL 3 (VIP area).
+
+Orientation map
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_1200,h_1000,c_limit,f_auto/v1649418029/www/logistics/teh21_exhibitor_orientation_map.png" alt="" style="max-width: 700px;"/>
+
+
+## Parking (the day before the event, the day of the event)
+
+When entering the EXPO XXI site for the first time, the parking ticket must be collected from the parking meter. 
+For a free exit, you will need a parking card, which you will get from us during our first meeting (assembly day or in the morning - before the event). 
+You need to use the parking card for subsequent entries and exits.
+ 
+ ## (Optional) Setup day - 13th of April
+ 
+ Assembly hours 4pm-8pm
+ 
+ After entering the main entrance you need to go to the **information point for the exhibitors** (located in the  Hall 1) . 
+ Your Account Manager will happily await :) 
+ 
+ ## Event Day - 14th of April
+
+Assembly on a event day is only possible between **8:00 and 9:30**. (At 10:00am we will open gates and welcome visitors.)
+
+If you have not picked up the welcome pack on the assembly day, please go to the Exhibitors' information desk located in the VIP area (Hall # 3 - on the right from the main entrance to the hall complex)
+ 
+ In case of any problem (e.g., with electricity) you should go to the information point (Hall # 3 / VIP Zone) or text your Account Manager, whose number you will find in the welcome pack.
+ 
+ **Disassembly** is possible from 5:15pm till 8:00pm.
+ 
+ ## Welcome pack
+ 
+ During our first meeting (optional setup day or immediately before the event. NOTE: the location is different on April 13 and 14, as mentioned above).) you will be handed a welcome pack with: 
+ 
+ * the badges of your representatives,
+
+ * lunch vouchers (number of vouchers = number of representatives registered but max 4... need more? please buy addditional via Additional services in your Exhibitor's Panel),
+ 
+ * contact numbers to the important staff of Expo, 
+ 
+ * parking card,
+ 
+ * WiFi password.
+ 
+ ## Additional recommendations
+ 
+ ** Please bring your own power extension cord / bar ** (of course we have some backup but...)
+ 
+ ** Remember, that standard power access is only 300 Watts ** 
+ 
+ If you plan to use something more than, for instance, 2 laptops and TV you need to buy an extra package (3000 Watts).  If you want to consult power usage of your equipment, please contact us.
+ 
+ 
+ Regards, 
+ 
+{{$footer}}
+ 
+ @endcomponent
+ 
+ 
