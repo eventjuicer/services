7e034721cf91f56297c68590058a7236692f8250
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-04-28 13:10:51 +0200

    ebe6 ticket

---
+++ resources/views/emails/visitor/ebe6-ticket-restore1.blade.php
@@ -0,0 +1,75 @@
+@component('mail::message')
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/f_auto/v1646742196/EB_2022_master_files_540x300.jpg" alt="E-commerce Berlin Expo" />
+
+# Hello {{$p->translate("[[fname]]")}},
+
+**The wait is finally over.**
+
+E-commerce Expo opens on **5th May on STATION-Berlin**, giving you access to over 200 leading e-commerce and retail suppliers!
+
+We're convinced that the combination of keynotes and presentations will give you knowledge and inspiration that you can take back to the office and use to help you thrive in the new digital environment.
+
+Let's have at who is going to share their expertise, shall we
+
+@component('mail::button', ['url' => 'https://ecommerceberlin.com/visit?utm_source=visitors_alternative&utm_medium=email&utm_campaign=visitors_alternative_email'])
+Check the full schedule ☛
+@endcomponent
+
+
+Here is a small preview of the hot topics of the conference:
+
+* Amazon, Rolf Kimmeyer - From Buxtehude to Brooklyn - what companies should know about **internationalization via Amazon Marketplace**
+
+* Delivery Hero, Inés Fernández - Take your campaign to the next 
+level with **SpotOn**
+
+* Douglas, Chiara Fraune - From Beauty retailer to **One-Stop Shopping Platform** for beauty & health
+
+* EXCONCEPT, Jason Weißbach & MissPompadour, Erik Reintjes - Content meets Commerce – MissPompadours Einkaufserlebnis für über 250.000 DIY
+
+* Henkel, Peter Hartmann & Claneo, Matthäus Michalik - Global SEO: Think Global, Act Local Hubspot, Ben Harmanus - The takeaways from extreme sports - and how it helps me to be a better marketer
+
+
+@component('mail::button', ['url' => 'https://ecommerceberlin.com/visit?utm_source=visitors_alternative&utm_medium=email&utm_campaign=visitors_alternative_email'])
+Collect your ticket ☛
+@endcomponent
+
+
+* MY TOYS GROUP, Monique Paulsen & Thomas Ficht - #BNPL – secret sauce or historic hype?
+
+* DKB Service, Dominique Leikauf - How to use the **full potential - of your current and future employees**
+
+* KADEWE GROUP, Philipp C. Engelmann - How to digitize a luxury department store in lightspeed
+
+* mokebo, Philip Kehela - How to run a DTC bootstrapped E-Commerce concept for a complex industry like Home & Living
+
+* Plug and Play, Carolin Wais - **Metaverse** and its implications for eCommerce
+
+* TUI.com, Christina Wolff - Personalization at TUI.com
+
+* Vodafone, Andrzej Kawalec - Extortion as a Service - the evolution of kidnap from people to data
+
+
+Participation in the conference is free of charge, but all people willing to participate are required to pre-register on the official website of the event.
+
+
+@component('mail::button', ['url' => 'https://ecommerceberlin.com/visit?utm_source=visitors_alternative&utm_medium=email&utm_campaign=visitors_alternative_email'])
+Check the full schedule ☛
+@endcomponent
+
+
+
+<a href="https://res.cloudinary.com/ecommerceberlin/image/upload/w_500,c_fit,f_auto/v1647954404/Newsletters/1.png" alt="" />
+
+
+There's now just one week to go - make sure you tune in to one day of unmissable speakers and content.
+
+
+All the best,
+
+Lucas Zarna
+
+E-commerce Berlin Expo Team
+
+@endcomponent
