3414bd9e5edc5fc86832df6326d230ec40011c78
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-16 13:17:42 +0200

    teh22 salesinfo

---
+++ resources/views/emails/company/teh22-salesplan-pl.blade.php
@@ -0,0 +1,23 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na 22 Targi eHandlu (miejsce: 27 października w Warszawie - EXPO XXI)
+
+## Środa - 18 maja 11:00
+
+**Sprzedaż zamknięta dla TOP30** Konkursu dla Wystawców (jeśli znajdujesz się w tym gronie to specjalny link otrzymasz najpóźniej 2h przed startem sprzedaży)
+
+Sprzedaż dla Wystawców 21. Targów eHandlu zainteresowanych większą przestrzenią (3 i więcej stoisk) - **prośba o info zwrotne jeśli taką przestrzenią zainteresowana jest Twoja firma**
+
+## Czwartek - 19 maja 11:00
+
+Sprzedaż otwarta dla wszystkich zainteresowanych - na https://targiehandlu.pl 
+
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
