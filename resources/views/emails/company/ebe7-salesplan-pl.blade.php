4bf846db0bc5058525e9b43f8f50bc40544f0ee9
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-16 14:20:52 +0200

    ebe7 salesplan

---
+++ resources/views/emails/company/ebe7-salesplan-pl.blade.php
@@ -0,0 +1,36 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na E-commerce Berlin 2023  (data STATION Berlin)
+
+## Środa - 18 maja 11:00
+
+**Sprzedaż zamknięta dla TOP30** Konkursu dla Wystawców (jeśli znajdujesz się w tym gronie to specjalny link otrzymasz najpóźniej 2h przed startem sprzedaży)
+Na kupno masz cały dzień. 
+Kolejnego dnia będą mogli rezerwować przestrzeń wszyscy zainteresowani więc jeśli nie dokonasz rezerwacji nie będziemy mogli zagwarantować dobrych lokalizacji ani najniższych cen.
+
+
+OPCJA JESLI JEST ZASADNE
+Sprzedaż dla Wystawców 21. Targów eHandlu zainteresowanych większą przestrzenią (3 i więcej stoisk) - **prośba o info zwrotne jeśli taką przestrzenią zainteresowana jest Twoja firma**
+OPCJA JESLI JEST ZASADNE
+
+
+## Czwartek - 19 maja 11:00
+
+Sprzedaż otwarta dla wszystkich zainteresowanych (wystawcy EBE 2022 spoza top30, waiting list) - mapka i ceny dostępne będą w środę na https://ecommerceberlin.com
+
+
+## Jak wygląda proces rezerwacji?
+
+Z uwagi na zainteresowanie przekraczające limit przestrzeni wdrażamy inny model sprzedaży.
+Stoiska będą sprzedawane w pulach sprzedaży, które są ograniczone ilościowo. 
+Każda kolejna pula aktywuje się automatycznie po wyczerpaniu limitu poprzedniej - może się zdarzyć że w pierwszym dniu aktywują się kolejne - droższe - sprzedaży.
+
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
