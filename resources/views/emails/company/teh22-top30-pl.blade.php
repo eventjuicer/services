6d12bd09055b6c4b885e9fab8e10661bc5e5737d
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-17 14:39:16 +0200

    Update teh22-top30-pl.blade.php

--- resources/views/emails/company/teh22-top30-pl.blade.php
+++ resources/views/emails/company/teh22-top30-pl.blade.php
@@ -3,13 +3,13 @@
 # Cześć {{ array_get($profile, "fname")  }},
 
 Możliwość rezerwacji przestrzeni wystawienniczej dla Twojej firmy i innych firm, 
-które znalazły się w TOP30 rankingu Konkursu dla Wystawców, **zaplanowana jest na jutro (tj czwartek 18 maja) - od godziny 11:00.**
+które znalazły się w TOP30 rankingu Konkursu dla Wystawców, **zaplanowana jest na jutro (tj środę 18 maja) - od godziny 11:00.**
 
 **Rezerwacja będzie możliwa tylko i wyłącznie przez stronę** [targiehandlu.pl/top30_23as4gxS](https://targiehandlu.pl/top30_23as4gxS)
 
 ...i potrwa do końca dnia.
 
-W czwartek uruchamiamy otwartą sprzedaż dla pozostałych Wystawców ubiegłej edycji Targów eHandlu i innych zainteresowanych.
+W czwartek uruchamiamy otwartą sprzedaż dla pozostałych Wystawców ubiegłej edycji Targów eHandlu oraz innych zainteresowanych.
 
 Pozdrowienia, 
 
