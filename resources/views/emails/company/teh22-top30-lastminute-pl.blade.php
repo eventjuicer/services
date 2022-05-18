cb70ab26d0f0a378f1dc6f562542e8bfe0e20f92
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-18 10:13:41 +0200

    teh22 eb lastminute

---
+++ resources/views/emails/company/teh22-top30-lastminute-pl.blade.php
@@ -0,0 +1,21 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Ostatnie przypomnienie :)
+
+Możliwość rezerwacji przestrzeni wystawienniczej dla Twojej firmy i innych firm, 
+które znalazły się w TOP30 rankingu Konkursu dla Wystawców, **zaplanowana jest na DZISIAJ (tj środę 18 maja) - od godziny 11:00.**
+
+**Rezerwacja będzie możliwa tylko i wyłącznie przez stronę** [targiehandlu.pl/top30_23as4gxS](https://targiehandlu.pl/top30_23as4gxS)
+
+...i potrwa do końca dnia.
+
+W czwartek uruchamiamy otwartą sprzedaż dla pozostałych Wystawców ubiegłej edycji Targów eHandlu oraz innych zainteresowanych.
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
