05a52692010ad6891ff1cc035ed5375ab2947af7
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-18 09:50:56 +0200

    top30 lastminute

---
+++ resources/views/emails/company/ebe7-sales-top30-lastminute-de.blade.php
@@ -0,0 +1,37 @@
+
+
+@component('mail::message')
+ 
+# Hallo {{ array_get($profile, "fname")  }},
+  
+@component('mail::panel')
+
+## Heute / 18. Mai, 11:00 Uhr
+ 
+**Exklusiver Vorverkauf für die TOP30 Unternehmen** aus dem Aussteller-Wettbewerb.
+
+**Ihr seid eines der 30 berechtigten Unternehmen!** 
+
+Bucht die Ausstellungsfläche heute über [ecommerceberlin.com/top30earlybird_t6d63sa9](https://ecommerceberlin.com/top30earlybird_t6d63sa9)
+ 
+Über diesen Link könnt ihr die Ausstellungsflächen bis **zum Ende des Tages buchen.** 
+
+Am Folgetag wird der Verkauf für alle geöffnet und die besten Plätze zu den niedrigsten Preisen können nicht mehr garantiert werden.
+ 
+@endcomponent
+
+**Donnerstag - 19. Mai, 11:00 Uhr**
+ 
+Offener Verkauf für alle - der Hallenplan mit den Preisen wird ab Donnerstag auf unserer Webseite verfügbar sein: https://ecommerceberlin.com/ 
+ 
+## Wie sieht der Buchungsprozess aus?
+ 
+Aufgrund des das Platzlimit übersteigenden Interesses setzen wir ein anderes Verkaufsmodell um.
+ 
+Jeder nachfolgende Pool wird automatisch aktiviert, nachdem das Limit des vorherigen Pools erreicht wurde. Es kann vorkommen, dass am ersten Tag ein neuerer, teurerer Salespool aktiviert wird, wenn der vorherige Pool bereits ausgebucht ist.
+ 
+Beste Grüße,
+ 
+{{$footer}}
+ 
+@endcomponent
---
+++ resources/views/emails/company/ebe7-sales-top30-lastminute-en.blade.php
@@ -0,0 +1,35 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+  
+@component('mail::panel')
+
+## Today - May 18, 11:00 am
+ 
+**Exclusive sale for the TOP30 companies** from the Exhibitor Contest. 
+
+**You are one of the 30 eligibled companies! 
+
+Book today via [ecommerceberlin.com/top30earlybird_t6d63sa9](https://ecommerceberlin.com/top30earlybird_t6d63sa9)**
+ 
+You will be able to book the exhibition space through secret link provided above - **till the end of the day.** 
+
+On the following day the sale will be open for everyone and we won’t be able to guarantee the best places at the lowest prices anymore.
+ 
+@endcomponent
+
+**Thursday - May 19, 11:00 am**
+ 
+Open sale for everyone - the floor plan with prices will be available from Wednesday
+ 
+## What does the booking process look like?
+ 
+Due to the interest exceeding the space limit, we implement a different sales model. 
+ 
+Each subsequent pool is activated automatically after the limit of the previous pool is reached. It may happen that on the first day new, more expensive sales pools, will be activated if the previous pool will be already booked out.
+ 
+Best regards, 
+ 
+{{$footer}}
+ 
+@endcomponent
