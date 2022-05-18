56a0c3d2e5f35b6fecb86ac7f6f3b8cf77297403
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-18 14:25:02 +0200

    teh22 nontop30 reminder

---
+++ resources/views/emails/company/teh22-sales-reminder-en.blade.php
@@ -0,0 +1,22 @@
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname")  }},
+
+We are rolling out the sales of the exhibition spaces for the upcoming 22. Ecommerce Warsaw Expo (October, 27th) this week! Here is the schedule: 
+
+## Wednesday - May 18, 11:00 am
+
+**Closed sales for TOP30** of the Contest for Exhibitors (if you are in this group, you will receive a special link at least 2 hours before the sale starts)
+
+Sales for exhibitors of the 21st Ecommerce EXPO interested in purchasing a bigger area (3 stands or more) - **let us know if your company is interested in such space**
+
+## Thursday - May 19, 11:00 am
+
+Start of the open sales - the map and prices will be available on Wednesday at https://targiehandlu.pl
+
+
+Best regards
+
+{{$footer}}
+
+@endcomponent
