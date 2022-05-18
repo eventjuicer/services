2d349a51b7c92299732be810becc6fad67305592
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-18 14:52:34 +0200

    Update teh22-sales-reminder-en.blade.php

--- resources/views/emails/company/teh22-sales-reminder-en.blade.php
+++ resources/views/emails/company/teh22-sales-reminder-en.blade.php
@@ -2,18 +2,11 @@
  
 # Hello {{ array_get($profile, "fname")  }},
 
-We are rolling out the sales of the exhibition spaces for the upcoming 22. Ecommerce Warsaw Expo (October, 27th) this week! Here is the schedule: 
+**Tomorrow (19th of May) 11:00 CET we are rolling out the open sales** of the exhibition spaces for the upcoming 22. Ecommerce Warsaw Expo (October, 27th)! 
 
-## Wednesday - May 18, 11:00 am
+Visit [targiehandlu.pl/exhibit](https://targiehandlu.pl/exhibit) to check available booths and prices.
 
-**Closed sales for TOP30** of the Contest for Exhibitors (if you are in this group, you will receive a special link at least 2 hours before the sale starts)
-
-Sales for exhibitors of the 21st Ecommerce EXPO interested in purchasing a bigger area (3 stands or more) - **let us know if your company is interested in such space**
-
-## Thursday - May 19, 11:00 am
-
-Start of the open sales - the map and prices will be available on Wednesday at https://targiehandlu.pl
-
+Some booths have been already reserved by TOP30 of the Exhibitor's Contest.
 
 Best regards
 
