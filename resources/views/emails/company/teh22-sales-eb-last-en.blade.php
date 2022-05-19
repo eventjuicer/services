6e7e2317120a144d006f624651db5ceabeb714d3
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-05-19 10:17:43 +0200

    teh22 last info re EB sales

---
+++ resources/views/emails/company/teh22-sales-eb-last-en.blade.php
@@ -0,0 +1,15 @@
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname")  }},
+
+**TODAY (19th of May) 11:00 CET we are rolling out the open sales** of the exhibition spaces for the upcoming 22. Ecommerce Warsaw Expo (October, 27th)! 
+
+Visit [targiehandlu.pl/exhibit](https://targiehandlu.pl/exhibit) to check available booths and prices.
+
+Some booths have been already reserved by TOP30 of the Exhibitor's Contest.
+
+Best regards
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/teh22-sales-eb-last-pl.blade.php
@@ -0,0 +1,16 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Dzisiaj - **w czwartek o 11:00** - rusza otwarta sprzedaż dla wszystkich zainteresowanych powierzchnią wystawienniczą na jesiennych Targach eHandlu (27 października w Warszawie - EXPO XXI)
+
+**Mapka i ceny dostępne na [targiehandlu.pl/exhibit](https://targiehandlu.pl/exhibit)**
+
+W dniu wczorajszym miała miejsce sprzedaż dla TOP30 konkursu dla Wystawców - stąd część stoisk została już zarezerwowana.
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
