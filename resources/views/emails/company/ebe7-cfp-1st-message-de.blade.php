c811b382f14d47395f12bbfd57e32163ed3db05c
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-09-07 12:02:33 +0200

    https://github.com/ecommerceberlin/ecommerceberlin.com/issues/200

---
+++ resources/views/emails/company/ebe7-cfp-1st-message-de.blade.php
@@ -0,0 +1,43 @@
+
+
+@component('mail::message')
+
+# Hallo {{ array_get($profile, "fname") }},
+
+Wir freuen uns, euch mitteilen zu können, dass der **E-Commerce Berlin Expo - Call for Papers Wettbewerb** gerade gestartet ist! Das bedeutet, ihr könnt bereits eure Präsentationsvorschläge einreichen, um Speaker auf einer der Expo-Bühnen zu werden.
+
+#Ein Vortrag auf einer der führenden E-commerce-Events Deutschlands vor über 10.000 Zuhörern wird dir helfen, eure Markenbekanntheit zu steigern, dein persönliches Profil zu stärken und hochwertige Verbindungen aufzubauen.
+
+
+Wir suchen datengetriebene Fallstudien zu folgenden Themen:
+> Tipps zu neuen Technologien, die wirklich funktionieren – mit Praxisbeispielen
+> Best Practices im Multichannel
+> Praktisch umsetzbare Erkenntnisse zur Datennutzung
+> Beispiele für erfolgreiche Personalisierung und ihre Auswirkungen auf das Kundenerlebnis
+> Grenzüberschreitende Chancen und erfolgreiche Expansion in ausländische Märkte
+> Weitere praktische Beispiele für gelöste E-Commerce-Probleme oder erzielte Erfolge
+
+Ein Hinweis - Präsentationen in einer **Case-Study-Formel**, die interessante Fakten für die Zusammenarbeit zwischen Dienstleistern und den Online-Shops zeigen, werden immer am meisten erwartet!
+
+
+Unserer Erfahrung nach freut sich das Publikum besonders auf innovative und wissensvermittelnde Themen, keine Sales-Pitch-Präsentationen, also vermeidet es bitte, diese einzureichen.
+
+Um ein Präsentationsvorschlag einzureichen, siehe den Link unten.
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking?utm_source=system_message&utm_medium=email&utm_campaign=system_message_email_cfp1"])
+Präsentationsvorschlag einreichen
+@endcomponent
+
+
+Die Frist für die Einreichung der Vorschläge ist der **30. September 2022**.
+
+Bei weiteren Fragen stehen wir gerne zur Verfügung.
+
+Mit besten Grüßen,
+
+{{$footer}}
+
+@endcomponent
+
+
+
---
+++ resources/views/emails/company/ebe7-cfp-1st-message-en.blade.php
@@ -0,0 +1,45 @@
+
+
+@component('mail::message')
+
+# Hello {{ array_get($profile, "fname") }},
+
+We are excited to announce that the **E-commerce Berlin Expo - Call for Papers contest** has just started! It means that you can already submit your presentation proposal to become a speaker on one of the stages at the Event.
+
+#Speaking at one of the most senior retail events in Germany in front of our 10000+ audience will help you grow your brand awareness, boost your personal profile and generate quality connections.
+
+We are looking for data-driven case studies that cover the following topics:
+
+> Tips on new technologies that really work – along with practical examples
+> Best practices in multi-channel
+> Actionable insights on the use of data
+> Examples of successful personalization and its impact on customer experience
+> Cross-border opportunities and successful expansion into foreign markets
+> Other practical examples of e-commerce related problems solved or successes achieved
+
+
+Just a hint - **case study formulas** showing examples of cooperation between service providers and the online stores are always the most expected ones! 
+
+The audience is not interested in listening to sales pitches, so please avoid submitting those, unless you have a great case to show.
+
+
+To start your submission, please follow the button below.
+
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking?utm_source=system_message&utm_medium=email&utm_campaign=system_message_email_cfp1"])
+Submit your presentation proposal
+@endcomponent
+
+
+The deadline for submitting proposals is **September 30, 2022**.
+
+
+Good luck!
+
+
+{{$footer}}
+
+@endcomponent
+
+
+
