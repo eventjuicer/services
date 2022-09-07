88de4bd09b9353d4dcd5f903f68e0cab0a551eee
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-09-07 12:16:54 +0200

    fixes

--- resources/views/emails/company/ebe7-cfp-1st-message-de.blade.php
+++ resources/views/emails/company/ebe7-cfp-1st-message-de.blade.php
@@ -6,19 +6,24 @@
 
 Wir freuen uns, euch mitteilen zu können, dass der **E-Commerce Berlin Expo - Call for Papers Wettbewerb** gerade gestartet ist! Das bedeutet, ihr könnt bereits eure Präsentationsvorschläge einreichen, um Speaker auf einer der Expo-Bühnen zu werden.
 
-#Ein Vortrag auf einer der führenden E-commerce-Events Deutschlands vor über 10.000 Zuhörern wird dir helfen, eure Markenbekanntheit zu steigern, dein persönliches Profil zu stärken und hochwertige Verbindungen aufzubauen.
+## Ein Vortrag auf einer der führenden E-commerce-Events Deutschlands vor über 10.000 Zuhörern wird dir helfen, eure Markenbekanntheit zu steigern, dein persönliches Profil zu stärken und hochwertige Verbindungen aufzubauen.
 
 
 Wir suchen datengetriebene Fallstudien zu folgenden Themen:
-> Tipps zu neuen Technologien, die wirklich funktionieren – mit Praxisbeispielen
-> Best Practices im Multichannel
-> Praktisch umsetzbare Erkenntnisse zur Datennutzung
-> Beispiele für erfolgreiche Personalisierung und ihre Auswirkungen auf das Kundenerlebnis
-> Grenzüberschreitende Chancen und erfolgreiche Expansion in ausländische Märkte
-> Weitere praktische Beispiele für gelöste E-Commerce-Probleme oder erzielte Erfolge
+
+* Tipps zu neuen Technologien, die wirklich funktionieren – mit Praxisbeispielen
+
+* Best Practices im Multichannel
+
+* Praktisch umsetzbare Erkenntnisse zur Datennutzung
+
+*  Beispiele für erfolgreiche Personalisierung und ihre Auswirkungen auf das Kundenerlebnis
+
+* Grenzüberschreitende Chancen und erfolgreiche Expansion in ausländische Märkte
+
+* Weitere praktische Beispiele für gelöste E-Commerce-Probleme oder erzielte Erfolge
 
 Ein Hinweis - Präsentationen in einer **Case-Study-Formel**, die interessante Fakten für die Zusammenarbeit zwischen Dienstleistern und den Online-Shops zeigen, werden immer am meisten erwartet!
-
 
 Unserer Erfahrung nach freut sich das Publikum besonders auf innovative und wissensvermittelnde Themen, keine Sales-Pitch-Präsentationen, also vermeidet es bitte, diese einzureichen.
 
--- resources/views/emails/company/ebe7-cfp-1st-message-en.blade.php
+++ resources/views/emails/company/ebe7-cfp-1st-message-en.blade.php
@@ -6,16 +6,21 @@
 
 We are excited to announce that the **E-commerce Berlin Expo - Call for Papers contest** has just started! It means that you can already submit your presentation proposal to become a speaker on one of the stages at the Event.
 
-#Speaking at one of the most senior retail events in Germany in front of our 10000+ audience will help you grow your brand awareness, boost your personal profile and generate quality connections.
+## Speaking at one of the most senior retail events in Germany in front of our 10000+ audience will help you grow your brand awareness, boost your personal profile and generate quality connections.
 
 We are looking for data-driven case studies that cover the following topics:
 
-> Tips on new technologies that really work – along with practical examples
-> Best practices in multi-channel
-> Actionable insights on the use of data
-> Examples of successful personalization and its impact on customer experience
-> Cross-border opportunities and successful expansion into foreign markets
-> Other practical examples of e-commerce related problems solved or successes achieved
+* Tips on new technologies that really work – along with practical examples
+
+* Best practices in multi-channel
+
+* Actionable insights on the use of data
+
+* Examples of successful personalization and its impact on customer experience
+
+* Cross-border opportunities and successful expansion into foreign markets
+
+*Other practical examples of e-commerce related problems solved or successes achieved
 
 
 Just a hint - **case study formulas** showing examples of cooperation between service providers and the online stores are always the most expected ones! 
