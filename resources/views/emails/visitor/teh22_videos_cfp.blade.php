0d2f02031d7b2d19c42f942e0f147a0faeac82fb
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-09-14 10:03:04 +0200

    teh22 cfp + videos

--- resources/views/emails/visitor/teh22_videos_cfp.blade.php
+++ resources/views/emails/visitor/teh22_videos_cfp.blade.php
@@ -1,42 +1,43 @@
-
-
 @component('mail::message')
  
 # Dzień dobry {{ $p->translate("[[fname]]") }},
 
 Kolejne Targi eHandlu odbędą się **27 października na EXPO XXI w Warszawie.**
 
-Aktualnie pracujemy nad agendą wystąpień. 
+Aktualnie pracujemy nad agendą wystąpień.
 
-**Chcesz wystąpić? Deadline na zgłoszenie mija dzisiaj - informacje poniżej!**
+## Chcesz wystąpić? Deadline na zgłoszenie mija dzisiaj o 23:59 - informacje poniżej!
 
 Udostępniamy pierwszą porcję nagrań video z 21. edycji Targów, która odbyła się w kwietniu. 
 
-
 <table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1633698528/c_1283_logotype.jpg" alt="" /></td>
-        <td>Damian Wiszowaty - CEO Gonito 
-            <strong><a href="https://ehandel.com.pl/rydwany-ognia-czyli-jak-wykorzystac-synergie-modeli-seller-i-vendor-w-serwisie-amazon,2202">Rydwany ognia, czyli jak wykorzystać synergię modeli Seller i Vendor w serwisie Amazon</a></strong>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1537364211/c_1282_logotype.png" alt="" /></td>
+        <td>Karol Wilczyński - Expert SEO @ Artefakt
+            <strong><a href="https://ehandel.com.pl/7-rzeczy-ktore-musisz-robic-aby-poprawic-sprzedaz-w-twoim-sklepie-jak-sxo-wplywa-na-konwersje-i-jak-to-robia-najlepsi,2253">
+            7 rzeczy, które musisz robić, aby poprawić sprzedaż w Twoim sklepie. Jak SXO wpływa na konwersję i jak to robią najlepsi?</a></strong>
        
     </td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1582620906/c_1705_logotype.png" alt="" /></td>
-        <td>Piotr Buszka - co-founder & CEO - feeCOMPASS 
-            <strong><a href="https://ehandel.com.pl/wirtualny-doradca-jak-poprawic-zadowolenie-klientow-i-konwersje-w-twoim-sklepie,2197">Wirtualny doradca. Jak poprawić zadowolenie klientów i konwersję w Twoim sklepie?</a></strong>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1523270597/c_1152_logotype.png" alt="" /></td>
+        <td>Katarzyna Sikorska - Business Relations Director @ Przelewy24 
+            <strong><a href="https://ehandel.com.pl/shopping-experience-kobiet-i-mezczyzn-ile-prawdy-jest-w-stereotypach,2251">
+            Shopping experience kobiet i mężczyzn. Ile prawdy jest w stereotypach?</a></strong>
         </td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1597311044/c_1726_logotype.png" alt="" /></td>
-        <td>Mateusz Wróbel - Head of Sales - LivePrice 
-            <strong><a href="https://ehandel.com.pl/zwiekszenie-sprzedazy-dzieki-automatyzacji-cen-w-sklepie,2195">Zwiększenie sprzedaży dzięki automatyzacji cen w sklepie</a></strong>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1554807308/c_1466_logotype.png" alt="" /></td>
+        <td>Witold Wrodarczyk - CEO @ Adequate Interactive Boutique 
+            <strong><a href="https://ehandel.com.pl/nie-daj-sie-zjesc-ciastkom,2246">
+                Nie daj się zjeść ciastkom</a></strong>
         </td>
     </tr>
     <tr>
-        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1554133278/c_1462_logotype.jpg" alt="" /></td>
-        <td>Krzysztof Marzec - CEO - DevaGroup 
-            <strong><a href="https://ehandel.com.pl/7-najczestszych-sposobow-przepalania-budzetu-w-google,2193">7 najczęstszych sposobów przepalania budżetu w Google</a></strong>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1631781578/c_1869_logotype.jpg" alt="" /></td>
+        <td>Tomasz Zabojszcz i Bartłomiej Mokrzyński - Nowe Kolory 
+            <strong><a href="https://ehandel.com.pl/emag-rumunia-duzy-rynek-mala-konkurencja-wysokie-marze,2245">
+            EMAG Rumunia - duży rynek, mała konkurencja, wysokie marże</a></strong>
         </td>   
     </tr>
 </table>
@@ -52,6 +53,38 @@ Chcę zostać Prelegentem na Targach eHandlu
 @endcomponent
 
 
+
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1646915121/c_1562_logotype.png" alt="" /></td>
+        <td>Marek Kich CEO Satisfly
+            <strong><a href="https://ehandel.com.pl/11-lekcji-z-11-lat-wdrozen-e-commerce,2240">
+            11 lekcji z 11 lat wdrożeń e-Commerce</a></strong>
+       
+    </td>
+    </tr>
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1627041204/c_1830_logotype.png" alt="" /></td>
+        <td>Michał Borzyszkowski - Ekspert ds. pozycjonowania @ Widoczni 
+            <strong><a href="https://ehandel.com.pl/optymalizacja-szybkosci-strony-a-core-web-vitals,2238">
+            Optymalizacja szybkości strony a Core Web Vitals</a></strong>
+        </td>
+    </tr>
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1647609659/c_1162_logotype.png" alt="" /></td>
+        <td>Grzegorz Maliszewski - Head of Business Development @ PromoTraffic
+            <strong><a href="https://ehandel.com.pl/dzwignia-w-seo-czyli-kreatywny-link-building-ktorego-boi-sie-twoja-konkurencja,2232">
+            Dźwignia w SEO, czyli kreatywny link building, którego boi się Twoja konkurencja!</a></strong>
+        </td>
+    </tr>
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_300,h_300,c_limit,f_auto/v1628842885/c_1837_logotype.png" alt="" /></td>
+        <td>Adam Miziołek - Country Manager @ Twisto
+            <strong><a href="https://ehandel.com.pl/wind-of-change-w-sprzedazy-online-i-offline-jak-platnosci-odroczone-zmieniaja-zakupy-i-zwiekszaja-przychody-sklepu,2233">
+            Wind of change w sprzedaży online i offline – jak płatności odroczone zmieniają zakupy i zwiększają przychody sklepu?</a></strong>
+        </td>   
+    </tr>
+</table>
 
 
 
