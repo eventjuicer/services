76e73c7037fa2eca5511b290c4acc585cf7086d2
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-06 12:36:55 +0200

    EMAIL agenda1

--- resources/views/emails/visitor/teh22-agenda1.blade.php
+++ resources/views/emails/visitor/teh22-agenda1.blade.php
@@ -6,13 +6,31 @@ Cześć {{ $p->translate("[[fname]]") }}!
 
 ## Właśnie opublikowaliśmy [listę prezentacji na scenach A i D](https://targiehandlu.pl/schedule)
 
-[Sprawdź!](https://targiehandlu.pl/schedule)
+**Scena A**
+
+| Godzina | Prelegent/-ka | Tytuł prezentacji |
+| --- | --- | --- |
+| 10:40 | Agnieszka Wąsowska **locatheart** | **Wielojęzyczne SEO w ekspansji zagranicznej**|
+| 11:15 | Beata Mordarska **Veneo Performance** | **„Śniło mi się, że je jadłam i znalazłam w żabce” – o tym jak Lody Koral Ekipa stały się viralem. DMF w praktyce.**|
+| 11:50 | Tomasz Zarychta **Media4U** | **Błędy w sklepach internetowych - nasze wnioski z przebadania ponad 100 sklepów**|
+| 12:40 | Mateusz Osiecki **PrestaShop** | **Od pomysłu do sukcesu: Rzeczy, które chcielibyście wiedzieć o PrestaShop ale baliście się zapytać.**|
+| 12:40 | Krystian Podemski **PrestaShop** | **Od pomysłu do sukcesu: Rzeczy, które chcielibyście wiedzieć o PrestaShop ale baliście się zapytać.**|
+| 13:15 | Sebastian Horoszko **Zaufane.pl** | **Niech moc społeczności będzie z Tobą! Social Proof na zagranicznym rynku**|
+| 14:05 | Dominika Rozprzak **adcookie** | **5 sposobów na efektywne skalowanie kampanii SEM – czyli jak zwiększyć przychody o 800% r/r.**|
+| 14:40 | Mikołaj Miszczak **SAMITO S.A.** | **Dlaczego marketing automation zawodzi w niektórych sklepach? Oto przykłady fail stories, o których się zwykle milczy**|
+| 15:30 | Barbara Tatar **Asseco Business Solutions S.A.** | **e-Commerce w branżach wymagających czyli czy e-Commerce jest dla wszystkich?**|
+| 16:05 | Cezary Kożon **Spyrosoft eCommerce** | **Nowoczesne wdrożenia eCommerce: Co zrobić by wdrożenie się udało oraz czego unikać?**|
+
 
 @component('mail::panel')
 Przypominamy, że 22. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
 @endcomponent
 
+Pozdrawiamy,
 
+Karolina Michalak
+
+Adam Zygadlewicz
 
 @endcomponent
 
