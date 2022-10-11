7bd49994742c0db0ada58a215144d86e447a97c1
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-11 13:32:38 +0200

    agenda for old visitors

---
+++ resources/views/emails/visitor/teh22-agenda1-for-old.blade.php
@@ -0,0 +1,103 @@
+
+
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+## Opublikowaliśmy [listę prezentacji na scenach A i D](https://targiehandlu.pl/visit)
+
+@component('mail::panel')
+Przypominamy, że 22. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny z prezentacjami Ekspertów** oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+@endcomponent
+
+
+## Scena A
+
+**locatheart**
+**Wielojęzyczne SEO w ekspansji zagranicznej**
+
+**Veneo Performance**
+Śniło mi się, że je jadłam i znalazłam w żabce” – o tym **„jak Lody Koral Ekipa stały się viralem. DMF w praktyce.**
+
+**Media4U**
+**Błędy w sklepach internetowych** - nasze wnioski z przebadania ponad 100 sklepów
+
+**PrestaShop**
+Od pomysłu do sukcesu: Rzeczy, które chcielibyście wiedzieć **o PrestaShop** ale baliście się zapytać.
+
+**Zaufane.pl**
+Niech moc społeczności będzie z Tobą! **Social Proof na zagranicznym rynku**
+
+**adcookie**
+**5 sposobów na efektywne skalowanie kampanii SEM** – czyli jak zwiększyć przychody o 800% r/r.
+
+**SAMITO S.A.**
+**Dlaczego marketing automation zawodzi w niektórych sklepach?** Oto przykłady fail stories, o których się zwykle milczy
+
+**Asseco Business Solutions S.A.**
+**e-Commerce w branżach wymagających** czyli czy e-Commerce jest dla wszystkich?
+
+**Spyrosoft eCommerce**
+Nowoczesne wdrożenia eCommerce: **Co zrobić by wdrożenie [eCommerce] się udało** oraz czego unikać?
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Zarejestruj się / Sprawdź agendę
+@endcomponent
+
+
+**Scena B**
+
+(wkrótce)
+
+**Scena C**
+
+(wkrótce)
+
+## Scena D
+
+
+**PayPo**
+ak sprzedawać więcej w czasach, kiedy klienci kupują mniej? **JZwiększ wartość koszyka zakupowego nawet o 60%.**
+
+**goodylabs**
+Zmarnowane szanse na konwersję kontra **Unified Experience.** Jak zmodyfikować strategię marki, żeby wesprzeć sprzedaż i poprawić doświadczenie użytkownika.
+
+**edrone**
+**Koniec epoki “search”**
+
+**Peoplefone Polska S.A.**
+**Ogranicz skutki inflacji w e-commerce, dzięki możliwościom telefonii internetowej.**
+
+**Yetiz Interactive**
+**"Trzy taktyki retencyjne, które pomogą Ci przetrwać kryzys”**
+
+**Grupa iCEA**
+**Międzynarodowe pozycjonowanie.** Czy agencja z Polski może robić dobre SEO na rynkach zagranicznych?
+
+**Linker Cloud Sp. z o.o.**
+Logistyka w eCommerce bez tajemnic: **Fulfillment jako narzędzie do szybszego dotarcia do klienta końcowego**
+
+**TrustMate S.A.**
+Cialdini ma rację! **O roli opinii w zwiększaniu sprzedaży e-commerce.**
+
+
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Zarejestruj się / Sprawdź agendę
+@endcomponent
+
+
+
+
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+@endcomponent
