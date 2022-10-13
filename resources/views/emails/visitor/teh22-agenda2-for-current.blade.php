9c4a62f3e26f080fe5c1a91d8481a4d90fc4cabc
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-13 15:32:46 +0200

    agenda for current participants

---
+++ resources/views/emails/visitor/teh22-agenda2-for-current.blade.php
@@ -0,0 +1,33 @@
+
+
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+Dziękujemy za rejestrację na Targi eHandlu.
+
+**W przyszłym tygodniu ruszą zapisy na warsztaty (2 strefy warsztatowe, na każdej po 7 warsztatów.)**
+
+## Dzisiaj opublikowaliśmy [kompletną listę prezentacji na scenach A, B, C i D](https://targiehandlu.pl/schedule), z którą warto się zapoznać by lepiej zaplanować swój dzień targowy.
+
+
+@component('mail::panel')
+
+Przypominamy, że 22. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny z prezentacjami Ekspertów** oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+@endcomponent
+
+# Bilety wstępu (PDF) będziemy rozsyłali najwcześniej ok 20 października.
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+
+
+
+@endcomponent
