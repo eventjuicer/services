daf135930806eef2b720e9c057c5c76a3a1a5529
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-17 12:15:17 +0200

    reps last call

---
+++ resources/views/emails/company/teh22-representatives-list-pl.blade.php
@@ -0,0 +1,38 @@
+
+
+@component('mail::message')
+ 
+# Cześć  {{ array_get($profile, "fname") }},
+
+**Jutro do 12:00 jest DEADLINE na dodanie przedstawicieli Wystawcy, którzy będą reprezentować firmę na Stoisku.** 
+
+Jutro o 12:01 wysyłamy identyfikatory do druku :)
+
+# Lista zdefiniowanych osób
+
+@forelse($representatives as $rep)
+
+	{{$rep->translate("[[fname]] [[lname]] [[position]]")}}
+
+@empty
+
+**Jeszcze nie zdefiniowano przedstawicieli**
+
+@endforelse
+
+@component('mail::button', ['url' => $accountUrlReps])
+Dodaj / usuń Przedstawicieli 
+@endcomponent
+
+Przypominamy, że w ramach wykupionej jednej przestrzeni wystawienniczej otrzymujesz maksymalnie 4 vouchery obiadowe w cenie stoiska i 1 kartę parkingową. 
+
+**Jeśli Przedstawicieli jest więcej niź 4 - dokup proszę vouchery obiadowe** (Panel wystawcy -> Udział w Targach -> Dodatkowe vouchery)
+
+Pozdrawiam,
+
+{{$footer}}
+
+@endcomponent
+
+
+
