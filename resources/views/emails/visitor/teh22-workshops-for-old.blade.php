56a1356b61198cf1aa3ba0f827df26a47eac5a28
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-18 12:39:48 +0200

    workshops for old

---
+++ resources/views/emails/visitor/teh22-workshops-for-old.blade.php
@@ -0,0 +1,83 @@
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+## Po raz pierwszy na Targach eHandlu organizujemy Warsztaty. ...aż 14 Warsztatów
+
+[Sprawdź listę planowanych Warsztatów](https://targiehandlu.pl/workshops)
+
+**Tematy**
+
+**E-paragony w e-commerce.** Jak to działa i dlaczego się opłaca?
+
+**Idealna kampania PLA** dla E-commerce. Jak ją zbudować?
+
+**Alibaba.com** to nie bajka! Poznaj tajniki sprzedaży na tej platformie
+
+**Jak zmniejszyć ilość zwrotów konsumenckich w branży Fashion?**
+
+Migruj, zarządzaj, integruj i **sprzedawaj na wielu marketplace'ach** używając jednego narzędzia.
+
+**Budżet marketingowy na Allegro**
+
+**Zmiana platformy eCommerce od A do Z** - jak skutecznie przeprowadzić migrację sklepu na inny silnik
+
+Jak wybrać **rynki dla cross-border e-commerce**?
+
+Kompleksowa **automatyzacja sprzedaży wielokanałowej** - poznaj autorski **system IDEA** commerce od środka 
+
+Jak w praktyce wdrożyć narzędzie do **marketing automation user.com**?
+
+Jak wycisnąć więcej z **email marketingu z GetResponse**?
+
+**AI i Big Data w praktyce via samba.ai** jak wykorzystać zgromadzone w ecommerce dane do zwiększenia liczby powracających klientów.
+
+**Data-driven UX design** - jak maksymalizować ROI platformy ecommerce
+
+**Twój esklep w fulfilmencie** - praktyczne strony wdrożenia i współpracy z operatorem logistycznym
+
+
+## Ważne informacje:
+
+**Udział** w Warsztatch w tej edycji będzie **bezpłatny.**
+
+**Zapisy** na warsztaty startują **w czwartek o 8:00.**
+
+Będzie można zapisać się na **maksymalnie 2 warsztaty.**
+
+**Prowadzący będzie musiał potwierdzić Twój udział** - otrzymasz powiadomienie jak tylko to nastąpi.
+
+Każdy Uczestnik Warsztatów **otrzymuje status VIP** (dostęp do Strefy VIP dla Wystawców i Prelegentów)
+
+W 1 Warsztacie może uczestniczyć **max 25 osób**
+
+Czas warsztatu to maksymalnie **45 minut** (30min + 15min na Q&A)
+
+
+**Niezależnie od warsztatów [dostępne są aż 4 sceny prezentacyjne](https://targiehandlu.pl/visit)** (udział bezpłatny, otwarty, wymagana rejestracja jako Zwiedzający)
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Zarejestruj się jako Zwiedzający
+@endcomponent
+
+@component('mail::panel')
+
+Przypominamy, że 22. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny** z prezentacjami Ekspertów oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+...**oraz 14 warsztatów**
+
+@endcomponent
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+
+@endcomponent
