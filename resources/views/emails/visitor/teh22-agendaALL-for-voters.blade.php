5a832f3119c649e58213094f11ea90a82bb25275
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-19 15:20:27 +0200

    newsletter voters

---
+++ resources/views/emails/visitor/teh22-agendaALL-for-voters.blade.php
@@ -0,0 +1,217 @@
+@component('mail::message')
+
+# Cześć {{ $fname }},
+
+Dziękujemy za Twój głos podczas wyboru Prelegentów Targów eHandlu.
+
+
+@component('mail::panel')
+
+22. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny z prezentacjami Ekspertów** oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+Podczas tej edycji planujemy także ponad 14 warsztatów.
+
+@endcomponent
+
+
+# Opublikowaliśmy [kompletną listę prezentacji na scenach A, B, C i D](https://targiehandlu.pl/visit)
+
+Naszym zdaniem **agenda jest rewelacyjna!** Sporo świeżości ...a Ty co uwazasz? :)
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Sprawdź całą agendę
+@endcomponent
+
+
+# Scena A
+
+**locatheart**
+
+**Wielojęzyczne SEO w ekspansji zagranicznej**
+
+**Veneo Performance**
+
+Śniło mi się, że je jadłam i znalazłam w żabce” – o tym **„jak Lody Koral Ekipa stały się viralem. DMF w praktyce.**
+
+**Media4U**
+
+**Błędy w sklepach internetowych** - nasze wnioski z przebadania ponad 100 sklepów
+
+**PrestaShop**
+
+Od pomysłu do sukcesu: Rzeczy, które chcielibyście wiedzieć **o PrestaShop** ale baliście się zapytać.
+
+**Zaufane.pl**
+
+Niech moc społeczności będzie z Tobą! **Social Proof na zagranicznym rynku**
+
+**adcookie**
+
+**5 sposobów na efektywne skalowanie kampanii SEM** – czyli jak zwiększyć przychody o 800% r/r.
+
+**SAMITO S.A.**
+
+**Dlaczego marketing automation zawodzi w niektórych sklepach?** Oto przykłady fail stories, o których się zwykle milczy
+
+**Asseco Business Solutions S.A.**
+
+**e-Commerce w branżach wymagających** czyli czy e-Commerce jest dla wszystkich?
+
+**Spyrosoft eCommerce**
+
+Nowoczesne wdrożenia eCommerce: **Co zrobić by wdrożenie [eCommerce] się udało** oraz czego unikać?
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Zarejestruj się / Sprawdź agendę
+@endcomponent
+
+
+# Scena B
+
+**InPost**
+
+Środowisko InPost: **One Stop Shop services**
+
+**Grupa Tense**
+
+Outsourcing a in-house. **Które działania SEO outsourcować**, a które wykonywać samodzielnie?
+
+**Przelewy24**
+
+Nie śpię, bo dbam o konwersję! **Jak „wycisnąć" maksimum z dostawcy płatności,** by zwiększać sprzedaż w czasach kryzysu?
+
+**FedEx**
+
+**Jak upewnić się, że Twoja przesyłka jest gotowa do zagranicznej podróży?**
+
+**Email Partners**
+
+Wszystkie drogi prowadzą do… konwersji. **Mailing, display, retargeting** - z Twoim klientem #NaKażdymKroku!
+
+**Trusted Shops Poland Sp. z o.o**
+
+Nowe przepisy dotyczące prezentacji opinii i rabatów w e-sklepach. Sprawdź, **na co uważać po wejściu w życie dyrektywy Omnibus.**
+
+**AtomStore**
+
+**6 błędów w ekspansji zagranicznej** w e-commerce i jak ich uniknąć
+
+**Omnipack**
+
+**Logistyka w e-commerce na czas kryzysu** 6 wskazówek jak zabezpieczyć biznes.
+
+**Ideo Force Sp. z o.o.**
+
+Optymalizacja doświadczeń użytkowników w oparciu o dane – czyli **data driven UX**
+
+@component('mail::panel')
+
+Przypominamy, że 22. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny z prezentacjami Ekspertów** oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+@endcomponent
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Sprawdź całą agendę
+@endcomponent
+
+
+# Scena C
+
+**Segmentify**
+
+**AI-driven Personalization**, czyli jak analityka i profilowanie klientów może wznieść Twój e-commerce na kolejny poziom.
+
+**Tribe47**
+
+Co po algorytmach? **Czas na kreatywną reklamę opartą na danych**
+
+**cyber_Folks**
+
+30 dni planowania kampanii po nocach i cały budżet w błoto? **Łatwe zmiany w sklepie, które wystrzelą Twoje konwersje w kosmos!**
+
+**IDEA commerce S.A.**
+
+**AI w e-commerce** - poznaj inteligentne rozwiązania dla biznesu online
+
+**Nethansa**
+
+Kijem Wisły nie zawrócisz- czyli **jak zautomatyzować strumień przychodów ze sprzedaży na Amazon i Kaufland.de**
+
+**Outvio**
+
+Ostatnia prosta w eCommerce: **Ujednolicona strategia po-checkoutowa**
+
+**Sempai**
+
+**SEO międzynarodowe. Optymalizacja sklepu pod wiele wersji językowych**
+
+**Exorigo-Upos SA; Amica, GoldenGrid**
+
+**Zaprojektowanie i wdrożenie nowego e-commerce Amica.pl. Jak to wyglądało "od kuchni"?**
+
+**Live.Market**
+
+**LiveCommerce - jak video zmienia oblicze sprzedaży online.**
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Zarejestruj się / Sprawdź agendę
+@endcomponent
+
+
+# Scena D
+
+**PayPo**
+
+ak sprzedawać więcej w czasach, kiedy klienci kupują mniej? **JZwiększ wartość koszyka zakupowego nawet o 60%.**
+
+**goodylabs**
+
+Zmarnowane szanse na konwersję kontra **Unified Experience.** Jak zmodyfikować strategię marki, żeby wesprzeć sprzedaż i poprawić doświadczenie użytkownika.
+
+**edrone**
+
+**Koniec epoki “search”**
+
+**Peoplefone Polska S.A.**
+
+**Ogranicz skutki inflacji w e-commerce, dzięki możliwościom telefonii internetowej.**
+
+**Yetiz Interactive**
+
+**"Trzy taktyki retencyjne, które pomogą Ci przetrwać kryzys”**
+
+**Grupa iCEA**
+
+**Międzynarodowe pozycjonowanie.** Czy agencja z Polski może robić dobre SEO na rynkach zagranicznych?
+
+**Linker Cloud Sp. z o.o.**
+
+Logistyka w eCommerce bez tajemnic: **Fulfillment jako narzędzie do szybszego dotarcia do klienta końcowego**
+
+**TrustMate S.A.**
+
+Cialdini ma rację! **O roli opinii w zwiększaniu sprzedaży e-commerce.**
+
+
+
+
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+
+
+@endcomponent
