f5c24f39698c2e9020e1fe267bf35c1d8c745214
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-20 09:14:27 +0200

    email current visitors - workshops apply

---
+++ resources/views/emails/visitor/teh22-workshops-apply-for-current.blade.php
@@ -0,0 +1,47 @@
+
+
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+## [Zgłoszenia na Warsztaty](https://targiehandlu.pl/workshops) wystartowały!
+
+Zarejestrowani uczestnicy (jak Ty) otrzymują oficjalną informację jako pierwsi.
+
+Ważne informacje:
+
+Prowadzący Warsztaty dostali prośbę by akceptować **nie więcej niż 7 zgłoszeń dziennie** (by dać każdemu szansę). Otrzymasz wiadomość przy akceptacji lub odmowie Twojego udziału.
+
+**Możesz się zgłosić do maksymalnie 2 warsztatów** (jeśli otrzymasz odmowy udziału Twój limit się odnowi)
+
+**Udział** w Warsztatch w tej edycji jest **bezpłatny.**
+
+Każdy Uczestnik Warsztatów **otrzymuje status VIP** (dostęp do Strefy VIP dla Wystawców i Prelegentów)
+
+W 1 Warsztacie może uczestniczyć **max 25 osób** i nie zwiększymy tego limitu.
+
+Czas warsztatu to maksymalnie **45 minut** (30min + 15min na Q&A)
+
+**Niezależnie od warsztatów [dostępne są aż 4 sceny prezentacyjne](https://targiehandlu.pl/schedule)** (udział bezpłatny, otwarty)
+
+
+@component('mail::panel')
+
+Przypominamy, że 22. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny** z prezentacjami Ekspertów oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+...**oraz 14 warsztatów**
+
+@endcomponent
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+PS: **Bilety wstępu (PDF)** będziemy rozsyłali **najwcześniej ok 20 października** (w czwartek)
+
+
+@endcomponent
