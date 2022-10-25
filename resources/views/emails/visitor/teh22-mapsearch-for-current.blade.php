020855ff943739b7c73fb236d46990dd313ae6bb
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-25 16:19:46 +0200

    teh22 email mapsearch for current

---
+++ resources/views/emails/visitor/teh22-mapsearch-for-current.blade.php
@@ -0,0 +1,54 @@
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+Kolejna porcja informacji dla zarejestrowanych Uczestników czwartkowych Targów eHandlu.
+
+
+@component('mail::panel')
+
+Targi eHandlu już w **czwartek 27 października na EXPO XXI w Warszawie.**
+
+Mamy **149 Wystawców**, **35 Prelegentów** 
+
+Pierwsze prezentacje startują o 10:40. 
+
+**Tylko osoby fizycznie obecne na Targach otrzymają dostęp do wszystkich nagrań prezentacji ze scen A, B, C i D** 
+
+@endcomponent
+
+
+Właśnie **[uruchomiliśmy możliwość umawiania się na spotkanie z Wystawcą](https://targiehandlu.pl/exhibitors) i wyszukiwania firm po nazwie lub branży** , w której działają.
+
+Jak umówić się na spotkanie z Wystawcą?
+
+(3a) Wejdź na [https://targiehandlu.pl/exhibitors](https://targiehandlu.pl/exhibitors). Znajdź interesującą Cię firmę.
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_1200,h_1000,c_limit,f_auto/v1666696812/www/teh22_mapsearch_keyword.png" alt="" style="max-width: 700px;"/>
+
+(3b) Kliknij na stoisko, kliknij przycisk "Umów się na spotkanie."
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_1200,h_1000,c_limit,f_auto/v1666696812/www/teh22_mapsearch_invite.png" alt="" style="max-width: 700px;"/>
+
+**Jeśli Wystawca zaakceptuje Twoją propozycję otrzymasz możliwość korzystania ze strefy VIP.**
+
+
+@component('mail::panel')
+
+**7 godzin na aktywnie spędzonych na Targach eHandlu to ekwiwalent tygodni** spędzonych na komunikacji z oferentami usług i produktów do e-handlu i czytaniu artykułów branżowych.
+
+@endcomponent
+
+
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+
+
+
+
+@endcomponent
