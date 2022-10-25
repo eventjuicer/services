d411e8550530caca9b119c8ab4420be2b3d00008
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-10-25 13:58:07 +0200

    teh22 email visitors mapsearch

---
+++ resources/views/emails/visitor/teh22-mapsearch-for-old.blade.php
@@ -0,0 +1,54 @@
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+@component('mail::panel')
+
+Targi eHandlu już w czwartek 27 października na EXPO XXI w Warszawie.
+
+Pierwsze prezentacje startują o 10:40.
+
+Mamy 149 Wystawców, 35 Prelegentów. 
+
+@endcomponent
+
+
+## Oto 3 powody by zawitać do EXPO XXI w czwartek:
+
+(1) Tylko osoby fizycznie obecne na Targach otrzymają dostęp do wszystkich nagrań prezentacji ze scen A, B, C i D
+
+(2) [Mamy świetną agendę...](https://targiehandlu.pl/schedule) sporo świeżych firm, dużo nowych tematów. 
+
+(3) Właśnie **[uruchomiliśmy możliwość umawiania się na spotkanie z Wystawcą](https://targiehandlu.pl/exhibitors) i wyszukiwania firm po nazwie lub branży, w której działają.** 
+
+(3a) Wejdź na [https://targiehandlu.pl/exhibitors](https://targiehandlu.pl/exhibitors). Znajdź interesującą Cię firmę.
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_1200,h_1000,c_limit,f_auto/v1666696812/www/teh22_mapsearch_keyword.png" alt="" style="max-width: 700px;"/>
+
+
+(3b) Kliknij na stoisko, umów się na spotkanie. **Jeśli Wystawca zaakceptuje Twoją propozycję otrzymasz możliwość korzystania ze strefy VIP.**
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_1200,h_1000,c_limit,f_auto/v1666696812/www/teh22_mapsearch_invite.png" alt="" style="max-width: 700px;"/>
+
+
+## 7 godzin na aktywnie spędzonych na Targach eHandlu to ekwiwalent tygodni spędzonych na komunikacji z oferentami usług i produktów do e-handlu i czytaniu artykułów branżowych.
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Sprawdź całą agendę i zarejestruj się.
+@endcomponent
+
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Adam Zygadlewicz
+
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+
+
+@endcomponent
