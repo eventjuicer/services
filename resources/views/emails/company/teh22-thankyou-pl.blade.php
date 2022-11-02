9f0e6f75f81ec8d3bbb58dc1f348327582a5474a
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-11-02 13:07:55 +0100

    teh22 survey

---
+++ resources/views/emails/company/teh22-thankyou-pl.blade.php
@@ -0,0 +1,25 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Dziękujemy za udział w 22. Targach eHandlu! 
+
+Zabieramy się za sortowanie zdjęć z wydarzenia i planowanie kolejnej - wiosenne - edycji.
+
+Mamy prośbą o wyrażenie swojej opinii na temat minionej edycji - link do ankiety poniżej.
+
+@component('mail::button', ['url' => "https://docs.google.com/forms/d/e/1FAIpQLSdCX0JmQDrQ9pAxcmFZlLXHH9Y3UumfpkjVsbJEGnVC3qv56Q/viewform"])
+Ankieta
+@endcomponent
+
+Jeśli Twój zespół skanował identyfikatory zwiedzających - [są one dostępne do pobrania w panelu Wystawcy.]({!! $accountUrlScans !!})
+
+
+Regards, 
+
+{{$footer}}
+
+@endcomponent
+
