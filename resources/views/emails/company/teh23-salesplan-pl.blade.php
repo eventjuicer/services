5c9ff50694d94556cf130275d4155914daef3bdc
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-11-09 13:20:25 +0100

    salesplan

---
+++ resources/views/emails/company/teh23-salesplan-pl.blade.php
@@ -0,0 +1,29 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na 23 Targi eHandlu (6 kwietnia 2023 w Warszawie - EXPO XXI)
+
+## Od dzisiaj do wtorku - Sprzedaż większych powierzchni 
+
+Sprzedaż dla Wystawców 22. Targów eHandlu zainteresowanych większą przestrzenią (3 i więcej stoisk) 
+
+[Układ hali](https://res.cloudinary.com/eventjuicer/image/upload/v1667996085/teh23_floorplan.png)
+
+**Prośba o info zwrotne jeśli taką przestrzenią zainteresowana jest Twoja firma**
+
+## Środa - 16 listopada 11:00
+
+**Sprzedaż zamknięta dla TOP30** Konkursu dla Wystawców (jeśli znajdujesz się w tym gronie to specjalny link otrzymasz najpóźniej 2h przed startem sprzedaży)
+
+## Czwartek - 17 listopada 11:00
+
+Sprzedaż otwarta dla wszystkich zainteresowanych - mapka i ceny dostępne będą w środę (16 listopada) na https://targiehandlu.pl 
+
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
