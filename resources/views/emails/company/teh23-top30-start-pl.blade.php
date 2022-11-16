a32ef4db4a2203013763b007b4739b007cb00e44
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-11-16 10:56:13 +0100

    teh23 top30 start

---
+++ resources/views/emails/company/teh23-top30-start-pl.blade.php
@@ -0,0 +1,28 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+**Wystartowała możliwość rezerwacji przestrzeni wystawienniczej** dla Twojej firmy i innych firm, 
+które znalazły się w TOP30 rankingu Konkursu dla Wystawców, 
+
+
+**Rezerwacja jest możliwa tylko i wyłącznie przez stronę** [targiehandlu.pl/top30_23as4w7S](https://targiehandlu.pl/top30_23as4w7S)
+
+...i potrwa do końca dnia.
+
+
+UWAGA: system nie pozwala na jednoczesną rezerwację 2 stoisk z tej samej kategorii cenowej (np 2x HOT, 2x ULTRA). W przypadku chęci rezerwacji stoisk w takiej konfiguracji prosimy o oddzielne transakcje.
+
+UWAGA (2): niedostępne przestrzenie (oznaczone kolorem szarym) - to stoiska z przedsprzedaży "hurtowej" (>2 przestrzeni) lub dostępne w terminie późniejszym.
+
+
+
+**W czwartek uruchamiamy publiczną sprzedaż dla pozostałych Wystawców** ubiegłej edycji Targów eHandlu oraz innych zainteresowanych.
+
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
