5236ac303775bdf1e7be22835bbc085707d6338f
Adam Zygadlewicz <adam@zygadlewicz.com>
2022-11-16 09:34:04 +0100

    Update teh23-top30-lastminute-pl.blade.php

--- resources/views/emails/company/teh23-top30-lastminute-pl.blade.php
+++ resources/views/emails/company/teh23-top30-lastminute-pl.blade.php
@@ -4,13 +4,18 @@
 
 
 Możliwość rezerwacji przestrzeni wystawienniczej dla Twojej firmy i innych firm, 
-które znalazły się w TOP30 rankingu Konkursu dla Wystawców, **zaplanowana jest na DZISIAJ (tj środę 16 listopada) - od godziny 11:00.**
+które znalazły się w TOP30 rankingu Konkursu dla Wystawców, 
+**zaplanowana jest na DZISIAJ (tj środę 16 listopada) - od godziny 11:00.**
 
 **Rezerwacja będzie możliwa tylko i wyłącznie przez stronę** [targiehandlu.pl/top30_23as4w7S](https://targiehandlu.pl/top30_23as4w7S)
 
 ...i potrwa do końca dnia.
 
 W czwartek uruchamiamy otwartą sprzedaż dla pozostałych Wystawców ubiegłej edycji Targów eHandlu oraz innych zainteresowanych.
+
+UWAGA: system nie pozwala na jednoczesną rezerwację 2 stoisk z tej samej kategorii cenowej (np 2x HOT, 2x ULTRA). W przypadku chęci rezerwacji stoisk w takiej konfiguracji prosimy o oddzielne transakcje.
+
+UWAGA (2): niedostępne przestrzenie (oznaczone kolorem szarym) - to stoiska z przedsprzedaży "hurtowej" (>2 przestrzeni) lub dostępne w terminie późniejszym.
 
 Pozdrowienia, 
 
