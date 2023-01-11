3aff149924f9a23a9641e76c48baa41809be2a81
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-01-11 11:52:05 +0000

    Update ebe-invite2.blade.php

--- resources/views/emails/exhibitor/ebe-invite2.blade.php
+++ resources/views/emails/exhibitor/ebe-invite2.blade.php
@@ -11,7 +11,7 @@
 
 We would like to let you know that **{{  array_get($companydata, "name") }} is exhibiting at the E-Commerce Berlin Expo 2023** which will take place on **23rd of February** at STATION Berlin, Luckenwalder Str. 4-6.
 
-**Get your free ticket** now and stop by our stand to chat with us - **click here to register:**
+**Get your free ticket** now and stop by our stand to chat with us:**
 
 @component('mail::button', ['url' => $promolink])
 	Click here to register for a free ticket
@@ -28,7 +28,7 @@ E-commerce Berlin Expo enables you to connect with other like-minded individuals
 Join us for an unforgettable experience that will take your business to the next level!
 
 @component('mail::button', ['url' => $promolink])
-	Click here to register for a free ticket
+	Click here to get a free entry
 @endcomponent
 
 
