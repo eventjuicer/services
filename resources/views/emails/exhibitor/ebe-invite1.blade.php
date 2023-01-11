7b7d09796bbbd87dde7140781d34a3c362693467
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-01-11 11:43:24 +0000

    updated email templates

--- resources/views/emails/exhibitor/ebe-invite1.blade.php
+++ resources/views/emails/exhibitor/ebe-invite1.blade.php
@@ -6,41 +6,39 @@
 
 <div style="text-align: center;">
 	<a href="{{$promolink}}" target="_blank">
-		<img src='https://res.cloudinary.com/ecommerceberlin/image/upload/f_auto/v1648205241/6_get_your_ticket_646x220.png' alt="" style="margin-top: 10px; margin-bottom: 30px; max-height: 250px;" />
+		<img src='{{$imageEnc}}' alt="" style="margin-top: 10px; margin-bottom: 30px; max-height: 250px;" />
 	</a>
 </div>
 
+## Hallo,
 
-# Hallo,
+Wir freuen uns, Dir mitteilen zu können, dass wir als Aussteller auf der E-Commerce Berlin Expo 2023 auch mit dabei sind und wollen Dich einladen, uns am 23. Februar in der STATION Berlin, Luckenwalder Str. 4-6 persönlich zu besuchen.
 
-## Wir wollten dich wissen lassen, dass {{  array_get($companydata, "name") }} bei der kommenden E-commerce Berlin Expo am **05.05.2022** in der STATION Berlin (Luckenwalder Str. 4-6) ausstellen wird.
+Mit dieser E-Mail wollen wir Dich persönlich zum Besuch einladen. Wir hoffen, dich an unserem Stand auf der Expo zu sehen!
 
-Diese E-Mail soll als persönliche Einladung unsererseits verstanden werden. Wir hoffen dich an unserem Stand begrüßen zu dürfen!
-
-##Warum lohnt sich ein Besuch?
-
-Entscheidungsträger aus dem Digitalen Commerce, Marketing und Innovation werden sich bei der E-commerce Berlin Expo versammeln. Über **200 Aussteller** und **50 führende Experten** werden innovative und zukunftsorientierte Lösungen und Trends präsentieren.
-
-Experten von **Zalando, MediaMarktSaturn, Douglas, NBB, KaDeWe, TUI, MyToys, Otto** werden euch neue Wege ebnen, um den Wachstum eures Geschäfts zu fördern.
-
-Die E-commerce Berlin Expo ist eure Chance um auf den neuesten Stand zu kommen und zukunftsorientierter zu agieren.
-
-Die Teilnahme und das damit verbundene Networking, der gemeinsame Dialog als auch die Inspirationsgestaltung sind kostenlos. Hole dir jetzt dein Ticket und besuche uns auf unserem Stand!
+Sichere dir jetzt dein kostenloses Ticket und komm an unserem Stand vorbei, um sich mit uns zu unterhalten - hier geht's zur Anmeldung:
 
 @component('mail::button', ['url' => $promolink])
 	Klick hier, um dich für ein kostenloses Ticket zu registrieren
 @endcomponent
 
-Hast du die Agenda bereits gesehen? Wenn nicht, dann klick auf: [https://ecommerceberlin.com/schedule](https://ecommerceberlin.com/schedule)
+Warum ein Besuch bei uns lohnt?
 
-**Die Expo als auch die Konferenz sind kostenlos.** Jedoch müssen die Besucher sich vorab auf der Homepage registrieren.
+Auf der E-Commerce Berlin Expo treffen sich alle wichtigen Entscheidungsträger aus den Bereichen E-Commerce, Marketing und Innovation zu einem einmaligen Networking-Erlebnis.
 
-Egal, vor welcher E-commerce-Herausforderung du stehst, du und deine Kollegen bekommen einen herzlichen Empfang, E-commerce-Experten und die beste Beratung unter einem Schirm!
+**Über 250 Unternehmen** stellen aus und mehr als **70 führende Experten** von Branchenriesen wie **Borussia Dortmund, Zalando, Meta, OMIO, eBay, EVENTIM, Mercedes-Benz Cars, Paul Hewitt, Amorelie, Farfetch und Thomann.io** präsentieren innovative und zukunftsweisende Entwicklungen und Trends, die dir helfen werden, neue Möglichkeiten für das Wachstum deines Onlinegeschäfts zu finden.
 
-Wir freuen uns dich zu treffen! 
+Die E-Commerce Berlin Expo bietet die Chance, mit Gleichgesinnten in Kontakt zu treten, die nach Erfolg streben, langfristige Partnerschaften zu schließen und das eigene Wissen und Können zu erweitern, um der Konkurrenz einen Schritt voraus zu sein.
 
-Mit freundlichen Grüßen,
+Klingt gut?
 
+Melde dich für den ganzen Tag voller Networking, Dialog, Bildung und Inspiration an - und das alles völlig kostenlos! Alles, was du tun musst, ist, dich im Voraus auf der offiziellen Website des Events zu registrieren.
+
+
+
+Komm und erlebe mit uns ein unvergessliches Abenteuer, das dein Unternehmen auf die nächste Stufe bringt!
+
+Wir freuen uns auf die Begegnung mit Dir!
 
 @endcomponent
 
--- resources/views/emails/exhibitor/ebe-invite2.blade.php
+++ resources/views/emails/exhibitor/ebe-invite2.blade.php
@@ -1,47 +1,40 @@
-
-
 @component('mail::message')
-
 
 
 <div style="text-align: center;">
 	<a href="{{$promolink}}" target="_blank">
-	<img src='https://res.cloudinary.com/ecommerceberlin/image/upload/f_auto/v1648205241/6_get_your_ticket_646x220.png' alt="" style="margin-top: 10px; margin-bottom: 30px; max-height: 250px;" />
+	<img src='{{$imageEnc}}' alt="" style="margin-top: 10px; margin-bottom: 30px; max-height: 250px;" />
 	</a>
 </div>
 
-
 # Hi,
 
-## We would like to let you know that {{  array_get($companydata, "name") }} is exhibiting at the E-Commerce Berlin Expo 2022 on **5th May** at STATION Berlin, Luckenwalder Str. 4-6.
+## We would like to let you know that {{  array_get($companydata, "name") }} is exhibiting at the E-Commerce Berlin Expo 2023 which will take place on **23rd of February** at STATION Berlin, Luckenwalder Str. 4-6.
 
 Please consider this email your personal invitation from us. We hope to see you at our booth during the Expo!
 
-##Why should you visit?
-
-All the major decision-makers from digital commerce, marketing, and innovation come together at E-commerce Berlin Expo. Over **200 companies** will exhibit and **50+ leading experts** will present innovative and forward-looking developments and trends.
-
-Experts from **Zalando, MediaMarktSaturn, Douglas, NBB, KaDeWe, TUI, MyToys, Otto** will help you find new opportunities to grow your e-commerce business.
-
-E-commerce Berlin Expo is your chance to bring yourself up-to-date and future-proof.
-
-You can register for the full day of networking, dialogue, education, and inspiration for free. Get your ticket now and stop by our stand to chat with us.
+**Get your free ticket** now and stop by our stand to chat with us - **click here to register:**
 
 @component('mail::button', ['url' => $promolink])
 	Click here to register for a free ticket
 @endcomponent
 
-If you haven't seen the full agenda yet, click here: [https://ecommerceberlin.com/schedule](https://ecommerceberlin.com/schedule)
 
-The expo and conference are **free of charge**, but participants must register in advance on the official website of the event.
+##Why should you visit?
 
-No matter what E-commerce challenge you're facing, you and your colleagues will find a warm welcome, E-commerce experts, and the best advice under one roof!
+All the major decision-makers from digital commerce, marketing, and innovation come together at E-commerce Berlin Expo for a one-of-a-kind networking experience.
 
-We’re looking forward to meeting you!
+**Over 250 companies will exhibit** and **over 70 leading experts** from giants such as **Borussia Dortmund, Zalando, Meta, OMIO, eBay, EVENTIM, Mercedes-Benz Cars, Paul Hewitt, Amorelie, Farfetch and Thomann.io,** will present innovative and forward-looking developments and trends to will help you find new opportunities to grow your e-commerce business.
 
-Kind Regards,
+E-commerce Berlin Expo enables you to connect with other like-minded individuals striving for success, find partnerships that will last for years, as well as expand your knowledge and skillset to stay ahead of the competition.
 
+Sounds good?
+
+You can register for the full day of networking, dialogue, education, and inspiration - and all that completely free of charge! All you have to do is register in advance on the official website of the event.
+
+
+Join us for an unforgettable experience that will take your business to the next level!
+
+We're looking forward to meeting you!
 
 @endcomponent
-
-
