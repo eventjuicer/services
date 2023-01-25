9824319cfc48c8a5bd56f51e5fcaa678bd27a95f
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-01-25 14:00:18 +0100

    Update ebe7-promonija_c2p-de.blade.php

--- resources/views/emails/company/ebe7-promonija_c2p-de.blade.php
+++ resources/views/emails/company/ebe7-promonija_c2p-de.blade.php
@@ -4,9 +4,9 @@
 
 Vor nicht allzu langer Zeit haben wir maßgeschneiderte Promomaterialien für euch zur Verfügung gestellt und die Möglichkeit für die Besucher, euch nach einem Meeting zu fragen. Das war jedoch noch nicht alles. 
 
-Hiermit freuen wir uns, ein weiteres Tool für unsere Aussteller vorstellen zu können, mit dem man die Zahl der Leads auf der Expo erhöhen kann. Um dem Ganzen den richtigen Schwung zu geben, könnt ihr jetzt die Liste der registrierten Expo-Teilnehmer durchgehen und erfahren, wer kommen wird!
+Hiermit freuen wir uns, **ein weiteres Tool für unsere Aussteller vorstellen zu können, mit dem man die Zahl der Leads auf der Expo erhöhen kann. Um dem Ganzen den richtigen Schwung zu geben, könnt ihr jetzt die Liste der registrierten Expo-Teilnehmer durchgehen und erfahren, wer kommen wird!**
 
-Das Beste kommt noch: Wenn ihr eure Präsenz auf der Messe über die von uns gelieferten Links bewirbt, bekommt ihr die Chance, **bis zu 200 Treffen mit registrierten Besuchern eurer Wahl zu vereinbaren, wie auch die Möglichkeit, ** unzählige Einladungen seitens der Besucher zu bekommen**. In beiden Fällen werden euch die vollständigen Kontaktdaten der jeweiligen Person zur Verfügung gestellt. 
+Das Beste kommt noch: Wenn ihr eure Präsenz auf der Messe über die von uns gelieferten Links bewirbt, bekommt ihr die Chance, **bis zu 200 Treffen mit registrierten Besuchern** eurer Wahl zu vereinbaren, wie auch die Möglichkeit, ** unzählige Einladungen seitens der Besucher zu bekommen**. In beiden Fällen werden euch die vollständigen Kontaktdaten der jeweiligen Person zur Verfügung gestellt. 
 
 **Hierfür schlagen wir vor, einen kurzen Termin mit dem zuständigen Account Manager zu vereinbaren, um alle aufkommenden Fragen zu besprechen um zu erfahren, wie man das Beste aus den von uns gelieferten Möglichkeiten herausziehen kann**.
 
