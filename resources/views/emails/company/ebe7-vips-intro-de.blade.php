2d3bc679dc5af6f284134021c4223b287d981819
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-01-31 12:37:43 +0100

    ebe vips

---
+++ resources/views/emails/company/ebe7-vips-intro-de.blade.php
@@ -0,0 +1,29 @@
+@component('mail::message')
+
+# Hallo {{ array_get($profile, "fname")  }}
+
+Auf der Aussteller-Plattform wurde eine Funktion für VIP-Einladungen eingeführt.
+
+## Lade jetzt deine VIPs zur exklusiven VIP-Zone auf der Messe ein. 
+
+Die VIP-Zone ist ein Ort für komfortable Gespräche hinter den Kulissen zwischen Speakern, Ausstellern, ausgewählten VIP Besuchern.
+
+**Jeder Firmenvertreter hat Zugang zur VIP-Zone - verwende keine VIP-Einladungen hierfür**.
+
+@component('mail::button', ['url' => $accountUrlVips])
+VIP-Einladungen für Partner und Kunden
+@endcomponent
+
+VIP-Einladungen können auf 2 Wegen verteilt werden:
+
+* automatischer Versand über die Plattform (einfach die E-Mail-Adresse des Empfängers eingeben)
+
+* Kopieren eines individuellen Links auf der Ausstellerplattform und Versand über eigene Kanäle.
+
+Du hast 5 Einladungen als Grundlage. **Indem euer öffentliches Ausstellerprofil bewerbt, könnt ihr bis zu 20 VIP-Einladungen zur Nutzung erhalten.**
+
+Beste Grüße,
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/ebe7-vips-intro-en.blade.php
@@ -0,0 +1,30 @@
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname")  }},
+
+A VIP invitations feature has been launched on the Exhibitor's platform.
+
+## Invite your VIPs to the exclusive VIP Zone at the Fair. 
+
+The VIP Zone is the place for comfortable, behind-the-scenes conversations between Speakers, Exhibitors and selected VIP Visitors.
+
+**Every company representative has access to the VIP Zone - do not use VIP invitations for this purpose.**
+
+@component('mail::button', ['url' => $accountUrlVips])
+VIP invitations for Partners and Customers
+@endcomponent
+
+VIP invitations can be distributed in 2 ways:
+
+* automatic dispatch from the platform (just enter the recipient's email address)
+
+* copying an individual link from the Exhibitor's platform and sending via your own channels.
+
+You get 5 invitations to start. By promoting your public company profile ("promo materials"), you can easily get up to 20 invitations.
+
+Regards, 
+
+{{$footer}}
+
+@endcomponent
+
