085de016eed0e32cc55d166e0428d0f950defc10
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-02 13:28:28 +0100

    ebe7 promoninja

---
+++ resources/views/emails/company/ebe7-promoninja-ping-de.blade.php
@@ -0,0 +1,56 @@
+@component('mail::message')
+
+Hallo {{ array_get($profile, "fname")  }}, 
+
+Die maximale Rendite eurer Investition auf der Messe hängt nicht nur von der Lage oder der Attraktivität des Standes ab! :) Es ist entscheidend, Interesse zu wecken und den Verkehr zu eurem Stand zu lenken, indem ihr für Aufregung rund um eure Teilnahme an der Veranstaltung sorgt.
+
+
+**Nutzt die von uns bereitgestellten Tools, um eure Dienstleistungen dem Publikum zu präsentieren**. Schaut euch an, was wir zu bieten haben:
+
+
+
+**VIP-Einladungen** (Ladet eure wichtigsten Partner und Interessenten zur Teilnahme an der Expo ein und verwöhnt sie mit dem Zugang zur exklusiven VIP-Zone mit Sofas und Erfrischungen)
+ 
+**Ausstellerprofil** (Teilt euer Ausstellerprofil auf euren Social-Media-Kanälen, um eure Fläche in der Mappe zu präsentieren und Einladungsanfragen für Meetings von den potentiellen Kunden zu erhalten)
+
+**Zugriff auf die Liste der registrierten Teilnehmer** (Durchsucht die Liste der aktuell registrierten Besucher und sendet Meetingeinladungen, um Termine am Tag der Expo zu planen)
+
+**Alle diese Funktionen sind im Aussteller-Profil verfügbar und warten darauf, von euch genutzt zu werden!**
+
+Ein kurzes Update: 
+
+# VIP Einladungen 
+Der Top-Verteiler der VIP-Einladungen ist derzeit **Firmenname** - Wow!
+
+@component('mail::button', ['url' => $accountUrlVips])
+Versende VIP-Einladungen an Partner und Kunden
+@endcomponent
+
+# Die TOP 5 <Stand 00:00 Uhr>-Unternehmen, die aktiv für ihre Präsenz auf der Expo werben und unsere Tools nutzen (mit zusätzlichen Dienstleistungen belohnt!), sind:
+
+* 
+
+* 
+
+* 
+
+*
+
+*
+
+**Wir freuen uns sehr, dass ihr die Bedeutung der Lead-Generierung versteht und unsere Tools nutzt, um eure Präsenz auf der Veranstaltung zu maximieren!**
+
+@component('mail::button', ['url' => $accountUrlPromote])
+Siehe Werbematerialien und Ranking-Seite
+@endcomponent
+
+An alle anderen da draußen, ihr habt noch Zeit, für eure Präsenz bei unserer Veranstaltung zu werben! Am einfachsten geht das, indem ihr einfach den folgenden Link mit eurer PR- & Social-Media-Abteilung teilt (Zugriff auf das restliche Aussteller-Panel ausgenommen).
+
+{!! $accountUrlPromotePublic !!}
+
+Liebe Grüße, 
+
+{{$footer}}
+
+@endcomponent
+
---
+++ resources/views/emails/company/ebe7-promoninja-ping-en.blade.php
@@ -0,0 +1,62 @@
+@component('mail::message')
+
+Hi {{ array_get($profile, "fname")  }}, 
+
+Maximizing the return of your investment at the fair depends not only on the location or attractiveness of the stand! :) It’s crucial to generate interest and drive traffic to your booth by creating a buzz around your participation in the event.
+
+**Utilize the tools we provide to showcase your services to the audience**. Check out what we have to offer:
+
+@component('mail::panel')
+
+**VIP invitations** (invite your key partners and prospects to attend the Expo and treat them with the access to the exclusive VIP Zone with sofas and refreshments)
+
+**Exhibitor’s Profile** (promote your Exhibitor’s Profile on your social media channels to showcase your booth location on the map and **receive unlimited meeting invitation requests**)
+
+**Registered attendees list access** (browse the list of current registered visitors and **send meeting invitations** to schedule appointments)
+
+@endcomponent
+
+** All these features are available in the Exhibitor’s Panel and are waiting for you to use!**
+
+A short update: 
+
+# VIP invitations
+
+The top distributor of the VIP invitations are currently ** WOW24-7**m, **Synesty**, **ITscope** - wow! 
+
+@component('mail::button', ['url' => $accountUrlVips])
+Send VIP invitations to partners and customers
+@endcomponent
+
+
+# TOP 5 (1:20pm CET) companies actively promoting their presence at the Expo and using our tools (rewarded with additional services!) are:
+
+* Allegro
+
+* Outvio OÜ
+
+* Dealavo
+
+* 2txt – natural language generation GmbH
+
+* weclapp
+
+**We are so happy that you understand the importance of lead generation and that you use our tools to maximize your exposure at the event!**
+
+@component('mail::button', ['url' => $accountUrlPromote])
+See promotional materials and ranking page
+@endcomponent
+
+@component('mail::panel')
+
+To all others out there, you still have time to promote your presence at our event! The simplest way to do so is to simply share the following link with your PR & social media department (access to the rest of the Exhibitor Panel excluded).
+
+{!! $accountUrlPromotePublic !!}
+
+@endcomponent
+
+Cheers
+
+{{$footer}}
+
+@endcomponent
