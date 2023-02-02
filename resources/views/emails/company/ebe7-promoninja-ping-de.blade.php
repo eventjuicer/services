e164066d8c46444e5798a7b35b88b408014b6d24
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-02 13:52:56 +0100

    Update ebe7-promoninja-ping-de.blade.php

--- resources/views/emails/company/ebe7-promoninja-ping-de.blade.php
+++ resources/views/emails/company/ebe7-promoninja-ping-de.blade.php
@@ -8,6 +8,7 @@ Die maximale Rendite eurer Investition auf der Messe hängt nicht nur von der La
 **Nutzt die von uns bereitgestellten Tools, um eure Dienstleistungen dem Publikum zu präsentieren**. Schaut euch an, was wir zu bieten haben:
 
 
+@component('mail::panel')
 
 **VIP-Einladungen** (Ladet eure wichtigsten Partner und Interessenten zur Teilnahme an der Expo ein und verwöhnt sie mit dem Zugang zur exklusiven VIP-Zone mit Sofas und Erfrischungen)
  
@@ -15,28 +16,33 @@ Die maximale Rendite eurer Investition auf der Messe hängt nicht nur von der La
 
 **Zugriff auf die Liste der registrierten Teilnehmer** (Durchsucht die Liste der aktuell registrierten Besucher und sendet Meetingeinladungen, um Termine am Tag der Expo zu planen)
 
+@endcomponent
+
 **Alle diese Funktionen sind im Aussteller-Profil verfügbar und warten darauf, von euch genutzt zu werden!**
 
 Ein kurzes Update: 
 
 # VIP Einladungen 
-Der Top-Verteiler der VIP-Einladungen ist derzeit **Firmenname** - Wow!
+
+Der Top-Verteiler der VIP-Einladungen ist derzeit **WOW24-7** - Wow!
 
 @component('mail::button', ['url' => $accountUrlVips])
 Versende VIP-Einladungen an Partner und Kunden
 @endcomponent
 
-# Die TOP 5 <Stand 00:00 Uhr>-Unternehmen, die aktiv für ihre Präsenz auf der Expo werben und unsere Tools nutzen (mit zusätzlichen Dienstleistungen belohnt!), sind:
+# Ausstellerprofil
 
-* 
+Die TOP 5 (13:40 Uhr) Unternehmen, die aktiv für ihre Präsenz auf der Expo werben und unsere Tools nutzen (mit zusätzlichen Dienstleistungen belohnt!), sind:
 
-* 
+* Allegro
 
-* 
+* Outvio OÜ
 
-*
+* Dealavo
 
-*
+* 2txt – natural language generation GmbH
+
+* weclapp
 
 **Wir freuen uns sehr, dass ihr die Bedeutung der Lead-Generierung versteht und unsere Tools nutzt, um eure Präsenz auf der Veranstaltung zu maximieren!**
 
@@ -44,9 +50,15 @@ Versende VIP-Einladungen an Partner und Kunden
 Siehe Werbematerialien und Ranking-Seite
 @endcomponent
 
-An alle anderen da draußen, ihr habt noch Zeit, für eure Präsenz bei unserer Veranstaltung zu werben! Am einfachsten geht das, indem ihr einfach den folgenden Link mit eurer PR- & Social-Media-Abteilung teilt (Zugriff auf das restliche Aussteller-Panel ausgenommen).
+@component('mail::panel')
+
+An alle anderen da draußen, ihr habt noch Zeit, für eure Präsenz bei unserer Veranstaltung zu werben! **Am einfachsten geht das, indem ihr einfach den folgenden Link mit eurer PR- & Social-Media-Abteilung teilt** (Zugriff auf das restliche Aussteller-Panel ausgenommen).
 
 {!! $accountUrlPromotePublic !!}
+
+@endcomponent
+
+
 
 Liebe Grüße, 
 
