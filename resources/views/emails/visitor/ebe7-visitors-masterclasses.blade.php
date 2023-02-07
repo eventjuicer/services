235225ff454983585526b0c4892480043424ddad
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-07 14:07:06 +0100

    ebe7 masterclasses for visitors

---
+++ resources/views/emails/visitor/ebe7-visitors-masterclasses.blade.php
@@ -0,0 +1,37 @@
+@component('mail::message')
+
+Hello {{ $p->translate("[[fname]]") }},
+
+Can you believe **it's already the E-commerce Berlin Expo time** again? We heard you are hungry for some e-commerce insights, so **this year, in addition to 5 stages** full of presentations from the industry leaders, **Masterclass sessions will await you**.
+
+Take advantage of this opportunity to learn from the experts from renowned brands such as **Sendinblue, Custobar, Mondu, Zissu, FedEx Express, everstox, Xentral, axytos, Tradebyte and Intive.** With topics varying from multichannel marketing to e-commerce logistics, and growth strategies, there's something for everyone. 
+
+
+[Check out the Masterclasses agenda here](https://ecommerceberlin.com/masterclasses)
+
+## What do you have to know about the Masterclasses?
+
+@component('mail::panel')
+
+The registration starts on **Thursday (09.02) at 9:00am CET**
+
+Participation is **free of charge**, but the **seats are limited** to 30 for each session.
+
+Each session consists of **45 minutes of presentation** followed by a **15 minutes of Q&A** designed to give you a deeper understanding of the topics discussed. 
+
+You can only take part in **one session!**
+
+@endcomponent
+
+**Presenters will review all applications and we will notify you via email if you have been accepted**. If there won’t be a free slot for you - no worries! You will be able to apply for another Masterclass or just come to the Expo and benefit from 5 open stages and > 240 exhibitors.
+
+[Check out the Masterclasses agenda here](https://ecommerceberlin.com/masterclasses)
+
+See you on EBE2023!
+
+Best regards,
+
+EBE Team
+
+@endcomponent
+
