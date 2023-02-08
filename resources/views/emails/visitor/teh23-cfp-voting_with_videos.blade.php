04955eb71ce2f66eabd9485f5ba6dd5c0ee59bb5
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-08 12:30:12 +0100

    cfp voting

---
+++ resources/views/emails/visitor/teh23-cfp-voting_with_videos.blade.php
@@ -0,0 +1,141 @@
+@component('mail::message')
+ 
+# Dzień dobry {{ $p->translate("[[fname]]") }},
+
+Potrzebujemy Cię! :) Tylko **do 16:00 w czwartek możesz wpłynąć na kształt agendy kwietniowej edycji Targów eHandlu w Warszawie.**
+
+<a href="https://targiehandlu.pl/vote" target="_blank">
+    <img src="https://res.cloudinary.com/eventjuicer/image/upload/w_450,h_450,c_limit,f_auto/v1675852651/team/teh23_cfp_submitters.png" alt="strona głosowania" />
+</a>
+
+
+@component('mail::panel')
+
+**130 Ekspertów czeka na Twoją przychylność i [oddanie głosu](https://targiehandlu.pl/vote). Pomożesz?**
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/vote"])
+Chcę wpłynąć na agendę kwietniowej edycji Targów
+@endcomponent
+
+@endcomponent
+
+## W ramach podziękowań za Twoje zaangażowanie, udostępniamy **kolejną porcję nagrań video** z  edycji Targów eHandlu, która odbyła się jesienią 2022.
+
+
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1662656377/c_2143_logotype.png" alt="" /></td>
+        <td> Dawid Nowrocki - Outvio
+            <strong><a href="https://ehandel.com.pl/ostatnia-prosta-w-ecommerce-ujednolicona-strategia-po-checkoutowa,2282">Ostatnia prosta w eCommerce: Ujednolicona strategia po-checkoutowa</a></strong>
+       
+    </td>
+    </tr>
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1523520187/c_1132_logotype.png" alt="" /></td>
+        <td> Benedykt Protasiuk - Sempai
+            <strong><a href="https://ehandel.com.pl/seo-miedzynarodowe-optymalizacja-sklepu-pod-wiele-wersji-jezykowych,2283">SEO międzynarodowe. Optymalizacja sklepu pod wiele wersji językowych</a></strong>
+        </td>
+    </tr>
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1660214483/c_2109_logotype.jpg" alt="" /></td>
+        <td> Piotr Michalak - Grupa Tense
+            <strong><a href="https://ehandel.com.pl/outsourcing-a-in-house-ktore-dzialania-seo-outsourcowac-a-ktore-wykonywac-samodzielnie,2284">Outsourcing a in-house. Które działania SEO outsourcować, a które wykonywać samodzielnie?</a></strong>
+        </td>
+    </tr>
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1666014312/c_1558_logotype.jpg" alt="" /></td>
+        <td> Mikołaj Miszczak - Samito
+            <strong><a href="https://ehandel.com.pl/dlaczego-marketing-automation-zawodzi-w-niektorych-sklepach-oto-przyklady-fail-stories-o-ktorych-sie-zwykle-milczy,2285">Dlaczego marketing automation zawodzi w niektórych sklepach? Oto przykłady fail stories, o których się zwykle milczy</a></strong>
+        </td>
+    </tr>
+
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1632474081/c_1872_logotype.webp" alt="" /></td>
+        <td>  Michał Nawrocki - iCEA
+            <strong><a href="https://ehandel.com.pl/miedzynarodowe-pozycjonowanie-czy-agencja-z-polski-moze-robic-dobre-seo-na-rynkach-zagranicznych,2287">Międzynarodowe pozycjonowanie. Czy agencja z Polski może robić dobre SEO na rynkach zagranicznych?</a></strong>
+        </td>
+    </tr>
+
+
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1617713009/c_1136_logotype.png" alt="" /></td>
+        <td>  Łukasz Plutecki - AtomStore
+            <strong><a href="https://ehandel.com.pl/6-bledow-w-ekspansji-zagranicznej-w-e-commerce-i-jak-ich-uniknac,2286"></a>6 błędów w ekspansji zagranicznej w e-commerce i jak ich uniknąć<strong>
+        </td>
+    </tr>
+
+
+
+
+</table>
+
+@component('mail::panel')
+
+**130 Ekspertów czeka na Twoją przychylność i [oddanie głosu](https://targiehandlu.pl/vote). Pomożesz?**
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/vote"])
+Chcę wpłynąć na agendę kwietniowej edycji Targów
+@endcomponent
+
+@endcomponent
+
+
+
+Wcześniej udostępnione nagrania z 22. Targów eHandlu
+
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1673862421/c_2275_logotype.png" alt="" /></td>
+        <td>Mariya Kallagova - Tribe47
+            <strong><a href="https://ehandel.com.pl/co-po-algorytmach-czas-na-kreatywna-reklame-oparta-na-danych,2278">Co po algorytmach? Czas na kreatywną reklamę opartą na danych</a></strong>
+       
+    </td>
+    </tr>
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1665740338/c_1245_logotype.png" alt="" /></td>
+        <td>Michał Blak - edrone 
+            <strong><a href="https://ehandel.com.pl/koniec-epoki-search,2277">Koniec epoki “search”</a></strong>
+        </td>
+    </tr>
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1633672146/c_1893_logotype.png" alt="" /></td>
+        <td>Kacper Skoczylas - cyber_Folks 
+            <strong><a href="https://ehandel.com.pl/30-dni-planowania-kampanii-po-nocach-i-caly-budzet-w-bloto-latwe-zmiany-w-sklepie-ktore-wystrzela-twoje-konwersje-w-kosmos,2276">Łatwe zmiany w sklepie, które wystrzelą Twoje konwersje w kosmos!</a></strong>
+        </td>
+    </tr>
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_250,h_250,c_limit,f_auto/v1662453230/c_2111_logotype.png" alt="" /></td>
+        <td>Tomasz Zarychta - Media4U
+            <strong><a href="https://ehandel.com.pl/bledy-w-sklepach-internetowych-wnioski-z-przebadania-ponad-100-sklepow,2280">Błędy w sklepach internetowych - wnioski z przebadania ponad 100 sklepów</a></strong>
+        </td>
+    </tr>
+
+
+</table>
+
+
+
+
+
+Z pozdrowieniami,
+
+Karolina Michalak
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
