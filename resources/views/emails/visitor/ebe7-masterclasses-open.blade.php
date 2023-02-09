61bf4cef62b5c3a80c28c8f2fef61e107fad7e30
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-09 10:15:29 +0100

    masterclasses started

---
+++ resources/views/emails/visitor/ebe7-masterclasses-open.blade.php
@@ -0,0 +1,41 @@
+@component('mail::message')
+
+Hello {{ $p->translate("[[fname]]") }},
+
+**You can now sign up for our new, exclusive, format**, that will be part of this year's E-commerce Berlin Expo 
+    
+# Masterclasses applications are now OPEN
+
+Take advantage of this opportunity to learn from the **experts from** renowned brands such as **Sendinblue, Custobar, Mondu, Zissu, FedEx Express, everstox, Xentral, axytos, Tradebyte and Intive.** 
+With topics varying from **multichannel marketing to e-commerce logistics, and growth strategies**, there's something for everyone. 
+
+[Check the full masterclasses agenda here and apply here](https://ecommerceberlin.com/masterclasses)
+
+## Now, what do you have to know about the Masterclasses?
+
+@component('mail::panel')
+
+Masterclasses are part of E-commerce Berlin Expo - 23rd FEBRUARY 2023 - STATION Berlin.
+
+**The registration is now OPEN**
+
+Participation is **free of charge**, but the **seats are limited to 30/masterclass**.
+
+You can only take part in one session (and then take part in our 5 public stages!)
+
+@endcomponent
+
+**Presenters will review all applications and we will notify you via email if you have been accepted**. 
+If there won’t be a free slot for you - no worries! You will be able to apply for another Masterclass or just come to the Expo and benefit from 5 open stages and > 240 exhibitors.
+
+[Check the full masterclasses agenda and apply here](https://ecommerceberlin.com/masterclasses)
+
+See you on E-commerce Berlin Expo 2023!
+
+Best regards,
+
+EBE Team
+
+**[Newsletter configuration]({{ $unsubscribe }})**
+
+@endcomponent
