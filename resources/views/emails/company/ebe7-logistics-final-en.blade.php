c2c1f72de78746b9e93c90ee5dbf0f73262a134f
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-20 12:39:15 +0100

    ebe7 logistics

---
+++ resources/views/emails/company/ebe7-logistics-final-en.blade.php
@@ -0,0 +1,57 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+
+The E-commerce Berlin Expo 2023 is coming back to STATION Berlin on Thursday, February 23rd and we are happy to welcome you as one of our Exhibitors. To get you up and running for our event, we’ve prepared a bunch of helpful information. 
+
+**EBE 2023 | When and Where?**
+
+E-commerce Berlin Expo
+
+STATION Berlin
+
+Luckenwalder Str 4-6, 10963 Berlin
+
+**Thursday / February 23rd, from 10.00 AM to 5.00 PM** ­(Opening hours for visitors) 
+
+Here are also some factors to take into account whilst preparing for the expo. You can click on each factor to be redirected to your Exhibitors panel and find all the information needed.
+
+## EBE 2023 | Assembly Day
+
+[Entrance](https://account.ecommerceberlin.com/#/logistics?pre_welcome)
+
+[Welcome Packs](https://account.ecommerceberlin.com/#/logistics?pre_welcomepacks)
+
+[Stand Assembly](https://account.ecommerceberlin.com/#/logistics?preassembly)
+
+[Parking](https://account.ecommerceberlin.com/#/logistics?pre_parking)
+
+## EBE 2023 | Expo Day - 23rd of February
+
+[Parking is not possible in the venue during the expo day](https://account.ecommerceberlin.com/#/logistics?parking)
+
+[Entrance](https://account.ecommerceberlin.com/#/logistics?welcome)
+
+[Badge scanning / Lead collection](https://account.ecommerceberlin.com/#/logistics?scans)
+
+[Reporting Problems](https://account.ecommerceberlin.com/#/logistics?problems)
+
+[Stand disassembly](https://account.ecommerceberlin.com/#/logistics?disassembly)
+
+­## EBE 2023 | After the Expo
+
+[Export data from scanned badges](https://account.ecommerceberlin.com/#/logistics?leads)
+
+[Materials storage and pickup details](https://account.ecommerceberlin.com/#/logistics?packages)
+
+Here is also the map where you can see the plan for the map at the Expo.
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/v1676882600/Logistics/entrance_SB_ebe7.png" alt="" style="max-width: 700px;"/>
+
+­Let us know if you have any questions!
+
+See you in Berlin!
+
+{{$footer}}
+
+@endcomponent
