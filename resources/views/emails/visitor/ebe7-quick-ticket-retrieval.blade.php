87b5e5964a25b92eaacaa08f9b0bc856fd659325
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-22 18:13:24 +0100

    Update ebe7-quick-ticket-retrieval.blade.php

--- resources/views/emails/visitor/ebe7-quick-ticket-retrieval.blade.php
+++ resources/views/emails/visitor/ebe7-quick-ticket-retrieval.blade.php
@@ -6,7 +6,7 @@ Thank you for downloading the ticket for E-ecommerce Berlin Expo.
 
 We hope to see you tomorrow at 10:00am at STATION Berlin.
 
-If you are unable to print or accidentally have lost your printed ticket, please show the code presented below at the registration point.
+**If you are unable to print** or accidentally have lost your printed ticket, please **show the code** presented below **at the registration point.**
 
 # {{$hash}}
 
