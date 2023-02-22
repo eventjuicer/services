d51f031e80bf0c057b29c10dd4f34e0663f649e1
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-02-22 15:46:03 +0100

    Update GeneralReminder.php

--- app/Mail/GeneralReminder.php
+++ app/Mail/GeneralReminder.php
@@ -23,6 +23,7 @@ class GeneralReminder extends Mailable
     
     public $p, 
     $ticketUrl, 
+    $hash,
     $siteUrl, 
     $exhibitorsURl,
     $presentersURl,
@@ -70,6 +71,7 @@ class GeneralReminder extends Mailable
         $this->scheduleURl = $baseUrl . "/schedule" . $params;
         $this->registerURl = $baseUrl . "/visit" . $params;
         $this->url = $this->ticketUrl;
+        $this->hash = "!".$this->hash;
 
         $this->unsubscribe = "https://services.eventjuicer.com/unsubscribe/" . $hash; 
 
