18b89aed63976c83471db43f4f1c429098290089
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-14 10:22:45 +0100

    ebe8 top30 lastcall

--- resources/views/emails/company/ebe8-salesplan-starts-top30-de.blade.php
+++ resources/views/emails/company/ebe8-salesplan-starts-top30-de.blade.php
@@ -4,11 +4,11 @@ Hallo {{ array_get($profile, "fname") }},
 
 @component('mail::panel')
 
-## Heute - 14. März, 11:00 Uhr
+# Heute - 14. März, 11:00 Uhr
 
 Exklusiver Verkauf für die TOP30-Unternehmen aus dem Ausstellerwettbewerb.
 
-**Du gehörst zu den 30 berechtigten Unternehmen!** Buche heute über [LINK](https://ecommerceberlin.com/top30earlybird_t63sUa9)
+**Du gehörst zu den 30 berechtigten Unternehmen!** Buche heute über [ecommerceberlin.com/top30earlybird_t63sUa9](https://ecommerceberlin.com/top30earlybird_t63sUa9)
 
 Du kannst den Ausstellungsplatz über den oben bereitgestellten geheimen Link **bis zum Ende des Tages** buchen.
 
--- resources/views/emails/company/ebe8-salesplan-starts-top30-en.blade.php
+++ resources/views/emails/company/ebe8-salesplan-starts-top30-en.blade.php
@@ -4,11 +4,11 @@
 
 @component('mail::panel')
 
-## Today - March 14, 11:00 am
+# Today - March 14, 11:00 am
 
 Attention TOP30 companies from the Exhibitor Contest: Exclusive sale!
 
-**You are among the 30 eligible companies to take advantage of this offer. **Don't miss out and book your exhibition space today through this [LINK](https://ecommerceberlin.com/top30earlybird_t63sUa9) till the end of the day.
+**You are among the 30 eligible companies to take advantage of this offer.** Don't miss out and book your exhibition space today through this [ecommerceberlin.com/top30earlybird_t63sUa9](https://ecommerceberlin.com/top30earlybird_t63sUa9) **till the end of the day.**
 
 After today, the sale will be open to everyone, and we cannot guarantee the best places at the lowest prices anymore.
 
@@ -16,9 +16,7 @@ After today, the sale will be open to everyone, and we cannot guarantee the best
 
 ## What is the booking process?
 
-Each subsequent pool will be activated automatically after the previous pool reaches its limit. 
-
-Therefore, on the first day, new and more expensive sales pool may be activated if the previous pool has already been booked out.
+Each subsequent pool will be activated automatically after the previous pool reaches its limit.  Therefore, on the first day, new and more expensive sales pool may be activated if the previous pool has already been booked out.
 
 Best regards,
 
