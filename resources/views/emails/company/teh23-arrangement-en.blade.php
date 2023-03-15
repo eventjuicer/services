69abd5e9ef03de4598203a4ecb7821d457b85da1
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-15 14:38:05 +0100

    Update teh23-arrangement-en.blade.php

--- resources/views/emails/company/teh23-arrangement-en.blade.php
+++ resources/views/emails/company/teh23-arrangement-en.blade.php
@@ -26,16 +26,15 @@ Browse/order additional services
 
 * **Carpet**
 
-* Additional furniture (for free set of 2 high chairs and 1 table please choose apropriate order)
-
 * **50'' LED screen**
 
-* Independent power connection - 3-phase 16/32A - if you would like to connect coffee maker, etc
+* Independent **power connection: **3-phase 16/32A** - if you would like to connect coffee maker, etc
 
-* Independent power connection - 3kW (instead of the standard 300W) if you plan to equip the stand other than 1-2 laptops and TV
+* Independent **power connection: 3kW (instead of the standard 300W)** if you plan to equip the stand other than 1-2 laptops and TV
 
 * Permission for **distributing leaflets** outside the stand
 
+* Additional furniture (for free set of 2 high chairs and 1 table please choose apropriate "product")
 
 
 Best regards
