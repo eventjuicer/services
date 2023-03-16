176702cbbbe44867601f08d4105f211cb50680cf
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-16 14:21:58 +0100

    teh23 promoninja

---
+++ resources/views/emails/company/teh23-promoninja-intro-pl.blade.php
@@ -0,0 +1,41 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname") }},
+
+**Zgodnie z harmonogramem w Panelu Wystawcy, udostępniamy materiały promocyjne**: bannery, szablony newslettera, linki do udostępniania profilu Wystawcy i tym samym kolejną edycję Konkursu dla Wystawców!
+
+## Materiały
+
+@component('mail::button', ['url' => $accountUrlPromote])
+Strona z materiałami dla Twojej firmy
+@endcomponent
+
+**Chcesz podesłać stronę z materiałami promocyjnymi innej osobie?**
+
+Prześlij ten link: {{$accountUrlPromotePublic}} - umożliwia on korzystanie z materiałów bez dostępu do innych części panelu.
+
+Wszystkie materiały kierują [do Twojego publicznego profilu Wystawcy]({{$profileUrl}}). Ta strona nie tylko opisuje Twoją firmę ale także:
+
+* pokazuje **lokalizację Twojego stoiska** na hali targowej
+
+* pozwala Zwiedzającym **umówić się na spotkanie** (po akceptacji takiej propozycji masz dostęp do pełnych danych kontaktowych)
+
+**Ruch na Twoim publicznym profilu Wystawcy to punkty, które wymieniamy na super korzyści**: **dostęp do puli early-bird** na kolejną edycję Targów, dostęp do **większej puli zaproszeń** Zwiedzających na spotkanie czy większa liczbę zaproszeń VIP.
+
+Pozdrawiam, 
+
+{{$footer}}
+
+Jeśli chcesz się wyróżnić i masz możliwość przygotowania własnych materiałów graficznych - do kierowania na profil Wystawcy użyj "gołego" linka, który został podany na końcu strony z materiałami.
+
+...ten link to:
+
+@component('mail::panel')
+[{{ $trackingLink }}]({{ $trackingLink }}) 
+@endcomponent
+
+
+@endcomponent
+
