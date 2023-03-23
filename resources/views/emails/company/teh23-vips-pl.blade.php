ad06e92405278c541d2f4e1ea7fefd98cc59a9d4
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-23 12:55:34 +0100

    Update teh23-vips-pl.blade.php

--- resources/views/emails/company/teh23-vips-pl.blade.php
+++ resources/views/emails/company/teh23-vips-pl.blade.php
@@ -4,14 +4,14 @@
 
 W panelu wystawcy uruchomiona została opcja wysyłki zaproszeń VIP.
 
-## Zaproś swoich VIP do ekskluzywnej Strefy VIP na Targach. 
+## Zaproś swoich Partnerów, kluczowych Klientów do ekskluzywnej Strefy VIP na Targach. 
 
 Strefa VIP to miejsce do komfortowych, kuluarowych rozmów Prelegentów, Wystawców, wybranych Zwiedzających oraz **gości których zaprosisz przez panel Wystawcy.**.
 
-**Każdy Przedstawiciel Wystawcy ma automatyczny dostęp do Strefy VIP - nie wykorzystuj zaproszeń VIP do tego celu.**
+@component('mail::panel')
 
-@component('mail::button', ['url' => $accountUrlVips])
-Zaproszenia VIP dla Partnerów i Klientów
+**Każdy Przedstawiciel Wystawcy (zarejestrowany w Panelu) ma automatyczny dostęp do Strefy VIP - nie wykorzystuj zaproszeń VIP do tego celu.**
+
 @endcomponent
 
 Zaproszenia VIP mogą być dystrybuowane na 2 sposoby:
@@ -21,7 +21,12 @@ Zaproszenia VIP mogą być dystrybuowane na 2 sposoby:
 * pobranie indywidualnego linka i wysyłka za pośrednictwem swoich kanałów
 
 
-(Jeśli potrzebujesz więcej zaproszeń - odezwij się do nas.)
+@component('mail::button', ['url' => $accountUrlVips])
+Zaproszenia VIP dla Partnerów i Klientów
+@endcomponent
+
+
+## Jeśli potrzebujesz więcej zaproszeń, skorzystaj z materiałów promocyjnych, promuj swój publiczny profil Wystawcy. Liczba dostępnych zaproszeń VIP ulegnie automatycznemu powiększeniu.
 
 
 Regards, 
