3f9ab36defa3813ccf0a0d67424f37a90e4c1d4d
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-27 12:58:33 +0200

    current visitors - agenda/workshops

---
+++ resources/views/emails/visitor/teh23-agenda-workshops-for-current.blade.php
@@ -0,0 +1,36 @@
+
+
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+Dziękujemy za rejestrację jako Uczestnik Targów eHandlu :)
+
+@component('mail::panel')
+
+Przypominamy, że 23. Targi eHandlu odbędą się **6 kwietnia w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny z prezentacjami Ekspertów** oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+@endcomponent
+
+Ważne informacje
+
+Opublikowaliśmy **[kompletną listę prezentacji na scenach A, B, C i D](https://targiehandlu.pl/schedule)**, z którą warto się zapoznać by lepiej zaplanować swój dzień targowy.
+
+**Bilety wstępu (PDF)** będziemy rozsyłali **ok 31 marca** (w piątek)
+
+Jutro ruszają **zapisy na Warsztaty** [Agenda 21 warsztatów](https://targiehandlu.pl/workshops)
+
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Karolina Wojtas
+
+Adam Zygadlewicz
+
+
+
+@endcomponent
