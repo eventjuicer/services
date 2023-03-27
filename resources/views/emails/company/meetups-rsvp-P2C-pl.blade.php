a96ff942676828e9842a0317f6d1be1f347b50c0
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-27 12:45:44 +0200

    p2c

--- resources/views/emails/company/meetups-rsvp-P2C-pl.blade.php
+++ resources/views/emails/company/meetups-rsvp-P2C-pl.blade.php
@@ -2,18 +2,21 @@
  
 Cześć
 
-W Panelu Wystawcy czekają zaproszenia od Uczestników Targów eHandlu wysłane przez publiczny profil Wystawcy.
+W Panelu Wystawcy czekają **zaproszenia od Uczestników Targów eHandlu wysłane przez publiczny profil Wystawcy.**
 
 Bardzo prosimy o przetworzenie ich :)
 
 Akceptacja ujawnia dane kontaktowe Uczestnika oraz nadaje mu status VIP.
-
 
 @component('mail::button', ['url' => $url("meetups") ])
 Zobacz listę osób chcących się spotkać
 @endcomponent
 
 @component('mail::panel')
+
+Aby otrzymywać więcej takich zaproszeń i zwiększyć konwersję udziału w Targach - promuj swój [publiczny profil Wystawcy]({!!$profileUrl!!}).
+
+[Materiały promocyjne]({!!$accountUrlPromotePublic!!}) 
 
 @endcomponent
 
