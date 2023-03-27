ef05251f1951be90cfa96dd98d34d118cd9e2674
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-27 15:34:47 +0200

    Create teh23-agendaALL-for-voters.blade.php

---
+++ resources/views/emails/visitor/teh23-agendaALL-for-voters2.blade.php
@@ -0,0 +1,95 @@
+@component('mail::message')
+
+# Cześć {{ $fname }},
+
+Dziękujemy za Twój głos podczas wyboru Prelegentów Targów eHandlu. 
+
+@component('mail::panel')
+
+Targi eHandlu odbędą się **6 kwietnia w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny z prezentacjami Ekspertów** oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+Podczas tej edycji planujemy aż  **21 warsztatów.** Zapisy na warsztaty startują we wtorek o 12:00!
+
+@endcomponent
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Sprawdź agendę 23 Targów eHandlu w Warszawie
+@endcomponent
+
+
+## Opublikowaliśmy [kompletną listę prezentacji na scenach A, B, C i D](https://targiehandlu.pl/visit)
+
+Wybrane tematy PRELEKCJI:
+
+**[Omnibus] Promocja zabija Twój biznes** - jak skutecznie nią zarządzić!
+
+**Jak skutecznie obsłużyć rosnącą sprzedaż e-commerce bez zwiększania zatrudnienia?**
+
+**TikTok commerce:** jak zwiększać sprzedaż dzięki rolkom, shortom i tiktokom?
+
+**Sztuczna inteligencja w e-commerce.**
+
+Gdzie schowała się rentowność? **3 miejsca w Twoim e-Commerce, w których tracisz najwięcej pieniędzy**
+
+**Nowe doświadczenia zakupowe z Generative AI**
+
+**Od intencji do retencji. 9 rzeczy, o których zapominasz, aby zatrzymać klienta w sklepie**
+
+**EmpikPlace** ...jak dotrzeć do milionów klientów na platformie e-commerce z 25-letnim doświadczeniem.
+
+**Jak wykorzystać FOMO** i skusić klienta do zakupu TERAZ!
+
+**Automatyzacja po-checkoutowa**
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Szybka rejestracja
+@endcomponent
+
+## Opublikowaliśmy tematy WARSZTATÓW
+
+@component('mail::panel')
+Zapisy na warsztaty startują we wtorek o 12:00!
+@endcomponent
+
+DLACZEGO teraz warto **sprzedawać do Wielkiej Brytanii** i JAK to robić ?
+
+Najwyższy czas na **e-paragony.**
+
+Zwiększ sprzedaż w e-sklepie dzięki **przejęciu kontroli nad procesami post-checkout**
+
+Zaprojektuj **pulpit managera e-commerce** z pomocą systemu ERP nowej generacji!
+
+**Budżet marketingowy na Allegro**
+
+**Komunikacja mobile push** - od wdrożenia do konwersji
+
+Jak za pomocą **customer journey map** zwiększyć sprzedaż w Twoim e-commerce?
+
+**Rodzaje akcji promocyjnych** – którą kiedy zastosować, by osiągnąć pożądany efekt i jak ją niedrogo wdrożyć?
+
+Szybki **audyt działań email marketingowych**
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/workshops'])
+Lista wszystkich warsztatów
+@endcomponent
+
+
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Karolina Wojtas
+
+Adam Zygadlewicz
+
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+
+
+@endcomponent
