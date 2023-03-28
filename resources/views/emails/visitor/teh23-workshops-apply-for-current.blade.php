c6569d7cd48af49f97b352383c1172124f7aff4f
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-03-28 12:15:58 +0200

    teh23 workshops

---
+++ resources/views/emails/visitor/teh23-workshops-apply-for-current.blade.php
@@ -0,0 +1,49 @@
+
+
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+## [Zgłoszenia na Warsztaty](https://targiehandlu.pl/workshops) wystartowały!
+
+Zarejestrowani uczestnicy (jak Ty) otrzymują oficjalną informację jako pierwsi.
+
+Ważne informacje:
+
+Prowadzący Warsztaty dostali prośbę by akceptować **nie więcej niż kilka zgłoszeń dziennie** (by dać każdemu szansę). Otrzymasz wiadomość zwrotną przy akceptacji lub przekroczeniu czasu na akceptację zgłoszenia.
+
+**Możesz się zgłosić do maksymalnie 2 warsztatów** (w przypadku "uwolnienia" zgłoszenia przez brak reakcji ze strony Prowadzącego - limit się odnowi)
+
+**Udział** w Warsztatch w tej edycji jest **bezpłatny.**
+
+Każdy Uczestnik Warsztatów **otrzymuje status VIP** (dostęp do Strefy VIP dla Wystawców i Prelegentów)
+
+W 1 Warsztacie może uczestniczyć **max 25 osób** i nie zwiększymy tego limitu.
+
+Czas warsztatu to maksymalnie **45 minut** (30min + 15min na Q&A)
+
+**Niezależnie od warsztatów [dostępne są aż 4 sceny prezentacyjne](https://targiehandlu.pl/schedule)** (udział bezpłatny, otwarty)
+
+
+@component('mail::panel')
+
+Przypominamy, że 23. Targi eHandlu odbędą się **6 kwietnia w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny** z prezentacjami Ekspertów oraz **140 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+...**oraz 21 warsztatów**
+
+@endcomponent
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Karolina Wojtas
+
+Adam Zygadlewicz
+
+PS: **Bilety wstępu (PDF)** będziemy rozsyłali w piątek
+
+
+@endcomponent
