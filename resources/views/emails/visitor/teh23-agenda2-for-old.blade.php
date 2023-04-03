c19e47270df8bfae0a973404cbbc0d51a0bfd2c7
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-04-03 13:57:22 +0200

    teh23 agenda final

---
+++ resources/views/emails/visitor/teh23-agenda2-for-old.blade.php
@@ -0,0 +1,85 @@
+
+
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+# Kolejne Targi eHandlu już w czwartek w Warszawie! 
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Sprawdź agendę prezentacji
+@endcomponent
+
+
+@component('mail::panel')
+
+Targi eHandlu odbędą się **6 kwietnia (czwartek) w Warszawie (EXPO XXI)**
+
+Zwiedzanie stoisk - **140 produktów i usług wspierających rozwój e-handlu** - [wymagana rejestracja na stronie](https://targiehandlu.pl/visit)
+
+Udział w prezentacjach - **37 prelegentów** - [wymagana rejestracja na stronie](https://targiehandlu.pl/visit)
+
+Udział w jednym z 21 warszatów - [zgłoszenie chęci na stronie targiehandlu.pl/workshops](https://targiehandlu.pl/workshops) - o udziale decyduje Prowadzący.
+
+@endcomponent
+
+**Jak zaplanować skuteczny dzień na Targach eHandlu?**
+
+## [Zapoznaj się z listą Wystawców](https://targiehandlu.pl/exhibitors)
+
+Każdemu Wystawcy, którego oferta wydaje się Tobie interesująca, możesz zaproponować Spotkanie przez jego publiczny profil Wystawcy 
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_750,c_limit,f_auto/v1680522241/teh23_invites.png" alt="umawianie spotkań" />
+
+Jeśli Wystawca zaakceptuje Twoją propozycję spotkania, **otrzymasz od nas wejściówkę VIP** - uprawniająca do wejścia do strefy VIP (Hala 3). 
+W tej strefie spotkasz Przedstawicieli Wystawców, Prelegentów, Trenerów Warsztatów oraz innych Gości VIP. 
+
+## [Zapoznaj się z listą Prezentacji](https://targiehandlu.pl/schedule)
+
+**Sztuczna inteligencja w e-commerce.**
+
+Gdzie schowała się rentowność? **3 miejsca w Twoim e-Commerce, w których tracisz najwięcej pieniędzy**
+
+**Nowe doświadczenia zakupowe z Generative AI**
+
+**Od intencji do retencji. 9 rzeczy, o których zapominasz, aby zatrzymać klienta w sklepie**
+
+**EmpikPlace** ...jak dotrzeć do milionów klientów na platformie e-commerce z 25-letnim doświadczeniem.
+
+**Jak wykorzystać FOMO** i skusić klienta do zakupu TERAZ!
+
+**Automatyzacja po-checkoutowa**
+
+**Oto Wybrane tematy Scen Prezentacyjnych**
+
+**[Omnibus] Promocja zabija Twój biznes** - jak skutecznie nią zarządzić!
+
+**Jak skutecznie obsłużyć rosnącą sprzedaż e-commerce bez zwiększania zatrudnienia?**
+
+**TikTok commerce:** jak zwiększać sprzedaż dzięki rolkom, shortom i tiktokom?
+
+
+
+# {{ $p->translate("[[fname]]") }}, widzimy się w czwartek w EXPO XXI?
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Szybka Rejestracja 
+@endcomponent
+
+
+
+Pozdrawiamy,
+
+Karolina Michalak
+
+Karolina Wojtas
+
+Adam Zygadlewicz
+
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+
+@endcomponent
