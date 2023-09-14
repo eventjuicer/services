51b0786cba98cd7d7503e163caa29051793e133d
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-09-14 12:37:22 +0200

    cfp video

---
+++ resources/views/emails/visitor/teh24-videos_cfp.blade.php
@@ -0,0 +1,91 @@
+
+
+@component('mail::message')
+ 
+# Dzień dobry {{ $p->translate("[[fname]]") }},
+
+Kolejne Targi eHandlu odbędą się **27 października na EXPO XXI w Warszawie.**
+
+Intensywnie pracujemy nad agendą wystąpień. **Chcesz wystąpić? informacje poniżej!**
+
+Udostępniamy pierwszą porcję nagrań video z wiosennej 23. edycji Targów, która odbyła się w kwietniu. 
+
+
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
+
+<tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_450,h_450,c_limit,f_auto/v1674816459/c_2279_logotype.png" alt="" /></td>
+        <td>Tomasz Palak - sunzinet 
+            <strong><a href="https://ehandel.com.pl/jak-uniknac-prawnych-klopotow-w-e-commerce-proste-triki,2298">Jak uniknąć prawnych kłopotów w e-commerce? Proste triki</a></strong>
+       
+    </td>
+    </tr>
+
+    
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_450,h_450,c_limit,f_auto/v1673275779/c_2268_logotype.png" alt="" /></td>
+        <td>Robert Stolarczyk - Feed Octopus 
+            <strong><a href="https://ehandel.com.pl/data-warehouse-dla-e-commerce-w-oparciu-o-big-query-czym-jest-i-jak-moze-pomoc-rozwijac-biznes-w-oparciu-o-sztuczna-inteligencje,2306">Data Warehouse dla e-commerce w oparciu o Big Query. Czym jest i jak może pomóc rozwijać biznes w oparciu o sztuczną inteligencję?</a></strong>
+       
+    </td>
+    </tr>
+  
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_450,h_450,c_limit,f_auto/v1665044515/c_2099_logotype.jpg" alt="" /></td>
+        <td>Dawid Fabiś - OLX 
+            <strong><a href="https://ehandel.com.pl/wszystko-juz-bylo-co-nowego-wnosi-olx-na-rynek-e-commerce,2305">Wszystko już było? Co nowego wnosi OLX na rynek e-commerce.</a></strong>
+       
+    </td>
+    </tr>
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_450,h_450,c_limit,f_auto/v1674139215/c_2177_logotype.jpg" alt="" /></td>
+        <td>Jacek Lisowski - Asseco Business Solutions 
+            <strong><a href="https://ehandel.com.pl/jak-skutecznie-obsluzyc-rosnaca-sprzedaz-e-commerce-bez-zwiekszania-zatrudnienia,2304">Jak skutecznie obsłużyć rosnącą sprzedaż e-commerce bez zwiększania zatrudnienia?</a></strong>
+       
+    </td>
+    </tr>
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_450,h_450,c_limit,f_auto/v1665740338/c_1245_logotype.png" alt="" /></td>
+        <td>Michał Blak - edrone 
+            <strong><a href="https://ehandel.com.pl/nowe-doswiadczenia-zakupowe-z-generative-ai,2297">Nowe doświadczenia zakupowe z Generative AI</a></strong>
+       
+    </td>
+    </tr>
+
+  
+
+</table>
+
+**Polecamy zaglądać na portal ehandel.com.pl, na którym będziemy publikować kolejne nagrania.**
+
+
+@component('mail::panel')
+
+## Chcesz wystąpić na jednej ze scen 24. Targów eHandlu? Termin zgłoszeń kończy się w piątek!
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/speaking"])
+Chcę zostać Prelegentem na Targach eHandlu
+@endcomponent
+
+@endcomponent
+
+
+Z pozdrowieniami,
+
+Karolina Wojtas
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
