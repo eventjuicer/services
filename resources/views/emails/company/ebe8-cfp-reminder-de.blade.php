d050f9c4ddfdc7de5b2b4c71731467260a4369b3
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-09-19 12:08:26 +0200

    email - https://github.com/ecommerceberlin/exhibitors/issues/160

---
+++ resources/views/emails/company/ebe8-cfp-reminder-de.blade.php
@@ -0,0 +1,41 @@
+@component('mail::message')
+ 
+# Hallo,
+
+Wir möchten dich an den Wettbewerb E-commerce Berlin Expo - Call for Papers erinnern.
+
+Du hast noch die Möglichkeit, deine **Vortragsvorschlag** einzureichen und möglicherweise **Speaker** auf einer der bedeutendsten E-Commerce-Veranstaltungen in Deutschland zu werden.
+
+**Warum am Call for Papers-Wettbewerb teilnehmen?**
+
+Stell dir das vor: Die E-Commerce Berlin Expo 2024, auf der sich über 10.000 Besucher aus der Welt des Einzelhandels und der Marken treffen. Jetzt stell dir vor, wie du auf der Bühne stehst und deine Erkenntnisse und Strategien mit einem vielfältigen Publikum teilst. Das ist das Rampenlicht, das du durch unseren Call for Papers Wettbewerb für dich beanspruchen kannst.
+
+**Welche Art von Themen interessieren uns?**
+
+Wir sind auf der Suche nach Inhalten, die über die Theorie hinausgehen - praktische Fälle, echte Geschichten und umsetzbare Trendprognosen. Wir wollen deine Erfahrungen und Strategien, die andere E-Commerce-Experten in die Tat umsetzen können.
+
+- Kannst du eine beeindruckende Erfolgsgeschichte eines Kunden erzählen, die durch Daten und praktische Erkenntnisse gestützt wird?
+
+- Hast du in deiner Branche etwas Bemerkenswertes erreicht, das andere inspirieren könnte? 
+
+- Kannst du effektive Marktstrategien aufzeigen, die deinen Kunden oder Partnern geholfen haben?
+
+- Hast du eine Marktprognose abzugeben?
+
+Das ist genau die Art von Präsentationen, die wir suchen.
+
+**Wie läuft das Einreichungsverfahren ab?**
+
+Die Bewerbungsfrist endet am **29. September**. In den folgenden Schritten entscheiden unsere Besucher und Beiratsmitglieder, wer in das Programm der Veranstaltung aufgenommen wird! Die Teilnahme an dem Wettbewerb ist kostenlos. 
+
+Bist du bereit? Informiere dich über alle Einzelheiten und bewirb dich unten:
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking?utm_source=email&utm_medium=email&utm_campaign=system_mailing_exhibitors"])
+Reiche deinen Präsentationsvorschlag ein
+@endcomponent
+
+Viel Glück!
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/ebe8-cfp-reminder-en.blade.php
@@ -0,0 +1,40 @@
+@component('mail::message')
+ 
+# Hello,
+
+We'd like to remind you about the E-commerce Berlin Expo - Call for Papers contest. 
+
+There's still an opportunity for you to **submit your presentation proposal** and potentially **become a speaker** at one of the most distinguished e-commerce events in Germany! 
+
+**Why participate in the Call for Papers contest?**
+
+Picture this: The E-commerce Berlin Expo 2024, where over 10,000 attendees from the world of retail and brands gather. Now imagine yourself on stage, sharing your insights and strategies with a varied audience. That's the spotlight you could claim through our Call for Papers contest.
+
+**What kind of topics are we interested in?**
+
+We're on the lookout for content that goes beyond theory – practical cases, real stories, and actionable trend forecasts. We want your experiences, and strategies that fellow e-commerce professionals can put into action.
+
+- Can you share an impactful client success story backed by data and practical insights? 
+
+- Have you achieved something remarkable in your industry that could inspire others? 
+
+- Can you reveal effective market strategies that have helped your clients or partners? 
+
+- Do you have market predictions to share?
+These are precisely the kind of presentations we're seeking.
+
+**How does the submission process work?**
+
+The submission deadline is **September 29.** Subsequent steps involve our visitors and Advisory Board members deciding who should join the event's agenda! Participation is the competition is free of charge. 
+
+Are you ready? Find out all the details and apply below:
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking?utm_source=email&utm_medium=email&utm_campaign=system_mailing_exhibitors"])
+Submit your presentation proposal
+@endcomponent
+
+Best of luck!
+
+{{$footer}}
+
+@endcomponent
