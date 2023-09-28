f07a20d7d5f4add9a0a85f45b3c548fa21e65716
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-09-28 13:09:13 +0200

    EMAIL ebe cfp final https://github.com/ecommerceberlin/exhibitors/issues/161

---
+++ resources/views/emails/company/ebe8-cfp-final-de.blade.php
@@ -0,0 +1,28 @@
+@component('mail::message')
+
+# Hallo!
+
+Da ihr bei der E-Commerce Berlin Expo ein Aussteller seid und somit einen privilegierten Status habt, freuen wir uns, euch mit dieser **letzten Erinnerung** an unseren Call for Papers-Wettbewerb zu erinnern, der noch bis **morgen** für Bewerbungen geöffnet ist.
+
+**Dies ist deine letzte Chance, einen Vortragsvorschlag einzureichen und dir möglicherweise einen Platz auf der Veranstaltung zu sichern!** Auf diese Weise kannst du dein Wissen einbringen, deine Marke präsentieren und mit anderen aus der Branche in Kontakt treten.
+
+**Welche Vorträge werden erwartet?**
+
+Wir sind auf der Suche nach überzeugenden und interessanten Kundenbeispielen, Geschichten oder Trendprognosen. Die Inhalte, die du mitbringst, sollten einen hohen Wert haben und unsere Expo-Teilnehmer begeistern.
+
+**Wie funktioniert das?**
+
+Reiche deinen Präsentationsvorschlag bis zum **Freitag, 29. September** ein und werde von den Wählern und Beiratsmitgliedern ausgewählt, um in unsere Agenda aufgenommen zu werden! Die Teilnahme an dem Wettbewerb ist kostenlos.
+Bist du bereit, dein Fachwissen und deine Erkenntnisse zu präsentieren?
+
+**Erfahre alle Details und bewirb dich jetzt:**
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking?utm_source=email&utm_medium=email&utm_campaign=system_mailing_exhibitors"])
+Reiche deinen Präsentationsvorschlag ein
+@endcomponent
+
+Viel Glück!
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/ebe8-cfp-final-en.blade.php
@@ -0,0 +1,29 @@
+@component('mail::message')
+
+# Hello!
+
+As you hold an esteemed exhibitor status at the E-commerce Berlin Expo, we're thrilled to extend this **final reminder** regarding our Call for Papers contest, which is open for submissions until **tomorrow**.
+
+**This is your last opportunity to submit a presentation proposal and potentially secure a speaking slot at the Event!** It's a way to contribute your knowledge, showcase your brand, and connect with others in the industry.
+
+**What presentations are expected?**
+
+We're in search of compelling and insightful client cases, stories, or trend forecasts. The content you bring should hold significant value and appeal to our Expo attendees.
+
+**How does it work?**
+
+Submit your presentation proposal by **Friday, September 29**, and be chosen by the voters and Advisory Board members to join our agenda! Participation is the competition is free of charge. 
+
+Are you ready to showcase your expertise and insights? 
+
+**Learn all the details and apply now:**
+
+@component('mail::button', ['url' => "https://ecommerceberlin.com/speaking?utm_source=email&utm_medium=email&utm_campaign=system_mailing_exhibitors"])
+Submit your presentation proposal
+@endcomponent
+
+Best of luck!
+
+{{$footer}}
+
+@endcomponent
