b23ddeccb9bc6cdec273b5b97d4fb9aa20a49515
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-10-19 15:02:46 +0200

    teh24 update 1

---
+++ resources/views/emails/visitor/teh24-update1.blade.php
@@ -0,0 +1,32 @@
+
+
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+Dziękujemy za rejestrację jako Uczestnik Targów eHandlu :)
+
+
+Ważne informacje
+
+**TYLKO DO KOŃCA DNIA** trwają **zapisy na Warsztaty** [Agenda 18 warsztatów i zapisy](https://targiehandlu.pl/workshops). Możesz zapisać się na 6 warsztatów (1 w każdym slocie godzinowym) - nie planujemy drugiej tury zgłoszeń. Trenerzy zabierają się za przeglądanie zgłoszeń... 
+
+Opublikowaliśmy **[kompletną listę prezentacji na scenach A, B, C i D](https://targiehandlu.pl/schedule)**, z którą warto się zapoznać by lepiej zaplanować swój dzień targowy.
+
+**Bilety wstępu do druku (PDF)** będziemy rozsyłali **23 października** (w poniedziałek). 
+
+@component('mail::panel')
+
+Przypominamy, że 24. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny z prezentacjami Ekspertów** oraz **150 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+@endcomponent
+
+
+Pozdrawiamy,
+
+Aleksandra, Karolina & Karolina
+
+
+@endcomponent
