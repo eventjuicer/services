38b2b1cb77e85c48c83ba3a4e74d8eb272bd2abd
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-10-19 15:21:26 +0200

    workshops for old

---
+++ resources/views/emails/visitor/teh24-workshops-for-old-lastminute.blade.php
@@ -0,0 +1,78 @@
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+# Tylko do końca dnia możesz zgłosić chęć udziału w bezpłatnych Warsztatach Targów eHandlu.
+
+Mamy tylko 432 miejsca, 18 warsztatów.
+
+@component('mail::panel')
+
+**NOWA FORMUŁA** W tej edycji możesz zgłosić się aż na 6 warszatatów - jeden w każdym slocie godzinowym.
+
+@endcomponent
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/workshops'])
+Sprawdź listę Warsztatów i zgłoś się
+@endcomponent
+
+
+## Wybrane tematy
+
+**jak zbierać i prezentować opinie**, by twoje reklamy produktowe nie zostały zablokowane.
+
+O czym należy pamiętać wdrażając **duży sklep na PrestaShop**?
+
+**Analiza konkurencji w Google Ads i Social Ads**
+
+**PrestaShop Checkout**
+
+Jak **efektywnie zarządzać dostawami i zwrotami.**
+
+Analiza RFM. Jak ją wykorzystać do **personalizacji reklam Meta Ads i osiągnąć ROAS +3000%?**
+
+**Allegro Ads** - praktyczny przewodnik
+
+...**wykorzystanie klubów członkowskich** w budowaniu retencji.
+
+
+[Lista wszystkich warsztatów, zgłoszenia](https://targiehandlu.pl/workshops)
+
+
+## Zasady
+
+**Udział** w Warsztatch w tej edycji jest **bezpłatny.**
+
+Akceptacja zgłoszeń przez Prowadzących właśnie się rozpoczyna.
+
+Otrzymasz wiadomość zwrotną przy akceptacji.
+
+Każdy Uczestnik Warsztatów **otrzymuje status VIP** (dostęp do Strefy VIP dla Wystawców i Prelegentów)
+
+Czas warsztatu to maksymalnie **45 minut** (30min + 15min na Q&A)
+
+
+@component('mail::panel')
+
+Przypominamy, że 24. Targi eHandlu odbędą się **27 października w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny** z prezentacjami Ekspertów oraz **150 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+...**oraz 18 warsztatów**
+
+@endcomponent
+
+
+**Niezależnie od warsztatów [dostępne są aż 4 sceny prezentacyjne](https://targiehandlu.pl/visit)** (udział bezpłatny, otwarty, wymagana rejestracja jako Zwiedzający)
+
+
+
+Pozdrawiamy,
+
+Zespół Targów eHandlu
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+
+@endcomponent
+
