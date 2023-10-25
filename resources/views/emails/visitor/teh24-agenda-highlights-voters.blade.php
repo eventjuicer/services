a5931846ec07cc781b25730902a818fae45a0091
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-10-25 12:56:20 +0200

    teh24-agenda-highlights-voters

---
+++ resources/views/emails/visitor/teh24-agenda-highlights-voters.blade.php
@@ -0,0 +1,65 @@
+@component('mail::message')
+
+Cześć 
+
+@component('mail::panel')
+
+Targi eHandlu już w najbliższy piątek na EXPO XXI w Warszawie.
+
+Szykuje się inspirująca edycja.**Blisko 160 Wystawców**, 4 sceny prezentacyjne z najbardziej gorącymi tematami w E-commerce, m.in. **AI**, **Google Analytics 4**
+
+Wstęp na EXPO i Prezentacje otwarty - wymagana jedynie rejestracja.
+
+@endcomponent
+
+Czy warto? Warto! :)
+
+## Agenda ujawniona! (wybrane tematy)
+
+Start selling with eBay / **Rozpocznij sprzedaż na eBay**
+
+...dlaczego **Chiny** pozostaną dla nas najważniejszym źródłem **importu**.
+
+Dlaczego duże i zaawansowane sklepy wybierają **PrestaShop?**
+
+Poznaj skuteczne sposoby na **podniesienie konwersji,** poprzez komunikację email, SMS oraz push
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Odbierz bilet
+@endcomponent
+
+Co musisz wiedzieć o **atrybucji w Google Analytics 4?**
+
+**Demand Gen - nowy format w Google Ads.** Jak wpłynie na konwersje w e-commerce?
+
+**E-commerce na TikTok'u** - czy to ma sens?
+
+Na co uważać przy tworzeniu **opisów produktów z AI?**
+
+[Lista wszystkich prezentacji, bilet za 0 złotych](https://targiehandlu.pl/visit)
+
+[...] Do's and don'ts w relacji klient ↔ agencja
+
+Od zera do eksportera [...]
+
+E-commerce Przyszłości: Jak **Personalizacja Zmienia Zasady Gry**
+
+Rozpocznij sprzedaż międzynarodową z **Kaufland Global Marketplace**
+
+AI, opinie, Omnibus, Google i cross-border w e-commerce– jak to wszystko pogodzić i nie zwariować?
+
+## ...i wiele innych.
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Odbierz bilet
+@endcomponent
+
+Pozdrawiamy,
+
+Zespół Targów eHandlu
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+
+@endcomponent
+
