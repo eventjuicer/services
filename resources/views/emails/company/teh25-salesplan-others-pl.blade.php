9a600062a895dc489eabb4e695c0903978e493a3
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-11-09 13:48:19 +0100

    teh25 salesplan

---
+++ resources/views/emails/company/teh25-salesplan-others-pl.blade.php
@@ -0,0 +1,24 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na 25. Targi eHandlu (11 kwietnia 2024 w Warszawie - EXPO XXI)
+
+# Od dzisiaj do wtorku - Sprzedaż większych powierzchni 
+
+Sprzedaż dla Wystawców jesiennych Targów eHandlu zainteresowanych większą przestrzenią (3 i więcej stoisk) 
+
+[Układ hali, ceny](https://targiehandlu.pl/exhibit)
+
+**Prośba o info zwrotne jeśli taką przestrzenią zainteresowana jest Twoja firma**
+
+# Środa - 15 listopada 11:00
+
+Start sprzedaży dla Wystawców spoza Top30 rankingu Konkursu dla Wystawców poprzedniej edycji Targów; Start otwartej sprzedaży.
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
---
+++ resources/views/emails/company/teh25-salesplan-top30-pl.blade.php
@@ -0,0 +1,28 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na 25. Targi eHandlu (11 kwietnia 2024 w Warszawie - EXPO XXI)
+
+# Od dzisiaj do wtorku - Sprzedaż hurtowa powierzchni 
+
+Sprzedaż dla Wystawców jesiennych Targów eHandlu zainteresowanych większą przestrzenią - **3 i więcej stoisk**
+
+[Układ hali, ceny](https://targiehandlu.pl/top30_13x2apxw7)
+
+**Prośba o info zwrotne jeśli taką przestrzenią zainteresowana jest Twoja firma**
+
+# Wtorek - 14 listopada 11:00
+
+**Ekskluzywna sprzedaż dla firm z TOP30** Konkursu dla Wystawców czyli dla Twojej firmy
+
+## Środa - 15 listopada 11:00
+
+Sprzedaż otwarta dla pozostałych zainteresowanych
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
