420cabf9c4f0cf3eacd03b7cd9b80c6f631d2925
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-11-13 13:44:25 +0100

    salesplan

---
+++ resources/views/emails/company/teh25-salesplan2-others-pl.blade.php
@@ -0,0 +1,18 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na 25. Targi eHandlu (11 kwietnia 2024 w Warszawie - EXPO XXI)
+
+# Środa - 15 listopada 11:00
+
+Start sprzedaży dla Wystawców spoza Top30 rankingu Konkursu dla Wystawców poprzedniej edycji Targów; Start otwartej sprzedaży.
+
+
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
---
+++ resources/views/emails/company/teh25-salesplan2-top30-pl.blade.php
@@ -0,0 +1,20 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na 25. Targi eHandlu (11 kwietnia 2024 w Warszawie - EXPO XXI)
+
+
+# Wtorek - 14 listopada 11:00
+
+**Ekskluzywna sprzedaż dla firm z TOP30** Konkursu dla Wystawców czyli dla Twojej firmy
+
+**Środa - 15 listopada 11:00** - Sprzedaż otwarta dla pozostałych zainteresowanych
+
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
