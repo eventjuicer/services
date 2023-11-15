566388d64a45e4f821054c6e06cc9b0905a41558
Adam Zygadlewicz <adam@zygadlewicz.com>
2023-11-15 13:22:47 +0100

    teh25 opensales

---
+++ resources/views/emails/company/teh25-sales-started-en.blade.php
@@ -0,0 +1,17 @@
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, "fname")  }},
+
+**You can now book an exhibition space for the upcoming Ecommerce Warsaw Expo** Spring edition!
+
+The date of the event: 11th of April 2024 (EXPO XXI Warsaw)
+
+Visit [targiehandlu.pl/exhibit](https://targiehandlu.pl/exhibit) to check available booths, prices and reserve a space.
+
+Some booths are already reserved by TOP30 of the Exhibitor's Contest (winners of the earlybird booking prize)
+
+Best regards
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/teh25-sales-started-pl.blade.php
@@ -0,0 +1,20 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+**ruszyła otwarta sprzedaż** dla wszystkich zainteresowanych powierzchnią wystawienniczą na wiosennych Targach eHandlu. 
+
+Data targów: 11 kwietnia w Warszawie - EXPO XXI.
+
+**Mapka i ceny dostępne na [targiehandlu.pl/exhibit](https://targiehandlu.pl/exhibit)**
+
+UWAGA: system nie pozwala na jednoczesną rezerwację 2 stoisk z tej samej kategorii cenowej (np 2x HOT, 2x ULTRA). W przypadku chęci rezerwacji stoisk w takiej konfiguracji prosimy o oddzielne transakcje.
+
+UWAGA (2): niedostępne przestrzenie (oznaczone kolorem szarym) - to stoiska z przedsprzedaży "hurtowej" (>2 przestrzeni) lub sprzedaży dla TOP30 konkursu dla Wystawców (zdobywców nagrody "Rezerwacja EarlyBird" )
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
