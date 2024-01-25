88e247c709a0bae6899a7497b76793f7a12ab365
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-01-25 16:10:23 +0100

    /promote newsletter template update (#43)

--- resources/views/emails/exhibitor/ebe-invite2.blade.php
+++ resources/views/emails/exhibitor/ebe-invite2.blade.php
@@ -9,9 +9,9 @@
 
 # Hi,
 
-We would like to let you know that **{{  array_get($companydata, "name") }} is exhibiting at the E-Commerce Berlin Expo 2023** which will take place on **23rd of February at STATION Berlin**, Luckenwalder Str. 4-6.
+We would like to let you know that **{{  array_get($companydata, "name") }} is exhibiting at the E-Commerce Berlin Expo 2024** which will take place on **22rd of February at STATION Berlin**, Luckenwalder Str. 4-6.
 
-**Get your free ticket** now and stop by our stand to chat with us:**
+**Get your free ticket** now and stop by our stand to chat with us:
 
 @component('mail::button', ['url' => $promolink])
 	Click here to register for a free ticket
@@ -21,7 +21,7 @@ We would like to let you know that **{{  array_get($companydata, "name") }} is e
 
 All the major decision-makers from digital commerce, marketing, and innovation come together at E-commerce Berlin Expo for a one-of-a-kind networking experience.
 
-**Over 250 companies will exhibit** and **over 70 leading experts** from giants such as **Borussia Dortmund, Zalando, Meta, OMIO, eBay, EVENTIM, Mercedes-Benz Cars, Paul Hewitt, Amorelie, Farfetch and Thomann.io,** will present innovative and forward-looking developments and trends to will help you find new opportunities to grow your e-commerce business.
+**Over 280 companies will exhibit** and **over 75 leading experts** from giants such as **TikTok, Zalando, flaconi, BMW, Lieferando, HelloFresh, Philips, and Casio,** will present innovative and forward-looking developments and trends to will help you find new opportunities to grow your e-commerce business.
 
 E-commerce Berlin Expo enables you to connect with other like-minded individuals striving for success, find partnerships that will last for years, as well as expand your knowledge and skillset to stay ahead of the competition.
 
