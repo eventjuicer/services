fba405446fd3fbaa14bb02ab642c424c59b2b12a
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-01-30 15:52:44 +0100

    Update bulk_en.blade.php

--- resources/views/emails/meetups/bulk_en.blade.php
+++ resources/views/emails/meetups/bulk_en.blade.php
@@ -3,21 +3,22 @@
 @component('mail::message')
  
 
-# Hi, {{$p->translate("[[fname]]")}},
+# Hi {{$p->translate("[[fname]]")}},
 
-## Successful networking is crucial part of every business event. We have developed a tool to have that networking boosted. 
+## Hope you're getting ready for the E-commerce Berlin Expo! 
 
 @component('mail::panel')
-Selected exhibitors have an exclusive and limited access to the list of names of registered visitors. They are allowed to choose people they would love to meet during an expo. If you receive this message, you were chosen! Now decide if you share that interest...or not :) If you approve the meetup, exhibitor would be able to contact with you directly and schedule the meeting.
+Exciting news – one of our exhibitors is eager to connect with you and has sent you a meeting request.
+If you're interested, click the button below to approve the meeting. Once approved, the exhibitor will reach out directly to schedule an appointment.
 @endcomponent
 
 @component('mail::button', ['url' => $url])
 Approve or reject meetup requests
 @endcomponent
 
-Looking forward to see you! 
+It's that simple! Happy networking, and wishing you fruitful talks and connections at the Expo.
 
-Kind Regards, 
+Best! 
 Lucas Zarna, E-commerce Berlin Expo Team.
 
 PS: Link does not work? try this - {!!$url!!}
