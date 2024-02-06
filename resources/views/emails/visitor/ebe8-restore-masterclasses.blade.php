2b251670e448b6d9c6975b74c118229046c9d08a
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-02-06 21:24:39 +0100

    ebe8-restore-masterclasses.blade.php

---
+++ resources/views/emails/visitor/ebe8-restore-masterclasses.blade.php
@@ -0,0 +1,35 @@
+@component('mail::message')
+
+Hello {{ $p->translate("[[fname]]") }},
+
+**Don't forget to register for this year's E-commerce Berlin Expo masterclasses in our free-to-attend format**: 
+    
+# Masterclasses.
+
+Take advantage of this opportunity to learn from the **experts from** renowned brands such as  **Lemundo, Adobe, 7Learnings, ChannelEngine, DOOFINDER, Sitecore, Brevo, Kaufland Global Marketplace, ZEOS and Taxdoo.** With topics varying from **multichannel marketing to e-commerce logistics, and growth strategies**, there's something for everyone. 
+
+[Check the full masterclasses agenda here](https://ecommerceberlin.com/masterclasses)
+
+## Now, what do you have to know about the Masterclasses?
+
+@component('mail::panel')
+
+**The registration lasts only until on Wednesday (14.02) at 5:00 pm CET**
+
+Participation is **free of charge**, but the **seats are limited to 40/masterclass**.
+
+You can only take part in three sessions (and then take part in our 5 public stages!)
+
+@endcomponent
+
+**Presenters will review all applications and we will notify you via email if you have been accepted**. If there won’t be a free slot for you - no worries! You will be able to apply for another Masterclass or just come to the Expo and benefit from 5 open stages and > 240 exhibitors.
+
+[Check the full masterclasses agenda here](https://ecommerceberlin.com/masterclasses)
+
+See you at E-commerce Berlin Expo 2024!
+
+Best regards,
+
+EBE Team
+
+@endcomponent
