70cdfda0de79d2225549ea467837eba55d2ed7d6
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-02-06 12:29:15 +0100

    ebe8-masterclasses-open.blade.php (#46)
    * /promote newsletter template update
    
    * EMAIL - VIP + C2P
    
    * Update bulk_en.blade.php
    
    * ebe8-masterclasses-open.blade.php

---
+++ resources/views/emails/visitor/ebe8-masterclasses-open.blade.php
@@ -0,0 +1,38 @@
+@component('mail::message')
+
+Hello {{ $p->translate("[[fname]]") }},
+    
+# Masterclasses applications are now OPEN
+
+Take advantage of this opportunity to learn from the **experts from** renowned brands such as **Lemundo, Adobe, 7Learnings, ChannelEngine, DOOFINDER, Sitecore, Brevo, Kaufland Global Marketplace, ZEOS and Taxdoo.** 
+With topics varying from **multichannel marketing to e-commerce logistics, and growth strategies**, there's something for everyone. 
+
+[Check the full masterclasses agenda and apply here](https://ecommerceberlin.com/masterclasses)
+
+## Now, what do you have to know about the Masterclasses?
+
+@component('mail::panel')
+
+**Masterclasses are part of E-commerce Berlin Expo** (Do you remember the date and location?  **22nd FEBRUARY 2024 @ STATION Berlin**.)
+
+Participation is **free of charge**, but the **seats are limited to 40/masterclass**.
+
+You can only take part in three sessions.
+
+**Presenters will review all applications and we will notify you via email if you have been accepted** (If not: you will be able to apply for another Masterclass)
+
+Each participant of the Masterclass has access to the rest of the Expo: **280 exhibitors and 5 public stages.**
+
+@endcomponent
+
+[Check the full masterclasses agenda and apply here](https://ecommerceberlin.com/masterclasses)
+
+See you at E-commerce Berlin Expo 2024!
+
+Best regards,
+
+EBE Team
+
+**[Newsletter configuration]({{ $unsubscribe }})**
+
+@endcomponent
