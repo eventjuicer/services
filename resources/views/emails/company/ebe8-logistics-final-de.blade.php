9e17df2718c71ded5c56d1cbe88a47fef53a1e56
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-02-08 17:26:25 +0100

    ebe8-logistics-final

---
+++ resources/views/emails/company/ebe8-logistics-final-de.blade.php
@@ -0,0 +1,55 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+
+Die E-commerce Berlin Expo 2024 kommt wieder am Donnerstag, den 22. Februar in die STATION Berlin und wir freuen uns, dich als einen unserer Aussteller begrüßen zu dürfen. Um dich für unsere Veranstaltung bereit zu machen, haben wir eine Menge hilfreicher Informationen vorbereitet.
+
+**EBE 2024 | Wann und wo?**
+
+E-commerce Berlin Expo
+
+STATION Berlin
+
+Luckenwalder Str 4-6, 10963 Berlin
+
+**Donnerstag, 22. Februar von 9:00 bis 18:00 Uhr** (Öffnungszeiten für Besucher)
+
+Hier sind auch einige Faktoren zu beachten, während du dich auf die Messe vorbereitest. Du kannst auf jeden Faktor klicken, um zu deinem Ausstellerpanel weitergeleitet zu werden und alle benötigten Informationen zu finden.
+
+## EBE 2024 | Aufbautag - 21. Februar
+
+[Eingang](https://account.ecommerceberlin.com/#/logistics?pre_welcome)
+
+[Willkommenspakete](https://account.ecommerceberlin.com/#/logistics?pre_welcomepacks)
+
+[Standbau](https://account.ecommerceberlin.com/#/logistics?preassembly)
+
+[Parken](https://account.ecommerceberlin.com/#/logistics?pre_parking)
+
+## EBE 2024 | Messetag - 22. Februar
+
+[Parken ist am Messetag nicht auf dem Gelände möglich](https://account.ecommerceberlin.com/#/logistics?parking)
+
+[Eingang](https://account.ecommerceberlin.com/#/logistics?welcome)
+
+[Problemmeldung](https://account.ecommerceberlin.com/#/logistics?problems)
+
+[Standabbau](https://account.ecommerceberlin.com/#/logistics?disassembly)
+
+## EBE 2024 | Nach der Messe
+
+[Exportiere Daten von gescannten Badges](https://account.ecommerceberlin.com/#/logistics?leads)
+
+[Details zur Lagerung und Abholung von Materialien](https://account.ecommerceberlin.com/#/logistics?packages)
+
+Hier ist auch die Karte, auf der du den Plan für die Expo sehen kannst. 
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/v1707404211/entrance_SB_ebe8.pdf" alt="" style="max-width: 700px;" />
+
+Lass uns wissen, wenn du Fragen hast!
+
+Wir sehen uns in Berlin!
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/ebe8-logistics-final-en.blade.php
@@ -0,0 +1,55 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+
+The E-commerce Berlin Expo 2024 is coming back to STATION Berlin on Thursday, February 22nd and we are happy to welcome you as one of our Exhibitors. To get you up and running for our event, we’ve prepared a bunch of helpful information. 
+
+**EBE 2024 | When and Where?**
+
+E-commerce Berlin Expo
+
+STATION Berlin
+
+Luckenwalder Str 4-6, 10963 Berlin
+
+**Thursday / February 22nd, from 9.00 AM to 6.00 PM** ­(Opening hours for visitors) 
+
+Here are also some factors to take into account whilst preparing for the expo. You can click on each factor to be redirected to your Exhibitors panel and find all the information needed.
+
+## EBE 2024 | Assembly Day - 21st of February
+
+[Entrance](https://account.ecommerceberlin.com/#/logistics?pre_welcome)
+
+[Welcome Packs](https://account.ecommerceberlin.com/#/logistics?pre_welcomepacks)
+
+[Stand Assembly](https://account.ecommerceberlin.com/#/logistics?preassembly)
+
+[Parking](https://account.ecommerceberlin.com/#/logistics?pre_parking)
+
+## EBE 2024 | Expo Day - 22nd of February
+
+[Parking is not possible in the venue during the expo day](https://account.ecommerceberlin.com/#/logistics?parking)
+
+[Entrance](https://account.ecommerceberlin.com/#/logistics?welcome)
+
+[Reporting Problems](https://account.ecommerceberlin.com/#/logistics?problems)
+
+[Stand disassembly](https://account.ecommerceberlin.com/#/logistics?disassembly)
+
+## EBE 2024 | After the Expo
+
+[Export data from scanned badges](https://account.ecommerceberlin.com/#/logistics?leads)
+
+[Storage and parcel pickup details](https://account.ecommerceberlin.com/#/logistics?packages)
+
+Here is also the map where you can see the plan for the map at the Expo. Please note that the map and entries only apply to assembly day (21.02). 
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/v1707404211/entrance_SB_ebe8.pdf" alt="" style="max-width: 700px;"/>
+
+­Let us know if you have any questions!
+
+See you in Berlin!
+
+{{$footer}}
+
+@endcomponent
