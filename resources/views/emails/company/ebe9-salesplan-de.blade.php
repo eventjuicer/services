184700244f91094022584db91fe9b1ea4ca627ea
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-02-27 13:29:58 +0100

    ebe9-salesplan

---
+++ resources/views/emails/company/ebe9-salesplan-de.blade.php
@@ -0,0 +1,21 @@
+@component('mail::message')
+
+# Hallo {{ array_get($profile, "fname")  }},
+
+Die 8. Ausgabe der E-Commerce Berlin Expo war ein voller Erfolg, und wir freuen uns, die bevorstehende Ausgabe anzukündigen, die für **2 Tage am 19. & 20. Februar 2025** geplant ist. Planst du nächstes Jahr teilzunehmen? Stelle sicher, dass du den Verkaufsplan überprüfst, um wichtige Termine für die Buchung deiner Fläche im Auge zu behalten!
+
+## DIENSTAG, 05.03.2024
+
+Die Unternehmen, die als Aussteller an der Expo teilgenommen haben, erhalten exklusiven Zugang zum Kauf einer Fläche. Da du ausgestellt hast, erhältst du einen speziellen Link zur Landingpage. Dies ist exklusiv für dich als Teil der 8. Ausgabe, und diese Early Bird Runde dauert nur 1 Tag.
+
+## MITTWOCH, 06.03.2024
+
+Und zu guter Letzt werden alle anderen Zugang zur Verkaufskarte haben, wo du nach Leads und Möglichkeiten suchen kannst! 
+
+Also schnall dich an und mach dich bereit. Wir freuen uns darauf, am 19. & 20. Februar 2025 für **2 Tage** zurückzukehren, und freuen uns darauf, dich dort zu sehen. Wenn du Fragen hast, zögere nicht, uns zu kontaktieren.
+
+Liebe Grüße
+
+{{$footer}}
+ 
+@endcomponent
---
+++ resources/views/emails/company/ebe9-salesplan-en.blade.php
@@ -0,0 +1,21 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+
+The 8th edition of the E-commerce Berlin Expo was a resounding success, and we're thrilled to announce the upcoming edition, scheduled for **2 days, February 19 & 20, 2025**. Planning to participate next year? Make sure you check out the Sales schedule to stay up-to-date on important dates for booking your space!
+
+## TUESDAY 05/03/2024
+
+The Companies who participated in the Expo as exhibitors will gain exclusive access to purchase a space. Since you were exhibiting, you will receive a special link to the landing page. This is exclusive for you for being a part of the 8th edition, and this Early Bird round will last only 1 day.
+
+## WEDNESDAY 06/03/2024
+
+Last but not least, everyone else will have access to the sales map, where you can hunt for leads and opportunities! 
+
+So fasten your seatbelts and get ready. We're excited to come back on February 19 & 20, 2025, for **2 days**, and look forward to seeing you there. If you have any questions, feel free to reach out.
+
+Cheers
+
+{{$footer}}
+ 
+@endcomponent
