73ed02176a462faf98601095dc62bced2991656a
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-03-04 12:28:51 +0100

    Update ebe9-salesplan-reminder-en.blade.php

--- resources/views/emails/company/ebe9-salesplan-reminder-en.blade.php
+++ resources/views/emails/company/ebe9-salesplan-reminder-en.blade.php
@@ -1,3 +1,5 @@
+@component('mail::message')
+
 # Hi {{ array_get($profile, "fname") }},
 
 We would like to remind you of the limited-time Early Bird sales round for Exhibitors of the previous edition of the E-commerce Berlin Expo, which will begin tomorrow March 5th, 2024 at 11:00 AM.
