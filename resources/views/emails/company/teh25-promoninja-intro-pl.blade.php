4a24fad1ce1ec4ed9e5def38f48b97b7c23adf0f
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-03-20 11:14:37 +0100

    teh25 promoninja

--- resources/views/emails/company/teh25-promoninja-intro-pl.blade.php
+++ resources/views/emails/company/teh25-promoninja-intro-pl.blade.php
@@ -2,37 +2,49 @@
  
 # Cześć {{ array_get($profile, "fname") }},
 
-**Udostępniliśmy materiały promocyjne**: bannery, szablony newslettera, linki do udostępniania profilu Wystawcy i tym samym kolejną edycję Konkursu dla Wystawców!
+Do puli 6 nagród dla firm promujących swój profil Wystawcy oraz udział w Targach eHandlu dodaliśmy **2 nowe super-nagrody.**
 
-## Materiały
+## Pakiet Partnera Strefy VIP
 
-@component('mail::button', ['url' => $accountUrlPromote])
-Strona z materiałami dla Twojej firmy
+## Wyróżnienie firmy na mapie Wystawców w papierowym katalogu
+
+Pakiet Partnera Strefy VIP to możliwość zorganizowania dodatkowej ekspozycji (4m2) w Strefie VIP oraz realizowane przez nas świadczenia reklamowe (brandowane opaski dla VIPów, reklama na donicach). Wartość pakietu to ok 10 000 zł.
+
+@component('mail::button', ['url' => $accountUrlRanking])
+Sprawdź ranking i już przyznane świadczenia
 @endcomponent
 
-**Chcesz podesłać stronę z materiałami promocyjnymi innej osobie?**
+(WAŻNE: po kliknięciu na każdą nagrodę otrzymasz informację o warunkach jej przyznania - np minimalnej liczbie punktów)
 
-Prześlij ten link: {{$accountUrlPromotePublic}} - umożliwia on korzystanie z materiałów promocyjnych z wyłączeniem dostępu do innych części panelu.
+## Jak skutecznie wspinać się w rankingu? 
 
-Wszystkie materiały kierują [do Twojego publicznego profilu Wystawcy]({{$profileUrl}}). Ta strona nie tylko opisuje Twoją firmę ale także:
+Promuj publiczny profil Wystawcy]({{$profileUrl}}) wykorzystując własne kanały - np serwisy społecznościowe lub bazy newsletterowe.
+
+Publiczny profil Wystawcy nie tylko opisuje Twoją firmę ale także:
 
 * pokazuje **lokalizację Twojego stoiska** na hali targowej
 
 * pozwala Zwiedzającym **umówić się na spotkanie** (po akceptacji takiej propozycji masz dostęp do pełnych danych kontaktowych)
 
-**Ruch na Twoim publicznym profilu Wystawcy to punkty, które wymieniamy na super korzyści**: **dostęp do puli early-bird** na kolejną edycję Targów, dostęp do **większej puli zaproszeń** Zwiedzających na spotkanie czy **większa liczbę zaproszeń VIP.**
+## Jak to działa?
+
+Ruch na Twoim publicznym profilu Wystawcy to punkty, które wymieniamy na super korzyści, m.in. **dostęp do puli early-bird** na kolejną edycję Targów, dostęp do **większej puli zaproszeń** Zwiedzających na spotkanie czy **większa liczbę zaproszeń VIP.**
+
+## Materiały promocyjne
+
+**Udostępniliśmy przykładowe materiały promocyjne**: bannery, szablony newslettera, linki do udostępniania profilu Wystawcy
+
+@component('mail::button', ['url' => $accountUrlPromote])
+Strona z materiałami dla Twojej firmy
+@endcomponent
+
+**Chcesz podesłać stronę z materiałami promocyjnymi innej osobie?** Prześlij ten link: {{$accountUrlPromotePublic}} - umożliwia on korzystanie z materiałów promocyjnych z wyłączeniem dostępu do innych części panelu.
 
 Pozdrawiam, 
 
 {{$footer}}
 
-Jeśli chcesz się wyróżnić i masz możliwość przygotowania własnych materiałów graficznych - do kierowania na profil Wystawcy użyj "gołego" linka, który został podany na końcu strony z materiałami.
 
-...ten link to:
-
-@component('mail::panel')
-[{{ $trackingLink }}]({{ $trackingLink }}) 
-@endcomponent
 
 
 @endcomponent
