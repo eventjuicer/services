f000f8a434beb2693bd5a2d735e009f334db7a0f
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-04-05 14:21:51 +0200

    linkedin voters emails

---
+++ resources/views/emails/visitor/teh25-agenda-highlights-voters.blade.php
@@ -0,0 +1,52 @@
+@component('mail::message')
+
+Cześć 
+
+@component('mail::panel')
+
+Targi eHandlu już w czwartek  na EXPO XXI w Warszawie.
+
+Szykuje się inspirująca edycja.**Blisko 150 Wystawców**, **4 sceny** prezentacyjne z najbardziej gorącymi tematami w E-commerce czyli 
+
+# "Sztuczna inteligencja" :)
+
+Wstęp na EXPO i Prezentacje otwarty - wymagana jedynie rejestracja.
+
+@endcomponent
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Odbierz bilet
+@endcomponent
+
+
+## Wybrane tematy prezentacji
+
+**GA4 "low-hanging fruits"** dla poprawy współczynnika konwersji
+
+Rozwój sprzedaży **eobuwie.pl** /MODIVO na rynku **szwedzkim**
+
+Zwiększ sprzedaż, zbuduj markę i rozwiń swój międzynarodowy biznes z **Kaufland Global Marketplace!**
+
+**Sztuczna inteligencja dla contact center** i działów sprzedaży: case studies w e-commerce
+
+Jak wykorzystać **możliwości LLM/ modeli językowych do automatyzowania tworzenia treści** w ecommerce
+
+Jak AI dopasuje Twój feed do kontekstu w ekosystemie cookieless - case study
+
+Czy sztuczna inteligencja może przewidzieć pytania klienta zanim klient je Wam zada?
+
+Zatrudnij sobie armię stażystów - czyli **ile oszczędzamy na wdrożeniach AI?**
+
+
+[Lista wszystkich prezentacji, bilet za 0 PLN](https://targiehandlu.pl/visit)
+
+
+Pozdrawiamy,
+
+Zespół Targów eHandlu
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+
+@endcomponent
+
