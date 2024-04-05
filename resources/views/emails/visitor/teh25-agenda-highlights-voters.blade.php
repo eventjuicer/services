e1ac7947522b078d36dac4a73c3e3dca3ed6953d
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-04-05 14:43:47 +0200

    Update teh25-agenda-highlights-voters.blade.php

--- resources/views/emails/visitor/teh25-agenda-highlights-voters.blade.php
+++ resources/views/emails/visitor/teh25-agenda-highlights-voters.blade.php
@@ -19,15 +19,12 @@ Wygeneruj bilet
 # Agenda wiosennej edycji Targów eHandlu została zdominowana przez tzw. "Sztuczną Inteligencję" :)
 
 
-**GA4 "low-hanging fruits"** dla poprawy współczynnika konwersji
 
-Rozwój sprzedaży **eobuwie.pl** /MODIVO na rynku **szwedzkim**
-
-Zwiększ sprzedaż, zbuduj markę i rozwiń swój międzynarodowy biznes z **Kaufland Global Marketplace!**
 
 **Sztuczna inteligencja dla contact center** i działów sprzedaży: case studies w e-commerce
 
-[Lista wszystkich prezentacji](https://targiehandlu.pl/visit)
+Zatrudnij sobie armię stażystów - czyli **ile oszczędzamy na wdrożeniach AI?**
+
 
 Jak wykorzystać **możliwości LLM/ modeli językowych do automatyzowania tworzenia treści** w ecommerce
 
@@ -35,9 +32,13 @@ Jak AI dopasuje Twój feed do kontekstu w ekosystemie cookieless - case study
 
 Czy sztuczna inteligencja może przewidzieć pytania klienta zanim klient je Wam zada?
 
-Zatrudnij sobie armię stażystów - czyli **ile oszczędzamy na wdrożeniach AI?**
+[Lista wszystkich prezentacji](https://targiehandlu.pl/visit)
 
+**GA4 "low-hanging fruits"** dla poprawy współczynnika konwersji
 
+Rozwój sprzedaży **eobuwie.pl** /MODIVO na rynku **szwedzkim**
+
+Zwiększ sprzedaż, zbuduj markę i rozwiń swój międzynarodowy biznes z **Kaufland Global Marketplace!**
 
 
 @component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
