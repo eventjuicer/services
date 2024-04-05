2bd015f403ecac26b86de662ecba73779f87964c
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-04-05 14:50:40 +0200

    Update teh25-agenda-highlights-voters.blade.php

--- resources/views/emails/visitor/teh25-agenda-highlights-voters.blade.php
+++ resources/views/emails/visitor/teh25-agenda-highlights-voters.blade.php
@@ -4,9 +4,9 @@ Cześć
 
 @component('mail::panel')
 
-Targi eHandlu już w czwartek na EXPO XXI w Warszawie.
+Targi eHandlu już **w czwartek na EXPO XXI w Warszawie.**
 
-Wstęp na EXPO i wszystkie sceny prezentacyjne jest otwarty - wymagana jedynie rejestracja.
+**Wstęp** na EXPO i wszystkie sceny prezentacyjne jest **otwarty** - wymagana jedynie rejestracja.
 
 **34 prezentacje na 4 scenach i 150 Wystawców**
 
@@ -25,20 +25,13 @@ Wygeneruj bilet
 
 Zatrudnij sobie armię stażystów - czyli **ile oszczędzamy na wdrożeniach AI?**
 
-
 Jak wykorzystać **możliwości LLM/ modeli językowych do automatyzowania tworzenia treści** w ecommerce
 
 Jak AI dopasuje Twój feed do kontekstu w ekosystemie cookieless - case study
 
 Czy sztuczna inteligencja może przewidzieć pytania klienta zanim klient je Wam zada?
 
-[Lista wszystkich prezentacji](https://targiehandlu.pl/visit)
-
-**GA4 "low-hanging fruits"** dla poprawy współczynnika konwersji
-
-Rozwój sprzedaży **eobuwie.pl** /MODIVO na rynku **szwedzkim**
-
-Zwiększ sprzedaż, zbuduj markę i rozwiń swój międzynarodowy biznes z **Kaufland Global Marketplace!**
+[Lista pozostałych 29 prezentacji](https://targiehandlu.pl/visit)
 
 
 @component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
