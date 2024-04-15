9eb7d0c6d6a011fab34ab093bc0183e2ceaa5dcf
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-04-15 13:16:58 +0200

    Update ewa-salesplan-scans-pl.blade.php

--- resources/views/emails/company/ewa-salesplan-scans-pl.blade.php
+++ resources/views/emails/company/ewa-salesplan-scans-pl.blade.php
@@ -6,7 +6,7 @@ Potargowy tydzień zaczynamy od 3 tematów :)
 
 ## Skany
 
-Jeśli Przedstawiciele Twojej firmy skanowali identyfikatory Zwiedzających podczas czwartkowych Targów - prosimy o pilne pobranie listy z danymi kontaktowymi z panelu.
+Jeśli Przedstawiciele Twojej firmy skanowali identyfikatory Zwiedzających podczas czwartkowych Targów - prosimy o pilne pobranie listy z danymi kontaktowymi z panelu. **Uwaga:** Lista zawiera dane kontaktowe Uczestników z wszystkich edycji, podczas których skanowaliście identyfikatory. Aby rozwiać ew. wątpliwości należy skorzystać z opcji EXPORT. Pobrany plik zawiera kolumnę z datą skanowania.
 
 @component('mail::button', ['url' => $accountUrlScans])
 Dane kontaktowe z zeskanowanych identyfikatorów Zwiedzających 
@@ -22,15 +22,15 @@ Wypełnij ankietę satysfakcji
 
 ## Sprzedaż stoisk na kolejną edycję Targów.
 
-Jesienne Targi eHandlu odbędą się 25 października 2024 na EXPO XXI.
+Jesienne Targi eHandlu odbędą się **25 października 2024 na EXPO XXI.**
 
-Hurtowa sprzedaż stoisk (min. 3 stoiska) ruszy w najbliższy czwartek - 18 kwietnia.
+**Hurtowa sprzedaż stoisk** (min. 3 stoiska) ruszy w najbliższy **czwartek - 18 kwietnia**.
 
 **Sprzedaż dla TOP30 Konkursu** dla Wystawców **23 kwietnia (wtorek)**
 
 **Early-bird** (dla pozostałych Wystawców) + sprzedaż otwarta - **24 kwietnia (środa)**
 
-(Jeśli nie pamiętasz swojego miejsca w rankingu Konkursu dla Wystawców, w czwartek otrzymasz od nas informację kiedy odbędzie się sprzedaż konkretnie dla Twojej firmy)
+(Jeśli nie pamiętasz swojego miejsca w rankingu Konkursu dla Wystawców, w TEN czwartek otrzymasz od nas informację kiedy odbędzie się sprzedaż dla Twojej firmy.)
 
 
 Regards, 
