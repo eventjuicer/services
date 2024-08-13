59ef61d6c7f90a1321a0a09714b7c7f533e4e5b8
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-08-13 11:53:02 +0200

    ebe9-premium-services

---
+++ resources/views/emails/company/ebe9-premium-services-de.blade.php
@@ -0,0 +1,39 @@
+@component('mail::message')
+
+# Hallo {{ array_get($profile, 'fname') }},
+
+Bist du bereit, auf der E-commerce Berlin Expo 2025 einen bleibenden Eindruck zu hinterlassen?
+
+Während du dich auf die E-commerce Berlin Expo 2025 vorbereitest, lohnt es sich, unsere Premium-Sponsoring-Services in Betracht zu ziehen, um die Präsenz deiner Marke zu stärken und nachhaltig zu beeindrucken.
+
+@component('mail::panel')
+
+## Wichtige Premium-Sponsoring Möglichkeiten:
+
+**[VIP-Zone Sponsoring](https://ecommerceberlin.com/premium/vipzone):** Präsentiere deine Marke in unserer exklusiven 800 m² großen VIP-Zone – eine einmalige Chance, dich bei einflussreichen Teilnehmern hervorzuheben.
+
+**[Registrierungs-Sponsoring](https://ecommerceberlin.com/premium/registration):** Hinterlasse einen unvergesslichen ersten Eindruck, indem du den Registrierungsbereich und das Anmeldeformular sponserst – so steht deine Marke sofort im Mittelpunkt, sobald die Teilnehmer eintreffen.
+
+**[Marken-Lanyards Sponsoring](https://ecommerceberlin.com/premium/lanyards):** Steigere die Sichtbarkeit deiner Marke, indem du dein Logo auf allen Besucher-Lanyards platzierst.
+
+**[Besucher-Badges Sponsoring](https://ecommerceberlin.com/premium/badges):** Platziere dein Logo auf jedem Besucherausweis und sorge dafür, dass deine Marke stets im Rampenlicht steht.
+
+**[Networking Event Sponsoring](https://ecommerceberlin.com/premium/networking):** Werde zum Mittelpunkt des exklusivsten und am meisten erwarteten Events der Expo – dem Networking-Event, bei dem sich Branchenführer treffen. Diese besondere Gelegenheit bringt deine Marke ins Zentrum des Geschehens und bietet dir eine unschätzbare Plattform für hochkarätige Verbindungen.
+
+@endcomponent
+
+Diese exklusiven Möglichkeiten sind darauf ausgelegt, deiner Marke außergewöhnliche Sichtbarkeit und Interaktion zu verschaffen.
+
+Entdecke weitere Sponsoring-Optionen: Wir bieten diverse Services, die auf unterschiedliche Budgets und Ziele zugeschnitten sind, um die Präsenz deiner Marke auf der Expo weiter zu erhöhen.
+
+**Mehr erfahren:** [Entdecke Premium-Möglichkeiten](https://ecommerceberlin.com/premium)
+
+Lass uns gerne darüber sprechen, wie wir diese Möglichkeiten perfekt auf die Ziele deiner Marke abstimmen können. Hierzu antworte einfach auf diese E-Mail, oder vereinbare direkt einen Termin mit mir über den Link in der Signatur.
+
+Wir freuen uns darauf, gemeinsam etwas Außergewöhnliches zu schaffen!
+
+Beste Grüße,
+
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/ebe9-premium-services-en.blade.php
@@ -0,0 +1,37 @@
+@component('mail::message')
+ 
+# Hello {{ array_get($profile, 'fname') }},
+
+As you prepare for the E-commerce Berlin Expo 2025, consider our premium sponsorship services to enhance your brand's presence and make a significant impact.
+
+@component('mail::panel')
+
+## Key Premium Sponsorship Opportunities:
+
+**[VIP Zone Sponsorship](https://ecommerceberlin.com/premium/vipzone):** Stand out in our exclusive 800 m² VIP Zone - an exceptional opportunity to spotlight your brand to influential attendees.
+
+**[Registration Sponsorship](https://ecommerceberlin.com/premium/registration):** Make a lasting first impression by sponsoring the registration area and form - putting your brand front and center as attendees arrive.
+
+**[Branded Lanyards Sponsorship](https://ecommerceberlin.com/premium/lanyards):** Elevate your brand's visibility with your logo on all event lanyards.
+
+**[Visitor Badge Sponsorship](https://ecommerceberlin.com/premium/badges):** Put your logo on every visitor badge and ensure your brand is in the spotlight.
+
+**[Networking Event Sponsorship](https://ecommerceberlin.com/premium/networking):** Become the centrepiece of the Expo’s most exclusive and anticipated event—the networking gathering where industry leaders come together. This unique opportunity puts your brand at the heart of the action, offering an invaluable platform for high-profile connections.
+
+@endcomponent
+
+These exclusive opportunities are meticulously crafted to deliver unparalleled exposure and engagement for your brand.
+
+Discover more sponsorship options: We offer various services to fit different budgets and goals, helping you elevate your brand's presence at the Expo.
+
+**Learn more here:** [Explore Premium Sponsorships](https://ecommerceberlin.com/premium)
+
+We’d love to discuss how we can tailor these opportunities to align perfectly with your brand’s goals. Feel free to reply to this email or schedule a meeting at your convenience using the link in my footer.
+
+Looking forward to collaborating on something extraordinary!
+
+Best regards,
+
+{{$footer}}
+
+@endcomponent
