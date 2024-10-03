1aed20e95bfe71f7633dbe239558cfa0172ecca3
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2024-10-03 10:50:32 +0200

    invite100 update

--- resources/views/emails/exhibitor/invite101.blade.php
+++ resources/views/emails/exhibitor/invite101.blade.php
@@ -10,9 +10,9 @@
 
 # Hi,
 
-## We would like to let you know that {{  array_get($companydata, "name") }} is exhibiting at the E-Commerce Warsaw Expo 2024 on **April 11th** at EXPO XXI (Prądzyńskiego 12/14 str.). 
+## We would like to let you know that {{  array_get($companydata, "name") }} is exhibiting at the E-Commerce Warsaw Expo 2024 on **October 25th** at EXPO XXI (Prądzyńskiego 12/14 str.). 
 
-We’re excited for a day fully dedicated to e-commerce best practices &amp; trends. 
+We’re excited for a day fully dedicated to e-commerce best practices & trends. 
 
 Come have a chat with us at our stand.
 
@@ -20,7 +20,7 @@ Come have a chat with us at our stand.
 	Register for a free ticket here
 @endcomponent
 
-Let us also mention that there will be 4 stages full of speakers and almost 135 other service providers at their booths. 
+Let us also mention that there will be 4 stages full of speakers and over 150 other service providers at their booths. 
 
 The event is a must-attend! 
 
