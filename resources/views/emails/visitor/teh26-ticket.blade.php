748c55fdb0aa1224e3fa86310b4ddad288b51e9f
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-10-21 12:58:17 +0200

    teh26/ticketdownload

---
+++ resources/views/emails/visitor/teh26-ticket.blade.php
@@ -0,0 +1,33 @@
+@component('mail::message')
+
+Cześć {{$p->translate("[[fname]]") }} 
+
+Targi eHandlu odbędą się **25 października (piątek) w Warszawie (EXPO XXI)**
+
+Genialna agenda prezentacji! 
+
+M.in. wywiady z CEO Tezeusz.pl, CEO IMKER, Head of E-commerce Coffeedesk, wystąpienia przedstawicieli Trefl S.A., InPost Pay, Wygodne Zwroty, Kaufland Global Marketplace
+
+# [IDĘ / Pobieram bilet]({!! $going !!})
+
+## [Nie Idę]({!! $notGoing !!})
+
+@component('mail::panel')
+
+**150 produktów i usług e-commerce** w ramach przestrzeni wystawienniczej
+
+**34 prelegentów** w ramach 4 otwartych scen prezentacyjnych
+
+Dostęp do Strefy VIP jeśli umówisz się z Wystawcą na spotkanie
+
+@endcomponent
+
+
+
+
+
+Do zobaczenia! 
+
+Zespół Targów eHandlu
+
+@endcomponent
