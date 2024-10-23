d1b7a4e0e688b3ccbb1d7fc832e9cfd0a138727b
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-10-23 14:58:49 +0200

    Update teh26-restore3.blade.php

--- resources/views/emails/visitor/teh26-restore3.blade.php
+++ resources/views/emails/visitor/teh26-restore3.blade.php
@@ -11,81 +11,48 @@ Targi eHandlu odbędą się już **w najbliższy piątek** - 25 października.
 
 Oto kilka wybranych prezentacji, które - mamy taką nadzieję - zachęcą Ciebie do uczestnictwa.
 
-<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
 
-<tr>
-         <td>  
 
-        <img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1727852445/p_234317_logotype.png" alt="" />
-            <strong>  Quick Wins: Sprawdzone strategie personalizacji e-maili, które zwiększają sprzedaż w e-commerce.</strong>
-       
-          
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1727852445/p_234317_logotype.png" alt="" /><br/>
+<strong>  Quick Wins: Sprawdzone strategie personalizacji e-maili, które zwiększają sprzedaż w e-commerce.</strong>
+
+
 Personalizacja jest zbyt ważna, by z niej rezygnować. Aż 80% kupujących mówi, że chętniej kupuje w sklepach, które oferują spersonalizowane doświadczenie. Dlatego to kluczowa strategia w e-commerce. Jednak jakie podejścia naprawdę działają? W tej prezentacji Jessika podzieli się skutecznymi strategiami i wskazówkami dotyczącymi personalizacji, popartymi danymi z MailerLite oraz historiami sukcesów jej użytkowników. Dowiesz się, jak zastosować te same strategie w swoich kampaniach e-mail, aby poprawić doświadczenia klientów oraz zwiększyć sprzedaż.
-    </td>
-    </tr>
-
-    
-
-    <tr>
-        
-        <td>  
-
-        <img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1726652384/p_234921_logotype.jpg" alt="" /><br/><br />
-        <img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1727792175/p_236759_logotype.jpg" alt="" />
-            <strong> Case study na żywo - klient opowiada jak zwiększył sprzedaż dzięki opiniom. </strong>
-       
-        "Podczas prelekcji: - Wyjaśnimy dlaczego warto zbierać zarówno o opinie na temat sklepu, jak i te dotyczące produktów, - Pokażemy jak ciekawy sposób ich prezentacji pozwala zwiększyć zaangażowanie kupujących. - Wyjaśnimy jak pogłębione opinie wzbogacone o sondaże, atrybuty produktowe i cechy użytkownika wpływają na trafność decyzji zakupowych i ograniczenie liczby zwrotów."
-    </td>
-    </tr>
-
-    
-    <tr>
-        
-        <td>  
-
-        <img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1726216971/p_234862_logotype.png" alt="" />
-
-            <strong>5 powodów, dla których Twoje SEO nie działa w 2024
-            </strong>
-       
-        (...) "nierealne oczekiwania, brak popytu lub niedostosowanie się do trendów, dług technologiczny oraz brak spójnej strategii i współpracy w organizacji. Dowiedz się, jak uniknąć tych pułapek i sprawić, by Twoje SEO w 2025 roku było naprawdę skuteczne."
-    </td>
-    </tr>
 
 
-    <tr>
-   
-        <td>  
 
-        <img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1712507836/p_233877_logotype.jpg" alt="" />
-        
-            <strong>  Jak skutecznie zarządzać zwrotami w e-commerce: Strategia minimalizacji kosztów i budowania lojalności klientów </strong>
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1727792175/p_236759_logotype.jpg" alt="" /><br/>
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1726652384/p_234921_logotype.jpg" alt="" /><br/>
 
-            Prezentacja omawia sposoby na minimalizację kosztów i usprawnienie procesów operacyjnych, ale także podkreśli znaczenie zwrotów jako narzędzia do wzmacniania relacji z klientami i budowania zaufania do marki.
+<strong> Case study na żywo - klient opowiada jak zwiększył sprzedaż dzięki opiniom. </strong>
+
+"Podczas prelekcji: - Wyjaśnimy dlaczego warto zbierać zarówno o opinie na temat sklepu, jak i te dotyczące produktów, - Pokażemy jak ciekawy sposób ich prezentacji pozwala zwiększyć zaangażowanie kupujących. - Wyjaśnimy jak pogłębione opinie wzbogacone o sondaże, atrybuty produktowe i cechy użytkownika wpływają na trafność decyzji zakupowych i ograniczenie liczby zwrotów."
 
 
-        
-    </td>
-    </tr>
+
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1726216971/p_234862_logotype.png" alt="" /><br/>
+
+<strong>5 powodów, dla których Twoje SEO nie działa w 2024
+</strong>
+
+(...) "nierealne oczekiwania, brak popytu lub niedostosowanie się do trendów, dług technologiczny oraz brak spójnej strategii i współpracy w organizacji. Dowiedz się, jak uniknąć tych pułapek i sprawić, by Twoje SEO w 2025 roku było naprawdę skuteczne."
 
 
-    <tr>
-        <td width="125" style="width: 125px;"><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1726226085/p_234791_logotype.jpg" alt="" /></td>
-        <td>  
-            <strong>10 najczęstszych błędów w kampaniach e-commerce. Wyeliminuj je i skaluj performance!
-  </strong>
-       
-  Poznaj konkretne problemy i wnioski płynące z ponad 100 przeprowadzonych audytów kont reklamowych. Zadbaj o to, by rozwój Twojego e-commerce był szybszy, eliminując przy tym najpopularniejsze błędy na kontach reklamowych.
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1712507836/p_233877_logotype.jpg" alt="" />
+
+<strong>  Jak skutecznie zarządzać zwrotami w e-commerce: Strategia minimalizacji kosztów i budowania lojalności klientów </strong>
+
+Prezentacja omawia sposoby na minimalizację kosztów i usprawnienie procesów operacyjnych, ale także podkreśli znaczenie zwrotów jako narzędzia do wzmacniania relacji z klientami i budowania zaufania do marki.
 
 
-    </td>
-    </tr>
+<img src="https://res.cloudinary.com/eventjuicer/image/upload/w_125,c_fit,f_auto/v1726226085/p_234791_logotype.jpg" alt="" />
 
-    
-    
-  
 
-</table>
+<strong>10 najczęstszych błędów w kampaniach e-commerce. Wyeliminuj je i skaluj performance!
+</strong>
+
+Poznaj konkretne problemy i wnioski płynące z ponad 100 przeprowadzonych audytów kont reklamowych. Zadbaj o to, by rozwój Twojego e-commerce był szybszy, eliminując przy tym najpopularniejsze błędy na kontach reklamowych.
+
 
 
 
