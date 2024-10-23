50d3273d87285e83f24314f61906e66382d4b1b1
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-10-23 14:16:40 +0200

    teh26/old2

---
+++ resources/views/emails/visitor/teh26-restore3.blade.php
@@ -0,0 +1,175 @@
+
+
+@component('mail::message')
+ 
+# Cześć {{ $p->translate("[[fname]]") }},
+
+Targi eHandlu odbędą się już **w najbliższy piątek** - 25 października.
+
+
+
+[Agenda wystąpień i wywiadów](https://targiehandlu.pl/visit)
+
+
+Oto kilka wybranych prezentacji, które - mamy taką nadzieję - zachęcą Ciebie do uczestnictwa.
+
+<table style="font-family:Avenir,Helvetica,sans-serif;box-sizing:border-box;color:#74787e;font-size:16px;line-height:1.5em;margin-top:0;text-align:left;border-spacing:30px;">
+
+<tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_200,h_200,c_limit,f_auto/v1727852445/p_234317_logotype.png" alt="" /></td>
+        <td>  
+            <strong>  Quick Wins: Sprawdzone strategie personalizacji e-maili, które zwiększają sprzedaż w e-commerce.</strong>
+       
+          
+Personalizacja jest zbyt ważna, by z niej rezygnować. Aż 80% kupujących mówi, że chętniej kupuje w sklepach, które oferują spersonalizowane doświadczenie. Dlatego to kluczowa strategia w e-commerce. Jednak jakie podejścia naprawdę działają? W tej prezentacji Jessika podzieli się skutecznymi strategiami i wskazówkami dotyczącymi personalizacji, popartymi danymi z MailerLite oraz historiami sukcesów jej użytkowników. Dowiesz się, jak zastosować te same strategie w swoich kampaniach e-mail, aby poprawić doświadczenia klientów oraz zwiększyć sprzedaż.
+    </td>
+    </tr>
+
+    
+
+    <tr>
+        <td>
+            <img src="https://res.cloudinary.com/eventjuicer/image/upload/w_200,h_200,c_limit,f_auto/v1726652384/p_234921_logotype.jpg" alt="" /><br/><br />
+        <img src="https://res.cloudinary.com/eventjuicer/image/upload/w_200,h_200,c_limit,f_auto/v1727792175/p_236759_logotype.jpg" alt="" />
+        </td>
+        <td>  
+            <strong> Case study na żywo - klient opowiada jak zwiększył sprzedaż dzięki opiniom. </strong>
+       
+        "Podczas prelekcji: - Wyjaśnimy dlaczego warto zbierać zarówno o opinie na temat sklepu, jak i te dotyczące produktów, - Pokażemy jak ciekawy sposób ich prezentacji pozwala zwiększyć zaangażowanie kupujących. - Wyjaśnimy jak pogłębione opinie wzbogacone o sondaże, atrybuty produktowe i cechy użytkownika wpływają na trafność decyzji zakupowych i ograniczenie liczby zwrotów."
+    </td>
+    </tr>
+
+    
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_200,h_200,c_limit,f_auto/v1726216971/p_234862_logotype.png" alt="" /></td>
+        <td>  
+            <strong>5 powodów, dla których Twoje SEO nie działa w 2024
+            </strong>
+       
+        (...) "nierealne oczekiwania, brak popytu lub niedostosowanie się do trendów, dług technologiczny oraz brak spójnej strategii i współpracy w organizacji. Dowiedz się, jak uniknąć tych pułapek i sprawić, by Twoje SEO w 2025 roku było naprawdę skuteczne."
+    </td>
+    </tr>
+
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_200,h_200,c_limit,f_auto/v1725007836/p_233877_logotype.jpg" alt="" /></td>
+        <td>  
+            <strong>  Jak skutecznie zarządzać zwrotami w e-commerce: Strategia minimalizacji kosztów i budowania lojalności klientów </strong>
+
+            Prezentacja omawia sposoby na minimalizację kosztów i usprawnienie procesów operacyjnych, ale także podkreśli znaczenie zwrotów jako narzędzia do wzmacniania relacji z klientami i budowania zaufania do marki.
+
+
+        
+    </td>
+    </tr>
+
+
+    <tr>
+        <td><img src="https://res.cloudinary.com/eventjuicer/image/upload/w_200,h_200,c_limit,f_auto/v1726226085/p_234791_logotype.jpg" alt="" /></td>
+        <td>  
+            <strong>10 najczęstszych błędów w kampaniach e-commerce. Wyeliminuj je i skaluj performance!
+  </strong>
+       
+  Poznaj konkretne problemy i wnioski płynące z ponad 100 przeprowadzonych audytów kont reklamowych. Zadbaj o to, by rozwój Twojego e-commerce był szybszy, eliminując przy tym najpopularniejsze błędy na kontach reklamowych.
+
+
+    </td>
+    </tr>
+
+    
+    
+  
+
+</table>
+
+
+
+@component('mail::panel')
+
+Targi eHandlu już **w piątek na EXPO XXI w Warszawie.**
+
+**Wstęp** na EXPO i wszystkie sceny prezentacyjne jest **otwarty** - wymagana jedynie rejestracja online.
+
+**34 prezentacje na 4 scenach i 150 Wystawców**
+
+@component('mail::button', ['url' => "https://targiehandlu.pl/visit"])
+Chcę pobrać bilet
+@endcomponent
+
+@endcomponent
+
+...a to tylko mały wycinek oferty merytorycznej Targów. Nowością w formule Targów są wywiady.
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Wygeneruj swój bilet
+@endcomponent
+
+
+**Marek Kich** - prowadzący podcast **"Sztuka E-Commerce"** zaprasza na wywiady z Goścmi Specjalnymi 
+
+## Kojarzysz antykwariat Tezeusz.pl? 
+
+@component('mail::panel')
+
+Gościem Marka Kicha będzie Andrzej Miszk - CEO Tezeusz.pl. 
+
+Poznaj historię firmy, który sprzedała 1,5mln używanych ksiązek i posiada 14 filii zakupowych.
+
+Andrzej Miszk opowie, jak rozwijać i zarządzać biznesem, który dla wielu jest dziś inspiracją.
+
+@endcomponent
+
+
+
+
+## Interesuje Ciebie self-publishing? Wiesz która firma odpowiadała za logistykę takich bestsellerów jak "Finansowy Ninja" czy "Finansowa Forteca"?
+
+@component('mail::panel')
+
+Marek Kich wypyta Krzysztofa Bartnika - CEO IMKER - czy sprzedaż e-booków i kursów online nadal się opłaca i jakie są warunki skutecznej sprzedaży produktów cyfrowych w Internecie.
+
+@endcomponent
+
+
+@component('mail::button', ['url' => 'https://targiehandlu.pl/visit'])
+Wygeneruj swój bilet
+@endcomponent
+
+
+## Trzecim gościem Marka Kicha będzie Remigiusz Chrzanowski - Chief Partner Sales Officer Health Labs Care.
+
+@component('mail::panel')
+"Apteka Gemini, Super Pharm, DOZ, a dzisiaj Health Labs Care. Można powiedzieć, że Remigiusz Chrzanowski zdobył koronę polskiej farmacji. Postanowiłem go o tę drogę zapytać na scenie. Podczas wywiadu porozmawiamy o różnicach w zarządzaniu dużym i trochę mniejszym e-Commerce, o sprzedaży D2C i perfekcyjnej obsłudze klienta."
+
+@endcomponent
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Z pozdrowieniami,
+
+Karolina 
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
+
+
+
+
+
+
+
+
