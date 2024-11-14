e9325740c0969a47bbe9a4e2f0718ea602dd3b8c
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-11-14 08:57:37 +0100

    fixes

--- resources/views/emails/company/ebe9-additional-services-de.blade.php
+++ resources/views/emails/company/ebe9-additional-services-de.blade.php
@@ -2,18 +2,19 @@
 
 # Hallo {{ array_get($profile, "fname") }},
 
-**Die E-Commerce Berlin Expo 2025 rückt immer näher!** Deshalb möchten wir dich darauf hinweisen, dass du auf der Ausstellerplattform immer noch zusätzliche Dienste wahrnehmen kannst, wie z.B.:**
+**Die E-Commerce Berlin Expo 2025 rückt immer näher! **Deshalb möchten wir dich darauf hinweisen, dass du auf der Ausstellerplattform immer noch zusätzliche Dienste wahrnehmen kannst, wie z.B.:**
 
 - Vollständiger Standbau,
 - Teppichbelag,
 - Broschürenhalter,
 - zusätzliche Stromleistung.
 
-Falls du ein Standardmöbelset für deinen Stand wünschst, bestehend aus 2 Hockern und einem Tisch, stelle bitte sicher, dass du diese Option so schnell wie möglich [hier](https://account.ecommerceberlin.com/#/arrangement/2832/show) ankreuzt. Andernfalls wird die Lieferung nicht erfolgen.
-
 @component('mail::button', ['url' => $accountUrlArrangement])
 Zu meinem Account gelangen
 @endcomponent
+
+
+Falls du ein Standardmöbelset für deinen Stand wünschst, bestehend aus 2 Hockern und einem Tisch, stelle bitte sicher, dass du diese Option so schnell wie möglich [hier](https://account.ecommerceberlin.com/#/arrangement/2832/show) ankreuzt. Andernfalls wird die Lieferung nicht erfolgen.
 
 Die Bestellfrist für den kompletten Standaufbau ist der **16. Dezember 2024.** Bei Interesse stelle sicher, dass du deine Bestellung frühzeitig machst, da die Preise später steigen und die Verfügbarkeit begrenzt ist. (Bitte beachte dabei, dass die Ausstellungsfläche keine Rückwand und keinen Teppich beinhaltet. Es ist eine offene Fläche, die du selbst gestalten kannst)
 
--- resources/views/emails/company/ebe9-additional-services-en.blade.php
+++ resources/views/emails/company/ebe9-additional-services-en.blade.php
@@ -2,18 +2,21 @@
 
 # Hello {{ array_get($profile, "fname") }},
 
-**The E-commerce Berlin Expo 2025 is getting closer and closer!** We would like to inform you that in your exhibitor's account admin panel **you can now order additional services, such as:**
+**The E-commerce Berlin Expo 2025 is getting closer and closer! **We would like to inform you that in your exhibitor's account admin panel **you can now order additional services, such as:**
 
 - complete booth arrangement with full print back wall, counter, carpet and leaflet holder,
 - carpet equipment,
 - foldable brochure rack display,
 - additional electricity connection.
 
-If you wish to have a standard set of furniture delivered to your stand: 2 hockers and one table, please make sure to tick this option [here](https://account.ecommerceberlin.com/#/arrangement/2832/show) as soon as possible. Otherwise they will not be delivered. 
-
 @component('mail::button', ['url' => $accountUrlArrangement])
 Sign Into My Account
 @endcomponent
+
+
+If you wish to have a standard set of furniture delivered to your stand: 2 hockers and one table, please make sure to tick this option [here](https://account.ecommerceberlin.com/#/arrangement/2832/show) as soon as possible. Otherwise they will not be delivered. 
+
+
 
 The deadline for the complete booth arrangement orders is **16th December, 2024.**  If you are interested, please make sure to place your order as soon as possible as later prices will go up and availability may run out of stock. (Be aware that your basic exhibition space doesn't include a back wall and a carpet. It's an open space you can arrange on your own)
 
