bc4892a64420f86e764efa93286fc7cce3793c0d
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2025-01-03 16:59:50 +0100

    ebe9-electricity

---
+++ resources/views/emails/company/ebe9-electricity-de.blade.php
@@ -0,0 +1,24 @@
+@component('mail::message')
+
+# Hallo {{ array_get($profile, "fname") }},
+
+Um sicherzustellen, dass ihr für die Expo bestmöglich vorbereitet seid, möchten wir Euch daran erinnern, dass ihr noch immer extra Strom bestellen könnt.
+
+Plant ihr, Laptops oder Kaffeemaschinen an euren Stand zu bringen? Bitte prüft vorher, wie viel Stromanschluss ihr habt. Wenn ihr mehr als 500W oder 1000W, je nach dem Austellerpaket, verwenden möchtet, musst du zusätzlichen Strom über die Ausstellerplattform bestellen.
+
+Die Standard-Stromversorgung für einen Stand beträgt 500 W (für 9 m² große Stände) oder 1000 W (für 18 m² große Stände). Diese Menge an Strom ist **nur ausreichend**, um ein paar Laptops und einen Fernseher zu betreiben.
+
+Je nach Bedarf könnt ihr weiterhin extra Strom bestellen - 1000W oder 3000W. Hierfür bitte die  untenstehende 'Standdienste' anklicken, die gewünschte Stromoption wählen und bestellen. 
+
+Bitte beachtet, dass **Bestellungen bis spätestens 17. JANUAR** möglich sind.
+
+@component('mail::button', ['url' => 'https://account.ecommerceberlin.com/#/arrangement']) Standdienste
+@endcomponent
+
+Falls noch Fragen zur Vorbereitung auf die Expo bestehen, stehe ich gerne zur Verfügung.
+
+Beste Grüße,
+ 
+{{$footer}}
+
+@endcomponent
---
+++ resources/views/emails/company/ebe9-electricity-en.blade.php
@@ -0,0 +1,24 @@
+@component('mail::message')
+
+# Hello {{ array_get($profile, "fname") }},
+
+To ensure that you are almost ready to participate in the Expo, we have a gentle reminder about electricity connection to the exhibitor booth.
+
+Are you planning to bring laptops or coffee machines to your booth? Check how much electricity connection you have. If you are going to use more than 500W or 1000W depending on your package, you will have to purchase additional power through the exhibitor platform.
+
+The standard electric power connected to a stand is 500W (for 9 sqm stands) or 1000W (for 18 sqm stands). This amount of power is **only sufficient** to power a few laptops and a TV.
+
+You can still order an additional electricity connection - 1000W or 3000W depending on your needs, simply click on the link below and choose electricity connection.
+
+Please note that the **deadline of placing your order is JANUARY 17th**.
+
+@component('mail::button', ['url' => 'https://account.ecommerceberlin.com/#/arrangement']) Stand arrangement
+@endcomponent
+
+If you have any questions regarding preparation for the Expo, please get back to me and I’ll be happy to assist you.
+
+Regards,
+
+{{$footer}}
+
+@endcomponent
