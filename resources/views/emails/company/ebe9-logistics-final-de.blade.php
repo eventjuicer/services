045612129965668ca64b522793b553d098bb8a08
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-02-14 14:02:05 +0100

    logistics

---
+++ resources/views/emails/company/ebe9-logistics-final-de.blade.php
@@ -0,0 +1,56 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+
+Die E-commerce Berlin Expo 2025 kommt wieder am Mittwoch, den 19. Februar in die STATION Berlin und wir freuen uns, dich als einen unserer Aussteller begrüßen zu dürfen. Um dich für unsere Veranstaltung bereit zu machen, haben wir eine Menge hilfreicher Informationen vorbereitet.
+
+**EBE 2025 | Wann und wo?**
+
+E-commerce Berlin Expo
+
+STATION Berlin
+
+Luckenwalder Str 4-6, 10963 Berlin
+
+**Mittwoch und Donnerstag, 19. und 20. Februar, 1. Tag 9:00-18:00; 2. Tag 9:00-17:00** (Öffnungszeiten für Besucher)
+
+Hier sind auch einige Faktoren zu beachten, während du dich auf die Messe vorbereitest. Du kannst auf jeden Faktor klicken, um zu deinem Ausstellerpanel weitergeleitet zu werden und alle benötigten Informationen zu finden.
+
+## EBE 2025 | Aufbautag - 18. Februar
+
+[Eingang](https://account.ecommerceberlin.com/#/logistics?pre_welcome)
+
+[Willkommenspakete](https://account.ecommerceberlin.com/#/logistics?pre_welcomepacks)
+
+[Standbau](https://account.ecommerceberlin.com/#/logistics?preassembly)
+
+[Parken](https://account.ecommerceberlin.com/#/logistics?pre_parking)
+
+## EBE 2025 | Messetage - 19. und 20. Februar
+
+[Parken ist am Messetag nicht auf dem Gelände möglich](https://account.ecommerceberlin.com/#/logistics?parking)
+
+[Eingang](https://account.ecommerceberlin.com/#/logistics?welcome)
+
+[Problemmeldung](https://account.ecommerceberlin.com/#/logistics?problems)
+
+[Standabbau](https://account.ecommerceberlin.com/#/logistics?disassembly)
+
+## EBE 2025 | Nach der Messe
+
+[Exportiere Daten von gescannten Badges](https://account.ecommerceberlin.com/#/logistics?leads)
+
+[Details zur Lagerung und Abholung von Materialien](https://account.ecommerceberlin.com/#/logistics?packages)
+
+Hier ist auch die Karte, auf der du den Plan für die Expo sehen kannst. 
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/v1738945877/Logistics/EXHIBITOR_INFO_POINT_SECTORS_C_D_E_F_5.png" alt="" style="max-width: 700px;"/>
+
+Lass uns wissen, wenn du Fragen hast!
+
+Wir sehen uns in Berlin!
+
+{{$footer}}
+
+@endcomponent
+
---
+++ resources/views/emails/company/ebe9-logistics-final-en.blade.php
@@ -0,0 +1,55 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+
+The E-commerce Berlin Expo 2025 is coming back to STATION Berlin on Wednesday, February 19th and we are happy to welcome you as one of our Exhibitors. To get you up and running for our event, we’ve prepared a bunch of helpful information.
+
+**EBE 2025 | When and Where?**
+
+E-commerce Berlin Expo
+
+STATION Berlin
+
+Luckenwalder Str 4-6, 10963 Berlin
+
+**Wednesday and Thursday  / February 19th and 20th, 1st day 9:00-18:00; 2nd day 9:00-17:00** ­(Opening hours for visitors) 
+
+Here are also some factors to take into account whilst preparing for the Expo. You can click on each factor to be redirected to your Exhibitors panel and find all the information needed.
+
+## EBE 2025 | Assembly Day - 18th of February
+
+[Entrance](https://account.ecommerceberlin.com/#/logistics?pre_welcome)
+
+[Welcome Packs](https://account.ecommerceberlin.com/#/logistics?pre_welcomepacks)
+
+[Stand Assembly](https://account.ecommerceberlin.com/#/logistics?preassembly)
+
+[Parking](https://account.ecommerceberlin.com/#/logistics?pre_parking)
+
+## EBE 2025 | Expo Days - 19th and 20th of February
+
+[Parking is not possible in the venue during the expo day](https://account.ecommerceberlin.com/#/logistics?parking)
+
+[Entrance](https://account.ecommerceberlin.com/#/logistics?welcome)
+
+[Reporting Problems](https://account.ecommerceberlin.com/#/logistics?problems)
+
+[Stand disassembly](https://account.ecommerceberlin.com/#/logistics?disassembly)
+
+## EBE 2025 | After the Expo
+
+[Export data from scanned badges](https://account.ecommerceberlin.com/#/logistics?leads)
+
+[Storage and parcel pickup details](https://account.ecommerceberlin.com/#/logistics?packages)
+
+Here is also the map where you can see the plan for the map at the Expo. Please note that the map and entries only apply to assembly day (18.02). 
+
+<img src="https://res.cloudinary.com/ecommerceberlin/image/upload/v1738945877/Logistics/EXHIBITOR_INFO_POINT_SECTORS_C_D_E_F_5.png" alt="" style="max-width: 700px;"/>
+
+­Let us know if you have any questions!
+
+See you in Berlin!
+
+{{$footer}}
+
+@endcomponent
