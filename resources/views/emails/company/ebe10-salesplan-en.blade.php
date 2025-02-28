101a16019acd29464ede1c4064dd8952ffbc979a
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2025-02-28 09:18:25 +0100

    ebe10-salesplan

---
+++ resources/views/emails/company/ebe10-salesplan-en.blade.php
@@ -0,0 +1,22 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+
+The 9th edition of the E-commerce Berlin Expo was a resounding success, and we're thrilled to announce the upcoming edition, scheduled for **2 days, February 17 & 18, 2026** at Messe Berlin. This new location offers a larger, more dynamic space to accommodate the growing number of exhibitors and attendees. 
+Planning to participate next year? Make sure you check out the timings of the Sales schedule to stay up-to-date on important dates for booking your space!
+
+## TUESDAY 04/03/2025
+
+The Companies who participated in the Expo as exhibitors will gain exclusive access to purchase a space. Since you were exhibiting, you will receive a special link to the landing page. This is exclusive for you for being a part of the 9th edition, and this Early Bird round will last only 1 day.
+
+## WEDNESDAY 05/03/2025
+
+Last but not least, everyone else will have access to the booking map, where you can hunt for leads and opportunities! 
+
+So fasten your seatbelts and get ready. We're excited to come back on February 17 & 18, 2025, for **2 days**, and look forward to seeing you there. If you have any questions, feel free to reach out.
+
+Cheers
+
+{{$footer}}
+ 
+@endcomponent
