f5b29fef875b55df730b9ae79ce6e2208bce1ecb
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-02-28 12:02:14 +0100

    ebe10 salesplan
    resolves https://github.com/ecommerceberlin/exhibitors/issues/227

---
+++ resources/views/emails/company/ebe10-salesplan-de.blade.php
@@ -0,0 +1,21 @@
+@component('mail::message')
+
+# Hallo {{ array_get($profile, "fname")  }},
+
+Die 9. Ausgabe der E-Commerce Berlin Expo war ein voller Erfolg, und wir freuen uns, die bevorstehende Ausgabe anzukündigen, die für **2 Tage am 17. & 18. Februar 2026** geplant ist und auf der Messe Berlin stattfindet. Dieser neue Veranstaltungsort bietet einen größeren, dynamischeren Raum, um die wachsende Zahl an Ausstellern und Besuchern unterzubringen. Planst du nächstes Jahr teilzunehmen? Stelle sicher, dass du die Timings des Verkaufsplans überprüfst, um wichtige Termine für die Buchung deiner Fläche im Auge zu behalten!
+
+## DIENSTAG, 04.03.2025
+
+Die Unternehmen, die als Aussteller an der Expo teilgenommen haben, erhalten exklusiven Zugang zum Kauf einer Fläche. Da du ausgestellt hast, erhältst du einen speziellen Link zur Landingpage. Dies ist exklusiv für dich als Teil der 9. Ausgabe, und diese Early Bird Runde dauert nur 1 Tag.
+
+## MITTWOCH, 05.03.2025
+
+Zu guter Letzt werden alle anderen Zugang zur Buchungskarte haben, wo du nach Leads und Möglichkeiten suchen kannst! 
+
+Also schnall dich an und mach dich bereit. Wir freuen uns darauf, am 17. & 18. Februar 2026 für **2 Tage** zurückzukehren, und freuen uns darauf, dich dort zu sehen. Wenn du Fragen hast, zögere nicht, uns zu kontaktieren.
+
+Liebe Grüße
+
+{{$footer}}
+ 
+@endcomponent
