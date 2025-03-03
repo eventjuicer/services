ae64c125fbfecbec5f321c18e8cc863a0ecbc192
KarolinaTEH <80756258+KarolinaTEH@users.noreply.github.com>
2025-03-03 21:13:30 +0100

    ebe10-salesplan-starts

---
+++ resources/views/emails/company/ebe10-salesplan-starts-de.blade.php
@@ -0,0 +1,22 @@
+@component('mail::message')
+
+# Ready, set, GO!
+
+@component('mail::panel')
+
+Heute (5. März, 11 Uhr CET) starten die Buchungen für die **10. E-commerce Berlin Expo** – am **17. & 18. Februar 2026** in der Messe Berlin!
+
+Sichere dir jetzt deinen **Top-Standplatz zum besten Preis** – schnell sein lohnt sich, denn weg ist weg!
+
+Am Tag der Expo sind die Stände aus dem Pre-Sale-Inventar als nicht verfügbare, grau markierte Flächen gekennzeichnet.
+
+Jetzt buchen:  [ecommerceberlin.com/exhibit](https://ecommerceberlin.com/exhibit)
+
+@endcomponent
+
+**Wichtig:** Du kannst nicht zwei Plätze aus derselben Preiskategorie buchen (z. B. 2x Hot, 2x Prime etc.).
+
+Fragen? Melde dich gern bei uns!
+
+{{$footer}}
+@endcomponent
---
+++ resources/views/emails/company/ebe10-salesplan-starts-en.blade.php
@@ -0,0 +1,22 @@
+@component('mail::message')
+
+# Ready, set, GO!
+
+@component('mail::panel')
+
+Today (5th March, 11 AM CET) we’re kicking off sales for the **10th E-commerce Berlin Expo** – happening **17-18 Feb 2026** at Messe Berlin!
+
+Lock in your **prime spot at the best price** – but hurry, once they’re gone, they’re gone. 
+
+On the day of the expo, booths from the pre-sale inventory are indicated with gray-highlighted unavailable slots.
+
+Book now: [ecommerceberlin.com/exhibit](https://ecommerceberlin.com/exhibit)
+
+@endcomponent
+
+**Note:** You can't grab two spots from the same price category (like 2x Hot, 2x Prime, etc.).
+
+Questions? We’re here for you!
+
+{{$footer}}
+@endcomponent
