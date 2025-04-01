1d9b057ff8c3859a0cb03cddb32ee6223cedf64c
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-01 15:34:49 +0200

    Create teh27-vipplus-old.blade.php

---
+++ resources/views/emails/visitor/teh27-vipplus-old.blade.php
@@ -0,0 +1,71 @@
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+Przypominamy że **już w czwartek, w Warszawie, ostatnie Targi eHandlu** w znanej od wielu lat formule... na jesień planujemy kosmiczne zmiany!
+
+Jedna ze zmian to udostępnienie [plannera dnia targowego i aplikacji networkingowej](https://ecwe.pl?goto=/vipplus)
+
+a kolejna ma premierę teraz...<strong>i pilotażowo jest ograniczona tylko do 30 pakietów</strong>. 
+
+@component('mail::panel')
+
+# Uzyskaj nielimitowane wejście na wszystkie 21 Warsztatów, 
+
+# Korzystaj ze Strefy VIP 
+
+# Zjedz obiad z Prelegentami, Wystawcami, innymi VIP oraz ...najlepiej sprawdź to na stronie pakietu VIP Plus :)
+
+[Zapoznaj się z pakietem VIPem Plus](https://ecwe.pl?goto=/vipplus)
+
+@endcomponent
+
+Czy warto?
+
+## Wybrane warsztaty (w ramach VIP Plus udział nielimitowany)
+
+**Psychologia cen** i przedstawianie promocji w praktyce – szkolenie interaktywne
+
+Jak z pomocą **AI** przygotować specyfikację nowej funkcjonalności do platformy e-commerce? Praktyczne przykłady na bazie modułów do PrestaShop.
+
+Zobacz, jak **Core Web Vitals** może przełożyć się na sukces Twojego sklepu!
+
+Porzucona intencja zakupowa – jak odzyskać klientów, zanim sami zapomną, że chcieli kupić?
+
+**Pricing w e-commerce.** Jak ustalać ceny, monitorować rynek i zwiększać konkurencyjność
+
+Milion złotych w kieszeni: Matematyka dobrego CX w B2B
+
+[Lista wszystkich warsztatów](https://ecwe.pl?goto=/schedule?tab=workshops)
+
+[Lista wszystkich prezentacji - wejście nie wymaga VIP Plus](https://ecwe.pl?goto=/schedule)
+
+
+@component('mail::panel')
+
+Przypominamy, że wiosenne Targi eHandlu odbędą się w czwartek, **3 kwietnia, w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny** z prezentacjami Ekspertów oraz **120 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+...**w czwartek o 15:00** zapraszamy do wzniesienia wspólnego toastu za minione 14 lat i za powodzenie zmian, które planujemy wdrożyć na jesienną edycję E-commerce Warsaw Expo.
+
+@endcomponent
+
+
+
+Już teraz możesz korzystać z nowego narzędzia do planowania swojego dnia na targach, nawiązać kontakt przed dniem targowym ...a także automagicznie wymieniać dane kontaktowe skanując identyfikator innego zwiedzającego podczas Targów.
+
+<image src="https://res.cloudinary.com/eventjuicer/image/upload/f_auto,w_700,c_fit/v1742988591/app/shots_ecwe.png" alt="" width="700" style="max-width: 700px;"/>
+
+
+[Sprawdź planner dnia targowego ](https://ecwe.pl)
+
+Pozdrawiamy,
+
+Zespół E-commerce Warsaw Expo / Targów eHandlu
+
+
+**[Konfiguracja newslettera]({{ $unsubscribe }})**
+
+@endcomponent
+
