9fc4ba86cfca83000ee1b3b72f156e437e2db6c5
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-02 15:19:09 +0200

    teh27 ecwe old

---
+++ resources/views/emails/visitor/teh27-ecwe-old.blade.php
@@ -0,0 +1,62 @@
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+Przypominamy że **już w czwartek, w Warszawie, ostatnie Targi eHandlu** 
+
+[Odbierz wejściówkę na prezentacje i EXPO](https://ecwe.pl/join)
+ 
+W ramach **bezpłatnego udziału** masz dostęp do ponad 120 oferentów produktów i usług wspomagających prowadzenie handlu w internecie a także wejście na 4 sceny prezentacyjne. 
+
+Agenda prezentacji jak zwykle dostrzega aktualne AI-trendy ale nie zapomina o tematach, które się nie dezaktualizują.
+
+Szczególnie polecamy wywiad Marka Kicha (Host @ Sztuka E-Commerce) z Aliną Taranovską (Dyrektor e-commerce @ **Cosibella**) E-Commerce na 9 rynkach - jak robić to dobrze? 
+
+Wybrane prezentacje.
+
+Co nowego w **Kaufland Global Marketplace?** Poznaj nowe funkcjonalności i dowiedz się jak rozpocząć sprzedaż na 5 europejskich rynkach
+
+Jak opakować swoją wiedzę w produkt cyfrowy pozwalający zarabiać **na automacie powyżej 30 000 zł miesięcznie**?
+
+**ChatGPT** – jak stać się widocznym i sprzedawać więcej w wyszukiwarce AI-first?
+
+Strategia Sukcesu: **55% wyższe przychody w czasie Black Week** rok do roku dzięki skutecznej strategii performance.
+
+
+[Odbierz wejściówkę na prezentacje i EXPO](https://ecwe.pl/join)
+
+
+Warto skorzystać z nowego narzędzia do planowania swojego dnia na targach, nawiązać kontakt przed dniem targowym ...a także automagicznie wymieniać dane kontaktowe skanując identyfikator innego zwiedzającego podczas Targów.
+
+<image src="https://res.cloudinary.com/eventjuicer/image/upload/f_auto,w_700,c_fit/v1742988591/app/shots_ecwe.png" alt="" width="700" style="max-width: 700px;"/>
+
+[Odbierz wejściówkę na prezentacje i EXPO](https://ecwe.pl/join)
+
+
+# Chcesz mieć dostęp do Warsztatów w strefie VIP? 
+
+Czas zgłoszeń już minął ale **nadal można wykupić nielimitowane wejście na Warsztaty**  (w ramach pakietu także catering)
+
+[Wejścia na wszystko = Vip Plus ](https://ecwe.pl/join)
+
+
+
+
+@component('mail::panel')
+
+Targi eHandlu odbędą się w czwartek, **3 kwietnia, w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny** z prezentacjami Ekspertów oraz **120 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+...**w czwartek o 15:00** zapraszamy do wzniesienia wspólnego toastu za minione 14 lat i za powodzenie zmian, które planujemy wdrożyć na jesienną edycję E-commerce Warsaw Expo.
+
+@endcomponent
+
+
+Pozdrawiamy,
+
+Zespół E-commerce Warsaw Expo / Targów eHandlu
+
+
+@endcomponent
+
