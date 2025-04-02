f7ef0b1761f2fe39f04b7166dbcac0640c20ab59
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-02 11:47:36 +0200

    teh27 ecwe email

---
+++ resources/views/emails/visitor/teh27-ecwe-current.blade.php
@@ -0,0 +1,74 @@
+@component('mail::message')
+
+Cześć {{ $p->translate("[[fname]]") }}!
+
+Przypominamy że **już w czwartek, w Warszawie, ostatnie Targi eHandlu** w znanej od wielu lat formule... na jesień planujemy kosmiczne wręcz zmiany!
+
+**Bilet** można pobrać lub przygotować do pokazania w recepcji targów logując się w aplikacji eventowej
+
+[Logowanie do aplikacji ECWE]({{$eventAppUrl}})
+
+Warto skorzystać z nowego narzędzia do planowania swojego dnia na targach, nawiązać kontakt przed dniem targowym ...a także automagicznie wymieniać dane kontaktowe skanując identyfikator innego zwiedzającego podczas Targów.
+
+<image src="https://res.cloudinary.com/eventjuicer/image/upload/f_auto,w_700,c_fit/v1742988591/app/shots_ecwe.png" alt="" width="700" style="max-width: 700px;"/>
+
+[Logowanie do aplikacji ECWE]({{$eventAppUrl}})
+
+@component('mail::panel')
+
+Nie udało się Tobie otrzymać bezpłatne wejście na Warsztaty?
+
+**Nadal można wykupić nielimitowane wejście na 20 Warsztatów** w Strefie VIP (w ramach pakietu także catering)
+
+[Zapoznaj się z VIPem Plus]({{$eventAppUrl}}?goto=/vipplus)
+
+@endcomponent
+
+# Wybrane tematy prezentacji (wstęp wolny)
+
+**ChatGPT** – jak stać się widocznym i sprzedawać więcej w wyszukiwarce AI-first?
+
+Strategia Sukcesu: **55% wyższe przychody w czasie Black Week** rok do roku dzięki skutecznej strategii performance.
+
+Co nowego w **Kaufland Global Marketplace?** Poznaj nowe funkcjonalności i dowiedz się jak rozpocząć sprzedaż na 5 europejskich rynkach
+
+Jak opakować swoją wiedzę w produkt cyfrowy pozwalający zarabiać **na automacie powyżej 30 000 zł miesięcznie**?
+
+[Lista wszystkich prezentacji]({{$eventAppUrl}}?goto=/schedule)
+
+## Wybrane warsztaty (tylko dla zaproszonych uczestników lub posiadaczy VIP Plus)
+
+**Psychologia cen** i przedstawianie promocji w praktyce – szkolenie interaktywne
+
+Jak z pomocą **AI** przygotować specyfikację nowej funkcjonalności do platformy e-commerce? Praktyczne przykłady na bazie modułów do PrestaShop.
+
+Zobacz, jak **Core Web Vitals** może przełożyć się na sukces Twojego sklepu!
+
+Porzucona intencja zakupowa – jak odzyskać klientów, zanim sami zapomną, że chcieli kupić?
+
+**Pricing w e-commerce.** Jak ustalać ceny, monitorować rynek i zwiększać konkurencyjność
+
+Milion złotych w kieszeni: Matematyka dobrego CX w B2B
+
+[Lista wszystkich warsztatów]({{$eventAppUrl}}?goto=/schedule?tab=workshops)
+
+
+
+@component('mail::panel')
+
+Targi eHandlu odbędą się w czwartek, **3 kwietnia, w EXPO XXI w Warszawie.**
+
+Targi eHandlu to **4 sceny** z prezentacjami Ekspertów oraz **120 Wystawców** - oferentów usług i produktów wspierających handel w Internecie.
+
+...**w czwartek o 15:00** zapraszamy do wzniesienia wspólnego toastu za minione 14 lat i za powodzenie zmian, które planujemy wdrożyć na jesienną edycję E-commerce Warsaw Expo.
+
+@endcomponent
+
+
+Pozdrawiamy,
+
+Zespół E-commerce Warsaw Expo / Targów eHandlu
+
+
+@endcomponent
+
