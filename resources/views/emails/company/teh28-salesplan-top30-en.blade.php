dba98f216763c5c790c992d78f559e61e72fd1d4
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-11 14:09:35 +0200

    up

---
+++ resources/views/emails/company/teh28-salesplan-top30-en.blade.php
@@ -0,0 +1,38 @@
+@component('mail::message')
+ 
+# Hi {{ array_get($profile, "fname")  }},
+
+As mentioned during the previous edition of the "Targi eHandlu" Trade Fair, we are implementing changes whose final effects will be visible during the **autumn edition of E-commerce Warsaw Expo – October 24, 2025, at EXPO XXI.**
+
+The most significant change – from the Exhibitors’ perspective – seems to be the introduction of an entrance fee for visitors. Visitors will need to purchase a ticket either via our homepage or through an Exhibitor’s profile – in the latter case, by sharing their contact details, the ticket will be available at a discounted price.
+
+These changes are driven by our desire to increase exposure for international exhibitors and to professionalize the industry through more effective networking.
+
+**Here is the plan for the launch of exhibitor space sales for E-commerce Warsaw Expo.**
+
+
+
+@component('mail::panel')
+
+Sales of exhibition spaces for TOP30 Exhibitors Contest (your company!) starts on 
+
+## Tuesday 10:00 am CET
+
+[Hall plan, prices](https://ecommercewarsaw.com/top30_ecwe24102025)
+
+Regular sales (higher prices)  - for all interested parties - starts on Wednesday.
+
+@endcomponent
+
+
+## Interested in 3 or more booths? 
+
+If you need 3 or more booths, please contact us before Tuesday 
+
+
+Regards, 
+
+{{$footer}}
+
+@endcomponent
+
