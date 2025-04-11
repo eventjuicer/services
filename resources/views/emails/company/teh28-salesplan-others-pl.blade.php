d279452b08045f7e2e732c8d306569c0dc33a3f1
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-11 13:36:36 +0200

    teh28 salesplan

---
+++ resources/views/emails/company/teh28-salesplan-others-pl.blade.php
@@ -0,0 +1,42 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+
+Zgodnie z tym co zasygnalizowaliśmy podczas minionej edycji Targów eHandlu, wdrażamy zmiany, których końcowe efekty pojawia się podczas **jesiennej edycji E-commerce Warsaw Expo - 24. października 2025, EXPO XXI**
+
+Najważniejszą - z punktu widzenia Wystawców - zmianą wydaje nam się plan wprowadzenia opłaty za udział w charakterze zwiedzającego. Zwiedzający będzie musiał zakupić bilet wstępu na stronie głównej lub przez profil Wystawcy - wtedy, udostepniąjąc swoje dane kontaktowe, bilet będzie w niższej cenie.
+
+Zmiany podyktowane są chęcią większej ekspozycji na wystawców zagranicznych oraz chęcią profesjonalizacji branży przez bardziej skuteczny networking.
+
+**Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na E-commerce Warsaw Expo.**
+
+
+@component('mail::panel')
+
+# Środa - 16 kwietnia 10:00
+
+Start sprzedaży dla Twojej firmy (Wystawców spoza grona Top30 rankingu Konkursu dla Wystawców poprzedniej edycji Targów); Start otwartej sprzedaży.
+
+[Układ hali, ceny](https://targiehandlu.pl/exhibit)
+
+
+@endcomponent
+
+
+## Od dzisiaj do 16:00 w poniedziałek - Sprzedaż większych powierzchni 
+
+Sprzedaż dla Wystawców jesiennych Targów eHandlu zainteresowanych większą przestrzenią (3 i więcej stoisk) 
+
+[Układ hali, ceny](https://targiehandlu.pl/exhibit)
+
+**Prośba o info zwrotne jeśli Twoja firma jest zainteresowana min. 3 stoiskami. **
+
+
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
---
+++ resources/views/emails/company/teh28-salesplan-top30-pl.blade.php
@@ -0,0 +1,41 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname")  }},
+
+Zgodnie z tym co zasygnalizowaliśmy podczas minionej edycji Targów eHandlu, wdrażamy zmiany, których końcowe efekty pojawia się podczas **jesiennej edycji E-commerce Warsaw Expo - 24. października 2025, EXPO XXI**
+
+Najważniejszą - z punktu widzenia Wystawców - zmianą wydaje nam się plan wprowadzenia opłaty za udział w charakterze zwiedzającego. Zwiedzający będzie musiał zakupić bilet wstępu na stronie głównej lub przez profil Wystawcy - wtedy, udostepniąjąc swoje dane kontaktowe, bilet będzie w niższej cenie.
+
+Zmiany podyktowane są chęcią większej ekspozycji na wystawców zagranicznych oraz chęcią profesjonalizacji branży przez bardziej skuteczny networking.
+
+**Oto plan dotyczący startu sprzedaży powierzchni wystawienniczej na E-commerce Warsaw Expo.**
+
+
+@component('mail::panel')
+
+# Wtorek - 15 kwietnia 10:00
+
+**Ekskluzywna sprzedaż dla firm z TOP30** Konkursu dla Wystawców czyli dla Twojej firmy
+
+[Układ hali, ceny](https://targiehandlu.pl/top30_ecwe24102025)
+
+(W środę - 16 kwietnnia -  sprzedaż zostanie publicznie otwarta dla pozostałych zainteresowanych.)
+
+@endcomponent
+
+
+## Od dzisiaj do 16:00 w poniedziałek - Sprzedaż hurtowa powierzchni 
+
+Sprzedaż dla Wystawców minionych/wiosennych Targów eHandlu zainteresowanych większą przestrzenią - **3 i więcej stoisk**
+
+[Układ hali, ceny](https://targiehandlu.pl/top30_ecwe24102025)
+
+**Prośba o info zwrotne jeśli Twoja firma jest zainteresowana min. 3 stoiskami. **
+
+
+Pozdrowienia, 
+
+{{$footer}}
+
+@endcomponent
+
