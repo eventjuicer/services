44e9b196c5845cea881484c1a6129e5a2560a2fa
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-04-14 11:19:18 +0200

    reminder

--- resources/views/emails/company/teh28-salesplan-others-pl.blade.php
+++ resources/views/emails/company/teh28-salesplan-others-pl.blade.php
@@ -5,7 +5,7 @@
 
 Zgodnie z tym co zasygnalizowaliśmy podczas minionej edycji Targów eHandlu, wdrażamy zmiany, których końcowe efekty pojawia się podczas **jesiennej edycji E-commerce Warsaw Expo - 24. października 2025, EXPO XXI**
 
-Najważniejszą - z punktu widzenia Wystawców - zmianą wydaje nam się plan wprowadzenia opłaty za udział w charakterze zwiedzającego. Zwiedzający będzie musiał zakupić bilet wstępu na stronie głównej lub przez profil Wystawcy - wtedy, udostepniąjąc swoje dane kontaktowe, bilet będzie w niższej cenie.
+Najważniejszą - z punktu widzenia Wystawców - zmianą wydaje nam się plan wprowadzenia opłaty za udział w charakterze zwiedzającego. Zwiedzający będzie musiał zakupić bilet wstępu na stronie głównej lub przez profil Wystawcy - wtedy, udostepniąjąc swoje dane kontaktowe, bilet będzie w niższej cenie (29 złotych).
 
 Zmiany podyktowane są chęcią większej ekspozycji na wystawców zagranicznych oraz chęcią profesjonalizacji branży przez bardziej skuteczny networking.
 
@@ -24,7 +24,7 @@ Start sprzedaży dla Twojej firmy (Wystawców spoza grona Top30 rankingu Konkurs
 @endcomponent
 
 
-## Od dzisiaj do 16:00 w poniedziałek - Sprzedaż większych powierzchni 
+## Do 16:00 w poniedziałek - Sprzedaż większych powierzchni 
 
 Sprzedaż dla Wystawców jesiennych Targów eHandlu zainteresowanych większą przestrzenią (3 i więcej stoisk) 
 
--- resources/views/emails/company/teh28-salesplan-top30-en.blade.php
+++ resources/views/emails/company/teh28-salesplan-top30-en.blade.php
@@ -4,7 +4,7 @@
 
 As mentioned during the previous edition of the "Targi eHandlu" Trade Fair, we are implementing changes whose final effects will be visible during the **autumn edition of E-commerce Warsaw Expo – October 24, 2025, at EXPO XXI.**
 
-The most significant change – from the Exhibitors’ perspective – seems to be the introduction of an entrance fee for visitors. Visitors will need to purchase a ticket either via our homepage or through an Exhibitor’s profile – in the latter case, by sharing their contact details, the ticket will be available at a discounted price.
+The most significant change – from the Exhibitors’ perspective – seems to be the introduction of an entrance fee for visitors. Visitors will need to purchase a ticket either via our homepage or through an Exhibitor’s profile – in the latter case, by sharing their contact details, the ticket will be available at a discounted price (approx 7.00 EUR)
 
 These changes are driven by our desire to increase exposure for international exhibitors and to professionalize the industry through more effective networking.
 
