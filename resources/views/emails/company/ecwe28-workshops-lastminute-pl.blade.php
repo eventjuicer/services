fe4aa73e74fc51bd5b585edb93071e4f32a29ffc
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-10-06 14:02:24 +0200

    ecwe workshops lastminute

---
+++ resources/views/emails/company/ecwe28-workshops-lastminute-pl.blade.php
@@ -0,0 +1,53 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname") }},
+
+Ostatnie momenty na zgłoszenie chęci **przeprowadzenia Warsztatu** podczas jesiennych targów E-commerce Warsaw Expo.
+
+Wszystkie informacje znajdują się na stronie: [https://ecommercewarsaw.com/workshops](https://ecommercewarsaw.com/workshops). **Zgłoszenia przyjmowane są tylko do 7 października.**
+
+Czy już promujesz swój profil Wystawcy? [Lista bannerów, link do profilu, link do rankingu!]({!!$accountUrlPromotePublic!!})
+
+Największa liczba kupujących bilet wybiera shoper, tpay i GS 1 na swojego "badge buddy".
+
+Pozdrawiam, 
+
+{{$footer}}
+
+PS: oto skrót wcześniejszej wiadomości (jeśli nie miał-aś/eś jeszcze okazji ją odczytać)
+
+(...)
+
+**Wystartowała sprzedaż biletów dla Zwiedzających na jesienne targi E-commerce Warsaw Expo.** 
+
+Po raz pierwszy w historii "Targów eHandlu", wstęp jest biletowany. 
+
+(...)
+
+@component('mail::panel')
+
+(...) **Bilety Zwiedzających są 4x tańsze gdy kupowane są przez profil Wystawcy**. Każdy Twój "Badge Buddy" będzie miał **identyfikator z Twoim logotypem**. Przekażemy Tobie listę wszystkich Twoich fanów (bez danych kontaktowych).
+
+@endcomponent
+
+To co? Czas start!
+
+[Lista bannerów, link do profilu, link do rankingu!]({!!$accountUrlPromotePublic!!})
+
+@component('mail::panel')
+
+Na Twojej stronie z materiałami znajdziesz link do rankingu "Love Brands" czyli które firmy wybierają zwiedzający kupujący bilet sponsorowany. Wyniki zaskakują :)
+
+@endcomponent
+
+PS: Niedługo wystartuje konkurs dla Wystawców - już teraz możesz wyrobić sobie lepsze miejsce w rankingu nie opóźniając promowania swojego udziału w Targach.
+
+PS: Jeśli Twoja strona z materiałami promocyjnymi jest niedostępna [zaktualizuj dane firmy]({!!$accountUrl!!})
+
+Pozdrawiam, 
+
+{{$footer}}
+
+
+@endcomponent
+
