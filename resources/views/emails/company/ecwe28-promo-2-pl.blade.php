9b1ab3780bcdc2de500f68fdc31220995353aee8
Adam Zygadlewicz <adam@zygadlewicz.com>
2025-10-10 15:04:47 +0200

    promo2

---
+++ resources/views/emails/company/ecwe28-promo-2-pl.blade.php
@@ -0,0 +1,30 @@
+@component('mail::message')
+ 
+# Cześć {{ array_get($profile, "fname") }},
+
+**Właśnie opublikowaliśmy agendę wystąpień** - tym samym wciskamy pedał gazu i zaczynamy aktywną promocję E-commerce Warsaw Expo!
+
+Widzimy, że Wystawcy mocno włączyli się w promocję. **Pierwsza - płatna - edycja** to spore wyzwanie komunikacyjne, organizacyjne i cieszymy się, że na sukces pracujemy razem. 
+
+Dziękujemy!
+
+[Lista bannerów, link do profilu, link do rankingu!]({!!$accountUrlPromotePublic!!})
+
+Link do "Badge Buddies", na powyższej stronie, pokazuje, że zmienił się lider! **Kupujący bilet sponsorowany najczęściej wybierają shoper na swojego "Badge Buddy"**. Gratulujemy!
+
+**A co jeśli w przyszłym tygodniu ogłosimy,** że zwycięzca konkursu dla Wystawców zostanie Patronem Generalnym wiosennej edycji E-commerce Warsaw Expo?
+
+...mocno zastanawiamy się nad tym :)
+
+Zapraszamy do promowania swojego profilu Wystawcy. 
+
+[Lista bannerów, link do profilu, link do rankingu!]({!!$accountUrlPromotePublic!!})
+
+
+Pozdrawiam, 
+
+{{$footer}}
+
+
+@endcomponent
+
